68c49e986f5a46294579ee600f0a43558a136f5c
Antti Leppä <antti.leppa@foyt.fi>
2013-07-05 14:54:43 +0300

    ** Added findByMultilingualStringAndLocale and updateValue methods into LocalizedStringDAO

--- persistence/src/main/java/fi/foyt/fni/persistence/dao/common/LocalizedStringDAO.java
+++ persistence/src/main/java/fi/foyt/fni/persistence/dao/common/LocalizedStringDAO.java
@@ -4,10 +4,14 @@ import java.util.Locale;
 
 import javax.enterprise.context.RequestScoped;
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import fi.foyt.fni.persistence.dao.DAO;
 import fi.foyt.fni.persistence.dao.GenericDAO;
 import fi.foyt.fni.persistence.model.common.LocalizedString;
+import fi.foyt.fni.persistence.model.common.LocalizedString_;
 import fi.foyt.fni.persistence.model.common.MultilingualString;
 
 @RequestScoped
@@ -29,5 +33,30 @@ public class LocalizedStringDAO extends GenericDAO<LocalizedString> {
 
     return localizedString;
   }
+
+	public LocalizedString findByMultilingualStringAndLocale(MultilingualString multilingualString, Locale locale) {
+		EntityManager entityManager = getEntityManager();
+
+    CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+    CriteriaQuery<LocalizedString> criteria = criteriaBuilder.createQuery(LocalizedString.class);
+    Root<LocalizedString> root = criteria.from(LocalizedString.class);
+    criteria.select(root);
+    criteria.where(
+    	criteriaBuilder.and(
+    		criteriaBuilder.equal(root.get(LocalizedString_.multilingualString), multilingualString),
+    		criteriaBuilder.equal(root.get(LocalizedString_.locale), locale)
+    	)
+    );
+
+    return getSingleResult(entityManager.createQuery(criteria));
+	}
+	
+	public LocalizedString updateValue(LocalizedString localizedString, String value) {
+		localizedString.setValue(value);
+		
+		getEntityManager().persist(localizedString);
+		
+		return localizedString;
+	}
 
 }
