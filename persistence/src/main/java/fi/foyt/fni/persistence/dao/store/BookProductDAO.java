3a432e0cdcf2e490ee8a45cbf9035690ee0d9ccb
Antti Leppä <antti.leppa@foyt.fi>
2013-07-06 12:37:19 +0300

    ** Added DAO for BookProduct

---
+++ persistence/src/main/java/fi/foyt/fni/persistence/dao/store/BookProductDAO.java
@@ -0,0 +1,43 @@
+package fi.foyt.fni.persistence.dao.store;
+
+import java.util.Date;
+
+import javax.enterprise.context.RequestScoped;
+
+import fi.foyt.fni.persistence.dao.DAO;
+import fi.foyt.fni.persistence.dao.GenericDAO;
+import fi.foyt.fni.persistence.model.common.MultilingualString;
+import fi.foyt.fni.persistence.model.store.BookProduct;
+import fi.foyt.fni.persistence.model.store.ProductImage;
+import fi.foyt.fni.persistence.model.users.User;
+
+@RequestScoped
+@DAO
+public class BookProductDAO extends GenericDAO<BookProduct> {
+  
+	public BookProduct create(MultilingualString name, MultilingualString description, Double price, Boolean downloadable, ProductImage defaultImage, Date created, User creator, Date modified, User modifier, Boolean published) {
+    
+		BookProduct bookProduct = new BookProduct();
+    bookProduct.setCreated(created);
+    bookProduct.setCreator(creator);
+    bookProduct.setDefaultImage(defaultImage);
+    bookProduct.setDescription(description);
+    bookProduct.setDownloadable(downloadable);
+    bookProduct.setModified(modified);
+    bookProduct.setModifier(modifier);
+    bookProduct.setName(name);
+    bookProduct.setPrice(price);
+    bookProduct.setPublished(published);
+    
+    getEntityManager().persist(bookProduct);
+    
+    return bookProduct;
+	}
+
+	public BookProduct updateDownloadable(BookProduct bookProduct, Boolean downloadable) {
+		bookProduct.setDownloadable(downloadable);
+		getEntityManager().persist(bookProduct);
+    return bookProduct;
+	}
+	
+}
