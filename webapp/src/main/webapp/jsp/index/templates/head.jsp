3e06b1909c206031250b9ba803511d4cc0779d1a
Antti Leppä <antti.leppa@foyt.fi>
2013-07-17 20:39:28 +0300

    ** Changed jshsh md5 scripts to be loaded from jsdelivr.net cdn ** Commented out editprofile view because it does not currently work ** Changed site index to use jquery instead of prototype ** Changed site index to use new novus theme ** Removed index jsp pages

--- webapp/src/main/webapp/jsp/index/templates/head.jsp
+++
@@ -1,5 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
-
-<jsp:include page="../../templates/head.jsp"></jsp:include>
-
-<link type="text/css" rel="stylesheet" href="${pageContext.request.contextPath}/themes/default_dev/css/index.css" />
--- webapp/src/main/webapp/resources/scripts/gui/index/index.js
+++ webapp/src/main/webapp/resources/scripts/gui/index/index.js
@@ -1,92 +1,37 @@
-IndexViewController = Class.create({
-  initialize : function() {
-    this._articleLinkClickListener = this._onArticleLinkClick.bindAsEventListener(this);
-    this._defaultArticleLinkClickListener = this._onDefaultArticleLinkClick.bindAsEventListener(this);
-  },
-  destroy: function () {
-    $('indexSidebarContent').select('.articleLink').invoke("stopObserving", "click", this._articleLinkClickListener);
-    $('indexSidebarContent').select('.defaultArticleLink').invoke("stopObserving", "click", this._defaultArticleLinkClickListener);
-  },
-  setup: function () {
-    // TODO: Test cookie support
-    
-    $('indexSidebarContent').select('.articleLink').invoke("observe", "click", this._articleLinkClickListener);
-    $('indexSidebarContent').select('.defaultArticleLink').invoke("observe", "click", this._defaultArticleLinkClickListener);
-    
-    var action = getJsVariable("action");
-    if (action) {
-      var actionParameters = getJsVariable("actionParameters");
-      this._handleAction(action, actionParameters);
-    }
-  },
-  openArticle: function (articleId) {
-    $('frontPageArticle').down('.frontPageArticleLoading').show();
-    API.get(CONTEXTPATH + '/v1/articles/' + articleId, {
-      onComplete: function () {
-        $('frontPageArticle').down('.frontPageArticleLoading').hide();
-      },
-      onSuccess: function (jsonResponse) {
-        var article = jsonResponse.response;
-        
-        var frontPageArticle = $('frontPageArticle');
-        frontPageArticle.down('.frontPageArticleTitle').update(article.title);
-        frontPageArticle.down('.frontPageArticleContent').update(article.content);
-      }
+(function() {
+  
+  function viewArticle(id) {
+    $('#index-front-page-article > index-front-page-article-loading').show();
+    $.ajax({
+      url: CONTEXTPATH + '/v1/articles/' + id
+    }).done(function(data, textStatus, jqXHR) {
+      var article = data.response;
+      $('#index-front-page-article > .index-front-page-article-title').html(article.title);
+      $('#index-front-page-article > .index-front-page-article-content').html(article.content);
+      $('#index-front-page-article > index-front-page-article-loading').hide();
     });
-  },
-  _handleAction: function (action, actionParameters) {
-    var parameters = new Hash();
+  };
+
+  $(document).ready(function(){
+    /* Articles */
     
-    if (actionParameters) {
-      var parametersArray = actionParameters.split(';');
-      for (var i = 0, l = parametersArray.length; i < l; i++) {
-        var parameterArray = parametersArray[i].split(':');
-        parameters.set(parameterArray[0], parameterArray[1]);
-      }
-    }
-    
-    switch (action) {
-      case 'viewmessage':
-        this._handleViewMessageAction(parameters);
-      break;
-    }
-  },
-  _handleViewMessageAction: function (parameters) {
-    var messageId = parameters.get('messageId');
-    // If no message id is found, the request is invalid and thus should be ignored
-    if (messageId) {
-      // Viewing messages requires authentication. If user is not logged in we need to login first.
-      if (!isLoggedIn()) {
-        window.location.href = CONTEXTPATH + '/login?redirectUrl=' + encodeURIComponent(window.location.href); 
-      } else {
-        var messagesWidget = siteMenuBarController.getWidget('messages');
-        var onFolderLoad = null;
-        onFolderLoad = function (event) {
-          messagesWidget.removeListener("folderLoad", window, onFolderLoad);
-          messagesWidget.viewMessage(messageId);
-        };
-        
-        messagesWidget.addListener("folderLoad", window, onFolderLoad, 9999);
-        siteMenuBarController.openMenuWidget(messagesWidget);
-      }
-    }
-  },
-  _onArticleLinkClick: function (event) {
-    var list = Event.element(event).up('li');
-    var articleId = list.down('input[name="articleId"]').value;
-    
-    this.openArticle(articleId);
-  },
-  _onDefaultArticleLinkClick: function (event) {
-    var list = Event.element(event).up('li');
-    var articleId = list.down('input[name="articleId"]').value;
-    
-    var _this = this;
-    API.put(CONTEXTPATH + '/v1/articles/setLocaleDefault/' + articleId, {
-      onSuccess: function (jsonResponse) {
-        var article = jsonResponse.response;
-        _this.openArticle(article.id);
-      }
+    $(document).on( 'click', '.index-sidebar-article-link', function (event) {
+      viewArticle($(this).data('article-id'));
     });
-  }
-});
+    
+    $(document).on('click', '.index-sidebar-default-article-link', function (event) {
+      $('#index-front-page-article > index-front-page-article-loading').show();
+      var id = $(this).data('article-id');
+      $.ajax({
+        url: CONTEXTPATH + '/v1/articles/setLocaleDefault/' + id,
+        method: 'PUT',
+        data: {
+          locale: LOCALE
+        }
+      }).done(function(data, textStatus, jqXHR) {
+        viewArticle(id);
+      });
+    });
+  });
+  
+}).call(this);
