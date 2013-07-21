40a3f926e3c059c95c2a92970c55ec4e7e8e202b
Antti Leppä <antti.leppa@foyt.fi>
2013-07-21 21:27:13 +0300

    ** Added scheduled synchronizer for remote blog entries

---
+++ webapp/src/main/java/fi/foyt/fni/blog/BlogSynchronizationScheduler.java
@@ -0,0 +1,37 @@
+package fi.foyt.fni.blog;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.inject.Inject;
+
+import fi.foyt.fni.persistence.model.blog.BlogCategory;
+import fi.foyt.fni.system.SystemSettingsController;
+
+@Singleton
+public class BlogSynchronizationScheduler {
+
+  @Inject
+  private Logger logger;
+
+  @Inject
+  private BlogController blogController;
+
+  @Inject
+  private SystemSettingsController systemSettingsController;
+
+  @Schedule(dayOfWeek = "*", hour = "*", minute = "0", second = "0", year = "*", persistent = false)
+  public synchronized void synchronizeFeeds() {
+  	logger.info("Scheduled blog rss synchronization started");
+  	
+  	List<BlogCategory> blogCategories = blogController.listUnsynchronizedBlogCategories();
+  	for (BlogCategory blogCategory : blogCategories) {
+  		blogController.synchronizeEntries(blogCategory);
+  	}
+
+  	logger.info("Scheduled blog rss synchronization finished");
+  }
+
+}
