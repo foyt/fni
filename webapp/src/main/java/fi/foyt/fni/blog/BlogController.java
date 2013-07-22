9eca33df2d5b2c175f6a8ec59fdd414d7f6f29c9
Antti Leppä <antti.leppa@foyt.fi>
2013-07-22 19:12:03 +0300

    ** Replaced Abdera with rome because Abdera uses httpcore 3.x which does not work with OpenShift ** Removed PDFBox from bom as unused

--- webapp/src/main/java/fi/foyt/fni/blog/BlogController.java
+++ webapp/src/main/java/fi/foyt/fni/blog/BlogController.java
@@ -1,9 +1,10 @@
 package fi.foyt.fni.blog;
 
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,19 +13,15 @@ import javax.ejb.Stateful;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
-import org.apache.abdera.Abdera;
-import org.apache.abdera.ext.rss.RssItem;
-import org.apache.abdera.model.Category;
-import org.apache.abdera.model.Document;
-import org.apache.abdera.model.Entry;
-import org.apache.abdera.model.Feed;
-import org.apache.abdera.model.Link;
-import org.apache.abdera.model.Person;
-import org.apache.abdera.protocol.Response.ResponseType;
-import org.apache.abdera.protocol.client.AbderaClient;
-import org.apache.abdera.protocol.client.ClientResponse;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
+
+import com.sun.syndication.feed.synd.SyndCategory;
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.FeedException;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
 
 import fi.foyt.fni.persistence.dao.blog.BlogCategoryDAO;
 import fi.foyt.fni.persistence.dao.blog.BlogEntryDAO;
@@ -71,105 +68,98 @@ public class BlogController {
 	public List<BlogCategory> listUnsynchronizedBlogCategories() {
 		return blogCategoryDAO.listBySyncNeNullAndNextSyncLe(new Date());
 	}
-	
+
+	@SuppressWarnings("unchecked")
 	public BlogCategory synchronizeEntries(BlogCategory blogCategory) {
-	  Abdera abdera = new Abdera();
-	  AbderaClient client = new AbderaClient(abdera);
-	  ClientResponse resp = client.get(blogCategory.getSyncUrl());
-	  if (resp.getType() == ResponseType.SUCCESS) {
-	    Document<Feed> doc = resp.getDocument();
-	    Feed root = doc.getRoot();
-	    
-	    List<Entry> entries = root.getEntries();
-	    for (Entry entry : entries) {
-	    	if (entry instanceof RssItem) {
-	    		RssItem rssItem = (RssItem) entry;
-	    		String link = null;
-	    		String guid = null;
-	    		String authorName = null;
-	    		Person author = rssItem.getAuthor();
-	    		if (author != null) {
-	    			authorName = author.getText();
-	    		}
-	    		
-	    		Link alternateLink = rssItem.getAlternateLink();
-	    		if (alternateLink != null) {
-						try {
-							link = alternateLink.getHref().toURL().toExternalForm();
-						} catch (MalformedURLException | URISyntaxException e) {
-	    				logger.log(Level.WARNING, "RSS Feed Entry contained erroneous link", e);
-						}
-						
-						try {
-							guid = rssItem.getId().toURL().toExternalForm();
-						} catch (MalformedURLException | URISyntaxException e) {
-	    				logger.log(Level.WARNING, "RSS Feed Entry contained erroneous GUID", e);
-						}
-	    			
-	    			Date modified = rssItem.getEdited();
-	    			Date created = rssItem.getPublished();
-	    			
-	    			if (created == null) {
-	    				created = new Date();
-	    			}
-	    			
-	    			if (modified == null) {
-	    				modified = created;
-	    			}
-	    			
-	    			String summary = entry.getSummary();
-	    			if (StringUtils.endsWith(summary, "[...]")) {
-	    				summary = summary.substring(0, summary.length() - 5);
-	    			}
-	    			summary = StringUtils.strip(summary);
+		SyndFeedInput input = new SyndFeedInput();
+		try {
+			SyndFeed feed = input.build(new XmlReader(new URL(blogCategory.getSyncUrl())));
+			Iterator<SyndEntry> entryIterator = feed.getEntries().iterator();
+			while (entryIterator.hasNext()) {
+				SyndEntry syndEntry = entryIterator.next();
+				String title = syndEntry.getTitle();
+				String summary = syndEntry.getDescription().getValue();
+				Date created = syndEntry.getPublishedDate();
+				Date modified = syndEntry.getUpdatedDate();
+				if (modified == null) {
+					modified = created;
+				}
+				String link = syndEntry.getLink();
+				String guid = syndEntry.getUri();
+				String authorName = syndEntry.getAuthor();
+				List<String> categories = new ArrayList<>();
+				for (SyndCategory category : (List<SyndCategory>) syndEntry.getCategories()) {
+					categories.add(category.getName());
+				};
+				
+  			synchronizeEntry(blogCategory, title, summary, modified, created, link, guid, authorName, categories);
+				
+			}
+			
+			scheduleNextSync(blogCategory);
+			
+		} catch (IllegalArgumentException | FeedException | IOException e) {
+			logger.log(Level.WARNING, "RSS Synchronization failed", e);
+		}
+		
+		return blogCategory;
+	}
+	
+	private void synchronizeEntry(BlogCategory blogCategory, String title, String summary, Date modified, Date created, String link, String guid, String authorName, List<String> categories) {
+		if (created == null) {
+			created = new Date();
+		}
+		
+		if (modified == null) {
+			modified = created;
+		}
+		
+		if (StringUtils.endsWith(summary, "[...]")) {
+			summary = summary.substring(0, summary.length() - 5);
+		}
+		summary = StringUtils.strip(summary);
 
-	    			if (StringUtils.isNotBlank(guid)) {
-	    				BlogEntry blogEntry = blogEntryDAO.findByGuid(guid);
-	    				if (blogEntry != null) {
-	    					blogEntryDAO.updateAuthorName(blogEntry, authorName);
-	    					blogEntryDAO.updateTitle(blogEntry, entry.getTitle());
-	    					blogEntryDAO.updateLink(blogEntry, link);
-	    					blogEntryDAO.updateSummary(blogEntry, summary);
-	    					blogEntryDAO.updateModified(blogEntry, modified);
-	    				} else {
-	    					blogEntry = blogEntryDAO.create(guid, blogCategory, null, authorName, link, entry.getTitle(), summary, null, created, null, modified, null);
-	    				}
-	    				
-	    				List<BlogTag> entryTags = new ArrayList<>();
+		if (StringUtils.isNotBlank(guid)) {
+			BlogEntry blogEntry = blogEntryDAO.findByGuid(guid);
+			if (blogEntry != null) {
+				blogEntryDAO.updateAuthorName(blogEntry, authorName);
+				blogEntryDAO.updateTitle(blogEntry, title);
+				blogEntryDAO.updateLink(blogEntry, link);
+				blogEntryDAO.updateSummary(blogEntry, summary);
+				blogEntryDAO.updateModified(blogEntry, modified);
+			} else {
+				blogEntry = blogEntryDAO.create(guid, blogCategory, null, authorName, link, title, summary, null, created, null, modified, null);
+			}
+			
+			List<BlogTag> entryTags = new ArrayList<>();
 
-	    				for (Category category : entry.getCategories()) {
-		    				String text = category.getText();
-		    				BlogTag blogTag = blogTagDAO.findByText(text);
-		    				if (blogTag == null) {
-		    					blogTag = blogTagDAO.create(text);
-		    				}
-		    				
-		    				entryTags.add(blogTag);
-		    			}
-	    				
-	    				List<BlogEntryTag> deleteEntryTags = blogEntryTagDAO.listByEntryAndTagNotIn(blogEntry, entryTags);
-	    				for (BlogEntryTag deleteEntryTag : deleteEntryTags) {
-	    					blogEntryTagDAO.delete(deleteEntryTag);
-	    				}
-	    				
-	    				for (BlogTag entryTag : entryTags) {
-	    					if (blogEntryTagDAO.findByEntryAndTag(blogEntry, entryTag) == null) {
-	    						blogEntryTagDAO.create(blogEntry, entryTag);
-	    					}
-	    				}
-	    			} else {
-	    				logger.warning("RSS Feed Entry did not contain a valid guid, skipping");
-	    			}
-	    		}
-	    	} else {
-  				logger.warning("Unknown Feed Entry type, skipping");
-	    	}
-	    }
-	  } else {
-			logger.warning("RSS Synchronization failed for " + blogCategory.getSyncUrl() + " with following error: " + resp.getStatusText());
-	  }
-	  
-	  Date now = new Date();
+			for (String category : categories) {
+				BlogTag blogTag = blogTagDAO.findByText(category);
+
+				if (blogTag == null) {
+					blogTag = blogTagDAO.create(category);
+				}
+				
+				entryTags.add(blogTag);
+			}
+			
+			List<BlogEntryTag> deleteEntryTags = blogEntryTagDAO.listByEntryAndTagNotIn(blogEntry, entryTags);
+			for (BlogEntryTag deleteEntryTag : deleteEntryTags) {
+				blogEntryTagDAO.delete(deleteEntryTag);
+			}
+			
+			for (BlogTag entryTag : entryTags) {
+				if (blogEntryTagDAO.findByEntryAndTag(blogEntry, entryTag) == null) {
+					blogEntryTagDAO.create(blogEntry, entryTag);
+				}
+			}
+		} else {
+			logger.warning("RSS Feed Entry did not contain a valid guid, skipping");
+		}
+	}
+
+	private void scheduleNextSync(BlogCategory blogCategory) {
+		Date now = new Date();
 	  
 	  switch (blogCategory.getSync()) {
 	  	case HOURLY:
@@ -182,8 +172,6 @@ public class BlogController {
 	  		blogCategoryDAO.updateNextSync(blogCategory, DateUtils.addMonths(now, 1));
 	  	break;
 	  }
-	  
-	  return blogCategory;
 	}
 
 }
