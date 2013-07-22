2ba1180f41a778fd30940303ab8712796ffa3b2c
Antti Leppä <antti.leppa@foyt.fi>
2013-07-22 05:22:14 +0300

    ** Changed BlogCategory sync property to be persisted as string

--- persistence/src/main/java/fi/foyt/fni/persistence/model/blog/BlogCategory.java
+++ persistence/src/main/java/fi/foyt/fni/persistence/model/blog/BlogCategory.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import javax.persistence.Cacheable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -73,5 +75,6 @@ public class BlogCategory {
   
   @NotNull
   @Column (nullable=false)
+  @Enumerated (EnumType.STRING)
   private BlogCategorySync sync;
 }
