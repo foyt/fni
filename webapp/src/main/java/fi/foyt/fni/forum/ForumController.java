066aa625a090df2590677221387f49c8932f80ae
Antti Leppä <antti.leppa@foyt.fi>
2013-07-23 20:37:27 +0300

    ** Changed ForumController to be dependent ** Added methods for counting posts by topics, topics by forum into ForumController ** Added methods for retrieving last post of topic and forum into ForumController

--- webapp/src/main/java/fi/foyt/fni/forum/ForumController.java
+++ webapp/src/main/java/fi/foyt/fni/forum/ForumController.java
@@ -3,20 +3,25 @@ package fi.foyt.fni.forum;
 import java.util.List;
 
 import javax.ejb.Stateful;
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
+import fi.foyt.fni.persistence.dao.forum.ForumCategoryDAO;
 import fi.foyt.fni.persistence.dao.forum.ForumDAO;
 import fi.foyt.fni.persistence.dao.forum.ForumPostDAO;
 import fi.foyt.fni.persistence.dao.forum.ForumTopicDAO;
 import fi.foyt.fni.persistence.model.forum.Forum;
+import fi.foyt.fni.persistence.model.forum.ForumCategory;
 import fi.foyt.fni.persistence.model.forum.ForumPost;
 import fi.foyt.fni.persistence.model.forum.ForumTopic;
 import fi.foyt.fni.persistence.model.users.User;
 
-@RequestScoped
+@Dependent
 @Stateful
 public class ForumController {
+
+	@Inject
+	private ForumCategoryDAO forumCategoryDAO;
 
 	@Inject
 	private ForumDAO forumDAO;
@@ -27,7 +32,17 @@ public class ForumController {
 	@Inject
 	private ForumPostDAO forumPostDAO;
 	
+	// Categories
+	
+	public List<ForumCategory> listForumCategories() {
+		return forumCategoryDAO.listAll();
+	}
+	
 	// Forums
+
+	public List<Forum> listForumsByCategory(ForumCategory category) {
+		return forumDAO.listByCategory(category);
+	}
 	
 	public Forum findForumByUrlName(String urlName) {
 		return forumDAO.findByUrlName(urlName);
@@ -47,8 +62,12 @@ public class ForumController {
 		return forumTopicDAO.listByForum(forum);
 	}	
 
-	public Long countTopicReplies(ForumTopic topic) {
-		return forumPostDAO.countByTopic(topic) - 1;
+	public Long countPostsByTopic(ForumTopic topic) {
+		return forumPostDAO.countByTopic(topic);
+	}
+
+	public Long countTopicsByForum(Forum forum) {
+		return forumTopicDAO.countByForum(forum);
 	}
 
 	public ForumTopic updateTopicViews(ForumTopic topic, long views) {
@@ -76,5 +95,27 @@ public class ForumController {
 
 	public ForumPost updatePostViews(ForumPost post, long views) {
     return forumPostDAO.updateViews(post, views);
+	}
+
+	public Long countPostsByForum(Forum forum) {
+		return forumPostDAO.countByForum(forum);
+	}
+
+	public ForumPost getLastPostByTopic(ForumTopic topic) {
+		List<ForumPost> posts = forumPostDAO.listByTopicSortByCreated(topic, 0, 1);
+		if (posts.size() == 1) {
+			return posts.get(0);
+		}
+		
+		return null;
+	}
+
+	public ForumPost getLastPostByForum(Forum forum) {
+		List<ForumPost> posts = forumPostDAO.listByForumSortByCreated(forum, 0, 1);
+		if (posts.size() == 1) {
+			return posts.get(0);
+		}
+		
+		return null;
 	}
 }
