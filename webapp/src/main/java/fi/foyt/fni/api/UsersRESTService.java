938607029a93c0ee6a319ae337998dde305610a0
Antti Leppä <antti.leppa@foyt.fi>
2013-08-02 12:37:13 +0300

    ** Removed profileImage getter method from users rest service

--- webapp/src/main/java/fi/foyt/fni/api/UsersRESTService.java
+++ webapp/src/main/java/fi/foyt/fni/api/UsersRESTService.java
@@ -1,6 +1,5 @@
 package fi.foyt.fni.api;
 
-import java.io.IOException;
 import java.util.Locale;
 import java.util.logging.Logger;
 
@@ -8,7 +7,6 @@ import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -19,7 +17,6 @@ import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 
 import org.apache.commons.lang3.LocaleUtils;
-import org.apache.commons.lang3.math.NumberUtils;
 
 import fi.foyt.fni.auth.AuthenticationController;
 import fi.foyt.fni.materials.MaterialController;
@@ -30,11 +27,7 @@ import fi.foyt.fni.persistence.dao.users.UserDAO;
 import fi.foyt.fni.persistence.dao.users.UserEmailDAO;
 import fi.foyt.fni.persistence.dao.users.UserSettingDAO;
 import fi.foyt.fni.persistence.dao.users.UserSettingKeyDAO;
-import fi.foyt.fni.persistence.model.materials.Image;
-import fi.foyt.fni.persistence.model.materials.ImageSize;
-import fi.foyt.fni.persistence.model.materials.MaterialThumbnail;
 import fi.foyt.fni.persistence.model.users.User;
-import fi.foyt.fni.persistence.model.users.UserRole;
 import fi.foyt.fni.session.SessionController;
 import fi.foyt.fni.system.SystemSettingsController;
 import fi.foyt.fni.users.UserController;
@@ -116,48 +109,6 @@ public class UsersRESTService extends RESTService {
 		}
 		
 		return Response.ok(new ApiResult<>(null)).build();
-	}
-	
-	@GET
-	@Path ("/{USERID}/profileImage/{SIZE}")
-	public Response profileImage(
-			@PathParam ("USERID") String userIdParam, 
-			@PathParam ("SIZE") String sizeParam,
-			@Context HttpHeaders httpHeaders) {
-		
-		boolean currentUser = "SELF".equals(userIdParam);
-		Locale browserLocale = getBrowserLocale(httpHeaders);
-    
-		User user = null;
-		
-		if (currentUser) {
-  		User loggedUser = getLoggedUser(httpHeaders);
-  		if (!hasRole(loggedUser, UserRole.GUEST)) {
-  		  return Response.status(Response.Status.FORBIDDEN).entity(ApiMessages.getText(browserLocale, "error.generic.permissionDenied")).build();
-  		}
-  		user = loggedUser;
-		} else {
-			Long userId =  NumberUtils.createLong(userIdParam);
-			user = userDAO.findById(userId);
-		}
-		
-		ImageSize size = sizeParam == null || sizeParam.equals("ORIGINAL") ? ImageSize.ORIGINAL : ImageSize.valueOf('_' + sizeParam);
-    
-    if (user == null) {
-      return Response.status(Response.Status.NOT_FOUND).entity(ApiMessages.getText(browserLocale, "error.users.profile.userNotFound")).build();
-    }
-    
-    Image profileImage = user.getProfileImage();
-    if (profileImage == null) {
-    	return Response.status(Response.Status.NOT_FOUND).build();
-    }
-    
-    try {
-    	MaterialThumbnail imageThumbnail = materialController.getImageThumbnail(profileImage, size);
-    	return createBinaryResponse(imageThumbnail.getContent(), imageThumbnail.getContentType());
-    } catch (IOException e) {
-    	return Response.noContent().build();
-    }
 	}
 
 }
