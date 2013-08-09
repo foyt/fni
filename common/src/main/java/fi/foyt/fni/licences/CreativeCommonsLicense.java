ea5cfcfabe9ce74f0cc344bffaa644dd950e63b6
Antti Leppä <antti.leppa@foyt.fi>
2013-08-09 08:47:52 +0300

    ** Added utils for handling creative commons licenses

---
+++ common/src/main/java/fi/foyt/fni/licences/CreativeCommonsLicense.java
@@ -0,0 +1,97 @@
+package fi.foyt.fni.licences;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.ArrayUtils;
+
+public class CreativeCommonsLicense {
+	
+	public static final String URL_PREFIX = "http://creativecommons.org/licenses/";
+	public static final String ICON_PREFIX = "http://i.creativecommons.org/l/";
+	public static final String NORMAL_ICON = "88x31.png";
+	public static final String COMPACT_ICON = "80x15.png";
+		
+	public CreativeCommonsLicense(String[] properties, String version, String jurisdiction) {
+		this.properties = properties;
+		this.version = version;
+		this.jurisdiction = jurisdiction;
+	}
+	
+	public String getIconUrl(boolean compact) {
+		StringBuilder urlBuilder = new StringBuilder();
+		
+		urlBuilder.append(ICON_PREFIX);
+		urlBuilder.append(StringUtils.join(properties, '-'));
+		
+		if (StringUtils.isNotBlank(version)) {
+		  urlBuilder.append('/');
+		  urlBuilder.append(version);
+		}
+		
+		if (StringUtils.isNotBlank(jurisdiction)) {
+			urlBuilder.append('/');
+			urlBuilder.append(jurisdiction);
+		}
+		
+		urlBuilder.append('/');
+		
+		if (compact) {
+			urlBuilder.append(COMPACT_ICON);
+		} else {
+			urlBuilder.append(NORMAL_ICON);
+		}
+		
+		return urlBuilder.toString();
+	}
+	
+	public String getIconUrl() {
+		return getIconUrl(false);
+	}
+	
+	/**
+	 * Returns whether 
+	 * 
+	 * @return
+	 */
+	public boolean getPublicDomain() {
+		return properties[0].equals("publicdomain");
+	}
+	
+	public boolean getShareAlike() {
+		return (!getPublicDomain()) && ArrayUtils.contains(properties, "sa");
+	}
+	
+	public boolean getDerivatives() {
+		return getPublicDomain() || !ArrayUtils.contains(properties, "nd");
+	}
+	
+	public boolean getCommercial() {
+		return getPublicDomain() || !ArrayUtils.contains(properties, "nc");
+	}
+	
+	public boolean getAttribution() {
+		return (!getPublicDomain()) && ArrayUtils.contains(properties, "by");
+	}
+	
+	public String getUrl() {
+		StringBuilder urlBuilder = new StringBuilder();
+		
+		urlBuilder.append(URL_PREFIX);
+		urlBuilder.append(StringUtils.join(properties, '-'));
+		
+		if (StringUtils.isNotBlank(version)) {
+  		urlBuilder.append('/');
+	  	urlBuilder.append(version);
+		}
+		
+		if (StringUtils.isNotBlank(jurisdiction)) {
+			urlBuilder.append('/');
+			urlBuilder.append(jurisdiction);
+		}
+		
+		return urlBuilder.toString();
+	}
+	
+	private String[] properties;
+	private String version;
+	private String jurisdiction;
+}
---
+++ common/src/main/java/fi/foyt/fni/licences/CreativeCommonsUtils.java
@@ -0,0 +1,58 @@
+package fi.foyt.fni.licences;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class CreativeCommonsUtils {
+	
+	public static CreativeCommonsLicense parseLicenseUrl(String url) {
+		if (StringUtils.startsWith(url, CreativeCommonsLicense.URL_PREFIX)) {
+			String[] parts = StringUtils.substring(url, CreativeCommonsLicense.URL_PREFIX.length()).split("/");
+			if (parts.length == 1) {
+			  // Public domain ? 
+				if (StringUtils.equals(parts[0], "publicdomain")) {
+					return new CreativeCommonsLicense(new String[] { "publicdomain" }, "", "");
+				} else {
+		  		// Without jurisdiction and version
+					return new CreativeCommonsLicense(parts[0].split("-"), "3.0", "");
+				}
+			} else if (parts.length == 2) {
+				// Without jurisdiction
+				return new CreativeCommonsLicense(parts[0].split("-"), parts[1], "");
+			} else if (parts.length == 3) {
+				// With jurisdiction
+				return new CreativeCommonsLicense(parts[0].split("-"), parts[1], parts[2]);
+			}
+		}
+		
+		return null;
+	}
+	
+	public static String createLicenseUrl(String[] properties, String version, String jurisdiction) {
+		CreativeCommonsLicense license = new CreativeCommonsLicense(properties, version, jurisdiction);
+		return license.getUrl();
+	}
+	
+	public static String createLicenseUrl(boolean attribution, boolean derivatives, boolean shareAlike, boolean commercial) {
+		List<String> properties = new ArrayList<String>();
+		if (attribution) {
+			properties.add("by");
+		}
+		
+		if (!derivatives) {
+			properties.add("nd");
+		}
+		
+		if (!commercial) {
+			properties.add("nc");
+		}
+		
+		if (shareAlike) {
+			properties.add("sa");
+		}
+
+		return createLicenseUrl(properties.toArray(new String[0]), null, null);
+	}
+}
