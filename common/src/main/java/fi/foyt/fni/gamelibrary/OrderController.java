ada47f47a39b12d8a89bb9005665448c26ae4b01
Antti Leppä <antti.leppa@foyt.fi>
2013-08-25 12:42:29 +0300

    ** Added customer info into createOrder method of OrderController ** Added methods to create and retrieve order items into OrderController ** Added method to mark order as "waiting for delivery" into OrderController

--- common/src/main/java/fi/foyt/fni/gamelibrary/OrderController.java
+++ common/src/main/java/fi/foyt/fni/gamelibrary/OrderController.java
@@ -2,15 +2,19 @@ package fi.foyt.fni.gamelibrary;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 import javax.ejb.Stateful;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
 import fi.foyt.fni.persistence.dao.gamelibrary.OrderDAO;
+import fi.foyt.fni.persistence.dao.gamelibrary.OrderItemDAO;
 import fi.foyt.fni.persistence.model.gamelibrary.Order;
+import fi.foyt.fni.persistence.model.gamelibrary.OrderItem;
 import fi.foyt.fni.persistence.model.gamelibrary.OrderStatus;
 import fi.foyt.fni.persistence.model.gamelibrary.PaymentMethod;
+import fi.foyt.fni.persistence.model.gamelibrary.Publication;
 import fi.foyt.fni.persistence.model.users.Address;
 import fi.foyt.fni.persistence.model.users.User;
 
@@ -23,11 +27,14 @@ public class OrderController implements Serializable {
 	@Inject
 	private OrderDAO orderDAO;
 	
+	@Inject
+	private OrderItemDAO orderItemDAO;
+	
 	/* Order */
 	
-	public Order createOrder(User customer, OrderStatus orderStatus, PaymentMethod paymentMethod, Double shippingCosts, String notes, Address deliveryAddress) {
+	public Order createOrder(User customer, String customerCompany, String customerEmail, String customerFirstName, String customerLastName, String customerMobile, String customerPhone, OrderStatus orderStatus, PaymentMethod paymentMethod, Double shippingCosts, String notes, Address deliveryAddress) {
 		Date now = new Date();
-		return orderDAO.create(customer, orderStatus, paymentMethod, shippingCosts, notes, deliveryAddress, now, null, null, null);
+		return orderDAO.create(customer, customerCompany, customerEmail, customerFirstName, customerLastName, customerMobile, customerPhone, orderStatus, paymentMethod, shippingCosts, notes, deliveryAddress, now, null, null, null, null);
 	}
 
 	// TODO: Security...
@@ -41,9 +48,26 @@ public class OrderController implements Serializable {
 		return orderDAO.updateOrderStatus(orderDAO.updatePaid(order, now), OrderStatus.PAID);
 	}
 
+  // TODO: Security...
+	public Order updateOrderAsWaitingForDelivery(Order order) {
+		Date now = new Date();
+		return orderDAO.updateOrderStatus(orderDAO.updatePaid(order, now), OrderStatus.WAITING_FOR_DELIVERY);
+	}
+	
+  // TODO: Security...
 	public Order updateOrderAsCanceled(Order order) {
 		Date now = new Date();
 		return orderDAO.updateOrderStatus(orderDAO.updateCanceled(order, now), OrderStatus.CANCELED);
+	}
+	
+	/* OrderItem */
+	
+	public OrderItem createOrderItem(Order order, Publication publication, String name, Double unitPrice, Integer count) {
+		return orderItemDAO.create(order, publication, name, unitPrice, count);
+	}
+
+	public List<OrderItem> listOrderItems(Order order) {
+		return orderItemDAO.listByOrder(order);
 	}
 	
 }
