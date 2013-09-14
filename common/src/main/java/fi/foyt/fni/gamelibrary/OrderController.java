830baf0780830e2eb1251706381ae21d27f3bd13
Antti Leppä <antti.leppa@foyt.fi>
2013-09-14 18:03:51 +0300

    ** Added events for order life phase (create, paid, wait for delivery and canceled)

--- common/src/main/java/fi/foyt/fni/gamelibrary/OrderController.java
+++ common/src/main/java/fi/foyt/fni/gamelibrary/OrderController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.ejb.Stateful;
 import javax.enterprise.context.Dependent;
+import javax.enterprise.event.Event;
 import javax.inject.Inject;
 
 import fi.foyt.fni.persistence.dao.gamelibrary.OrderDAO;
@@ -17,7 +18,8 @@ import fi.foyt.fni.persistence.model.gamelibrary.PaymentMethod;
 import fi.foyt.fni.persistence.model.gamelibrary.Publication;
 import fi.foyt.fni.persistence.model.users.Address;
 import fi.foyt.fni.persistence.model.users.User;
-
+import fi.foyt.fni.session.SessionController;
+	
 @Dependent
 @Stateful
 public class OrderController implements Serializable {
@@ -25,39 +27,62 @@ public class OrderController implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Inject
+	private SessionController sessionController;
+
+	@Inject
+	@OrderCreated
+	private Event<OrderEvent> orderCreatedEvent;
+	
+	@Inject
+	@OrderPaid
+	private Event<OrderEvent> orderPaidEvent;
+
+	@Inject
+	@OrderWaitingForDelivery
+	private Event<OrderEvent> orderWaitingForDeliveryEvent;
+
+	@Inject
+	@OrderCanceled
+	private Event<OrderEvent> orderCanceledEvent;
+
+	@Inject
 	private OrderDAO orderDAO;
 	
 	@Inject
 	private OrderItemDAO orderItemDAO;
-	
+
 	/* Order */
 	
 	public Order createOrder(User customer, String customerCompany, String customerEmail, String customerFirstName, String customerLastName, String customerMobile, String customerPhone, OrderStatus orderStatus, PaymentMethod paymentMethod, Double shippingCosts, String notes, Address deliveryAddress) {
 		Date now = new Date();
-		return orderDAO.create(customer, customerCompany, customerEmail, customerFirstName, customerLastName, customerMobile, customerPhone, orderStatus, paymentMethod, shippingCosts, notes, deliveryAddress, now, null, null, null, null);
+		Order order = orderDAO.create(customer, customerCompany, customerEmail, customerFirstName, customerLastName, customerMobile, customerPhone, orderStatus, paymentMethod, shippingCosts, notes, deliveryAddress, now, null, null, null, null);
+		orderCreatedEvent.fire(new OrderEvent(sessionController.getLocale(), order.getId()));
+		return order;
 	}
 
-	// TODO: Security...
 	public Order findOrderById(Long orderId) {
 		return orderDAO.findById(orderId);
 	}
 
-  // TODO: Security...
 	public Order updateOrderAsPaid(Order order) {
 		Date now = new Date();
-		return orderDAO.updateOrderStatus(orderDAO.updatePaid(order, now), OrderStatus.PAID);
+		order = orderDAO.updateOrderStatus(orderDAO.updatePaid(order, now), OrderStatus.PAID);
+		orderPaidEvent.fire(new OrderEvent(sessionController.getLocale(), order.getId()));
+		return order;
 	}
 
-  // TODO: Security...
 	public Order updateOrderAsWaitingForDelivery(Order order) {
 		Date now = new Date();
-		return orderDAO.updateOrderStatus(orderDAO.updatePaid(order, now), OrderStatus.WAITING_FOR_DELIVERY);
+		order = orderDAO.updateOrderStatus(orderDAO.updatePaid(order, now), OrderStatus.WAITING_FOR_DELIVERY);
+		orderWaitingForDeliveryEvent.fire(new OrderEvent(sessionController.getLocale(), order.getId()));
+		return order;
 	}
 	
-  // TODO: Security...
 	public Order updateOrderAsCanceled(Order order) {
 		Date now = new Date();
-		return orderDAO.updateOrderStatus(orderDAO.updateCanceled(order, now), OrderStatus.CANCELED);
+		order = orderDAO.updateOrderStatus(orderDAO.updateCanceled(order, now), OrderStatus.CANCELED);
+		orderCanceledEvent.fire(new OrderEvent(sessionController.getLocale(), order.getId()));
+		return order;
 	}
 	
 	/* OrderItem */
---
+++ common/src/main/java/fi/foyt/fni/gamelibrary/OrderEvent.java
@@ -0,0 +1,30 @@
+package fi.foyt.fni.gamelibrary;
+
+import java.util.Locale;
+
+public class OrderEvent {
+	
+	public OrderEvent(Locale locale, Long orderId) {
+		this.locale = locale;
+		this.orderId = orderId;
+	}
+
+	public Long getOrderId() {
+		return orderId;
+	}
+	
+	public void setOrderId(Long orderId) {
+		this.orderId = orderId;
+	}
+	
+	public Locale getLocale() {
+		return locale;
+	}
+	
+	public void setLocale(Locale locale) {
+		this.locale = locale;
+	}
+	
+	private Locale locale;
+	private Long orderId;
+}
