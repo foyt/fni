1a5f84f2a1a31d56d1c12f4802ed3849348ce912
Antti Leppä <antti.leppa@foyt.fi>
2013-09-28 13:32:55 +0300

    ** Removed old menu.js

--- webapp/src/main/webapp/resources/scripts/gui/menu.js
+++ webapp/src/main/webapp/resources/scripts/gui/menu.js
@@ -1,30 +1,13 @@
 (function() {
   'use strict';
-  $(document).ready(function() {
-    $('#site-menu-bar-widget-locale').click(function (event) {
-      $(this).addClass('site-menu-bar-widget-active');
-      $('#site-menu-bar-widget-locale-content')
-        .addClass('site-menu-bar-widget-content-active')
-        .show();
-    });
+  
+  $(document).on('click', '.menu-tools-locale', function (e) {
+    $(this).closest('.menu-tools-locale-container').find('.menu-tools-locale-list').show();
     
-    $('#site-menu-bar-widget-account').click(function (event) {
-      $(this).addClass('site-menu-bar-widget-active');
-      $('#site-menu-bar-widget-account-content')
-        .addClass('site-menu-bar-widget-content-active')
-        .show();
-    });
-
-    $(document).mousedown(function (event) {
-      var element = event.target;
-      if ($(element).closest('.site-menu-bar-widget-content').length == 0) {
-        $('.site-menu-bar-widget-content-active')
-          .removeClass('site-menu-bar-widget-content-active')
-          .hide();
-        
-        $('.site-menu-bar-widget-active').removeClass('site-menu-bar-widget-active');
-      }
+    $(document).one('click', function (e) {
+      $('.menu-tools-locale-list').hide();
     });
   });
+
   
 }).call(this);
