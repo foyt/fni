e11b748710fbd68c39b610f0ccd8535052a0a1c7
Antti Leppä <antti.leppa@foyt.fi>
2013-09-29 15:55:20 +0300

    ** Changed gamelibrarypublications to use newly created gameLibraryPublicationsBackingBean instead of abstractPublicationListBackingBean

---
+++ webapp/src/main/java/fi/foyt/fni/view/gamelibrary/GameLibraryPublicationsBackingBean.java
@@ -0,0 +1,75 @@
+package fi.foyt.fni.view.gamelibrary;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateful;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import fi.foyt.fni.forum.ForumController;
+import fi.foyt.fni.gamelibrary.GameLibraryTagController;
+import fi.foyt.fni.gamelibrary.PublicationController;
+import fi.foyt.fni.gamelibrary.SessionShoppingCartController;
+import fi.foyt.fni.persistence.model.gamelibrary.GameLibraryTag;
+import fi.foyt.fni.persistence.model.gamelibrary.Publication;
+import fi.foyt.fni.persistence.model.gamelibrary.PublicationAuthor;
+import fi.foyt.fni.persistence.model.gamelibrary.PublicationTag;
+import fi.foyt.fni.persistence.model.users.User;
+
+@RequestScoped
+@Named
+@Stateful
+public class GameLibraryPublicationsBackingBean {
+
+	@Inject
+	private PublicationController publicationController;
+
+	@Inject
+	private GameLibraryTagController gameLibraryTagController;
+
+	@Inject
+	private ForumController forumController;
+
+	@Inject
+	private SessionShoppingCartController sessionShoppingCartController;
+
+	public boolean hasImages(Publication publication) {
+		return publicationController.listPublicationImagesByPublication(publication).size() > 0;
+	}
+
+	public List<GameLibraryTag> getTags(Publication publication) {
+		List<GameLibraryTag> result = new ArrayList<>();
+
+		List<PublicationTag> publicationTags = gameLibraryTagController.listPublicationTags(publication);
+		for (PublicationTag publicationTag : publicationTags) {
+			result.add(publicationTag.getTag());
+		}
+
+		return result;
+	}
+	
+	public List<User> getAuthors(Publication publication) {
+		List<User> result = new ArrayList<>(); 
+
+		List<PublicationAuthor> publicationAuthors = publicationController.listPublicationAuthors(publication);
+		for (PublicationAuthor publicationAuthor : publicationAuthors) {
+			result.add(publicationAuthor.getAuthor());
+		}
+		
+		return result;
+	}
+	
+	public Long getPublicationCommentCount(Publication publication) {
+		if (publication.getForumTopic() != null) {
+			return forumController.countPostsByTopic(publication.getForumTopic());
+		}
+		
+		return null;
+	}
+	
+	public void addPublicationToShoppingCart(Publication publication) {
+		sessionShoppingCartController.addPublication(publication);
+	}
+}
