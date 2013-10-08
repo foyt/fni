d88e74feb44b00fc890a04ed04ed25a993897d21
Antti Leppä <antti.leppa@foyt.fi>
2013-10-08 05:58:44 +0300

    ** Added pdf uploading support into game library management view

--- webapp/src/main/webapp/resources/scripts/gui/gamelibrary-manage.js
+++ webapp/src/main/webapp/resources/scripts/gui/gamelibrary-manage.js
@@ -32,9 +32,38 @@
             }
           }]
         });
-        
-        
-        
+      } else {
+        // TODO: Proper error handling...
+        alert(err);
+      }
+    }); 
+  });
+  
+  $(document).on('click', '.gamelibrary-manage-upload-file-link', function (event) {
+    dust.render("gamelibrary-file-upload", {
+      publicationId: $(this).data('publication-id')
+    }, function(err, html) {
+      if (!err) {
+        var dialog = $(html);
+        dialog.dialog({
+          modal: true,
+          width: 600,
+          buttons: [{
+            'text': dialog.data('upload-button'),
+            'click': function(event) { 
+              $(this).find('iframe').load($.proxy(function () {
+                window.location.reload();
+              }, this));
+              
+              $(this).find('form').submit();
+            }
+          }, {
+            'text': dialog.data('cancel-button'),
+            'click': function(event) { 
+              $(this).dialog("close");
+            }
+          }]
+        });
       } else {
         // TODO: Proper error handling...
         alert(err);
