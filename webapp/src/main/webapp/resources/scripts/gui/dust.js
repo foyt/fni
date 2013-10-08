fa87dfcf0dc1c5519d0a0c29063e4d2d1bf0d371
Antti Leppä <antti.leppa@foyt.fi>
2013-10-08 05:39:07 +0300

    ** Added contextPath helper for dustjs (products current context path)

--- webapp/src/main/webapp/resources/scripts/gui/dust.js
+++ webapp/src/main/webapp/resources/scripts/gui/dust.js
@@ -1,7 +1,16 @@
-dust.onLoad = function(name, callback) {
-  $.ajax(CONTEXTPATH + '/resources/dust/' + name + '-' + LOCALE + '.dust', {
-    success : function(data, textStatus, jqXHR) {
-      callback(false, data);
-    }
-  });
-};
+(function() {
+  'use strict';
+  
+  dust.onLoad = function(name, callback) {
+    $.ajax(CONTEXTPATH + '/resources/dust/' + name + '-' + LOCALE + '.dust', {
+      success : function(data, textStatus, jqXHR) {
+        callback(false, data);
+      }
+    });
+  };
+  
+  dust.helpers.contextPath = function(chunk, context, bodies) {
+    return chunk.write(CONTEXTPATH);
+  };
+  
+}).call(this);
