e96125cbd3914bcfc9995efbe2055e140740a875
Antti Leppä <antti.leppa@foyt.fi>
2013-10-08 05:39:56 +0300

    ** Added publication image upload dialog into publication management view

--- webapp/src/main/webapp/resources/scripts/gui/gamelibrary-manage.js
+++ webapp/src/main/webapp/resources/scripts/gui/gamelibrary-manage.js
@@ -6,5 +6,40 @@
       type: 'image'
     });
   });
+  
+  $(document).on('click', '.gamelibrary-manage-upload-image-link', function (event) {
+    dust.render("gamelibrary-image-upload", {
+      publicationId: $(this).data('publication-id')
+    }, function(err, html) {
+      if (!err) {
+        var dialog = $(html);
+        dialog.dialog({
+          modal: true,
+          width: 600,
+          buttons: [{
+            'text': dialog.data('upload-button'),
+            'click': function(event) { 
+              $(this).find('iframe').load($.proxy(function () {
+                window.location.reload();
+              }, this));
+              
+              $(this).find('form').submit();
+            }
+          }, {
+            'text': dialog.data('cancel-button'),
+            'click': function(event) { 
+              $(this).dialog("close");
+            }
+          }]
+        });
+        
+        
+        
+      } else {
+        // TODO: Proper error handling...
+        alert(err);
+      }
+    }); 
+  });
 
 }).call(this);
