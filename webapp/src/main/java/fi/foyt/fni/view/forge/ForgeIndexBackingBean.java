334abcd5a62b13f0c364dbe1550552392220dab1
Antti Leppä <antti.leppa@foyt.fi>
2013-10-12 13:14:05 +0300

    ** Added support for opening and closing of materialists in Forge

--- webapp/src/main/java/fi/foyt/fni/view/forge/ForgeIndexBackingBean.java
+++ webapp/src/main/java/fi/foyt/fni/view/forge/ForgeIndexBackingBean.java
@@ -24,13 +24,7 @@ import fi.foyt.fni.session.SessionController;
 @RequestScoped
 @Named
 @Stateful
-@URLMappings(mappings = {
-  @URLMapping(
-	  id = "forge-index", 
-		pattern = "/forge/", 
-		viewId = "/forge/index.jsf"
-  )
-})
+@URLMappings(mappings = { @URLMapping(id = "forge-index", pattern = "/forge/", viewId = "/forge/index.jsf") })
 public class ForgeIndexBackingBean {
 
 	private static final int MAX_LAST_VIEWED_MATERIALS = 5;
@@ -38,51 +32,54 @@ public class ForgeIndexBackingBean {
 
 	@Inject
 	private MaterialController materialController;
-	
+
 	@Inject
 	private SessionController sessionController;
-	
+
 	@PostConstruct
 	public void init() {
-		
 	}
-	
+
 	@URLAction
 	@LoggedIn
 	public void load() {
 		folderId = null;
+		materialsOpen = false;
+		lastViewedOpen = true;
+		lastEditedOpen = true;
+		starredOpen = true;
 	}
-	
+
 	public List<Material> getMaterials() {
 		Folder folder = null;
-		
+
 		List<Material> materials = materialController.listMaterialsByFolder(sessionController.getLoggedUser(), folder);
-    Collections.sort(materials, new TitleComparator());
-    
-    return materials;
+		Collections.sort(materials, new TitleComparator());
+
+		return materials;
 	}
-	
+
 	public List<Material> getLastViewedMaterials() {
-    return materialController.listViewedMaterialsByUser(sessionController.getLoggedUser(), 0, MAX_LAST_VIEWED_MATERIALS);
+		return materialController.listViewedMaterialsByUser(sessionController.getLoggedUser(), 0, MAX_LAST_VIEWED_MATERIALS);
 	}
-	
+
 	public List<Material> getLastEditedMaterials() {
 		return materialController.listModifiedMaterialsByUser(sessionController.getLoggedUser(), 0, MAX_LAST_EDITED_MATERIALS);
 	}
-	
+
 	public List<Material> getStarredMaterials() {
 		return materialController.listStarredMaterialsByUser(sessionController.getLoggedUser());
 	}
-	
+
 	public boolean isStarred(Long materialId) {
 		Material material = materialController.findMaterialById(materialId);
 		if (material != null) {
 			return materialController.isStarred(sessionController.getLoggedUser(), material);
 		}
-		
+
 		return false;
 	}
-	
+
 	public void starMaterial(Long materialId) {
 		Material material = materialController.findMaterialById(materialId);
 		if (material != null) {
@@ -99,5 +96,81 @@ public class ForgeIndexBackingBean {
 		}
 	}
 
+	public Long getFolderId() {
+		return folderId;
+	}
+
+	public void setFolderId(Long folderId) {
+		this.folderId = folderId;
+	}
+
+	public boolean isMaterialsOpen() {
+		return materialsOpen;
+	}
+
+	public void setMaterialsOpen(boolean materialsOpen) {
+		this.materialsOpen = materialsOpen;
+	}
+
+	public boolean isLastViewedOpen() {
+		return lastViewedOpen;
+	}
+
+	public void setLastViewedOpen(boolean lastViewedOpen) {
+		this.lastViewedOpen = lastViewedOpen;
+	}
+
+	public boolean isStarredOpen() {
+		return starredOpen;
+	}
+
+	public void setStarredOpen(boolean starredOpen) {
+		this.starredOpen = starredOpen;
+	}
+
+	public boolean isLastEditedOpen() {
+		return lastEditedOpen;
+	}
+
+	public void setLastEditedOpen(boolean lastEditedOpen) {
+		this.lastEditedOpen = lastEditedOpen;
+	}
+
+	public void closeMaterialsList() {
+		setMaterialsOpen(false);
+	}
+
+	public void openMaterialsList() {
+		setMaterialsOpen(true);
+	}
+
+	public void closeLastViewedList() {
+		setLastViewedOpen(false);
+	}
+
+	public void openLastViewedList() {
+		setLastViewedOpen(true);
+	}
+
+	public void closeLastEditedList() {
+		setLastEditedOpen(false);
+	}
+
+	public void openLastEditedList() {
+		setLastEditedOpen(true);
+	}
+
+	public void closeStarredList() {
+		setStarredOpen(false);
+	}
+
+	public void openStarredList() {
+		setStarredOpen(true);
+	}
+
 	private Long folderId;
+	private boolean materialsOpen;
+	private boolean lastViewedOpen;
+	private boolean starredOpen;
+	private boolean lastEditedOpen;
 }
