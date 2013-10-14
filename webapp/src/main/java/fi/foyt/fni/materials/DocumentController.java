e39516a5cddcddd800ae6b15c78982f57a699e5e
Antti Leppä <antti.leppa@foyt.fi>
2013-10-14 06:03:48 +0300

    ** Added methods for creating and retriving document versions into Document Controller

--- webapp/src/main/java/fi/foyt/fni/materials/DocumentController.java
+++ webapp/src/main/java/fi/foyt/fni/materials/DocumentController.java
@@ -1,5 +1,9 @@
 package fi.foyt.fni.materials;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.ejb.Stateful;
@@ -9,6 +13,8 @@ import javax.inject.Inject;
 import fi.foyt.fni.persistence.dao.materials.DocumentDAO;
 import fi.foyt.fni.persistence.dao.materials.DocumentRevisionDAO;
 import fi.foyt.fni.persistence.model.materials.Document;
+import fi.foyt.fni.persistence.model.materials.DocumentRevision;
+import fi.foyt.fni.persistence.model.users.User;
 
 @Dependent
 @Stateful
@@ -23,12 +29,41 @@ public class DocumentController {
   @Inject
   private DocumentRevisionDAO documentRevisionDAO;
   
+  /* Document */
+  
 	public Document findDocumentById(Long documentId) {
 		return documentDAO.findById(documentId);
 	}
 
+	public Document updateDocumentData(Document document, String data, User user) {
+		return documentDAO.updateData(document, user, data);
+	}
+	
+	/* Revisions */
+	
+	public DocumentRevision createDocumentRevision(Document document, Long revisionNumber, Date created, boolean compressed, boolean completeVersion, byte[] revisionBytes, String checksum) {
+	  return documentRevisionDAO.create(document, revisionNumber, created, compressed, completeVersion, revisionBytes, checksum, null, null);
+	}
+
+	public List<DocumentRevision> listDocumentRevisionsAfter(Document document, Long revisionNumber) {
+		List<DocumentRevision> documentRevisions = documentRevisionDAO.listByDocumentAndRevisionGreaterThan(document, revisionNumber);
+    Collections.sort(documentRevisions, new Comparator<DocumentRevision>() {
+      @Override
+      public int compare(DocumentRevision documentRevision1, DocumentRevision documentRevision2) {
+        return documentRevision1.getRevision().compareTo(documentRevision2.getRevision());
+      }
+    });
+    
+    return documentRevisions;
+	}
+
 	public Long getDocumentRevision(Document document) {
-		return documentRevisionDAO.maxRevisionByDocument(document);
+		Long result = documentRevisionDAO.maxRevisionByDocument(document);
+		if (result == null) {
+			result = 0l;
+		}
+		
+		return result;
 	}
 	
 }
