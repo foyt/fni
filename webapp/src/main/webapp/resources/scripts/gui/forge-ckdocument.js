bc532d04f8d831b12d8e2769f80a8bb359ccedf6
Antti Leppä <antti.leppa@foyt.fi>
2013-10-16 17:17:28 +0300

    ** Changed CKEditor content width to 210mm (a4) with 1inch margin ** Removed extra buttons from CKEditor toolbar

--- webapp/src/main/webapp/resources/scripts/gui/forge-ckdocument.js
+++ webapp/src/main/webapp/resources/scripts/gui/forge-ckdocument.js
@@ -10,12 +10,13 @@
     CKEDITOR.plugins.addExternal('coops-dmp', PLUGIN_DIR + '/coops-dmp/');
     CKEDITOR.plugins.addExternal('coops-ws', PLUGIN_DIR + '/coops-ws/');
     CKEDITOR.plugins.addExternal('mrmonkey', PLUGIN_DIR + '/mrmonkey/');
-  
+    
     var editor = CKEDITOR.replace($('.forge-ckdocument-editor').attr('name'), { 
       skin: 'moono',
       extraPlugins: 'coops,coops-rest,coops-ws,coops-dmp,mrmonkey',
       readOnly: true,
       height: 500,
+      contentsCss: ['//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.2/contents.css', CONTEXTPATH + '/forge/ckcontents.css' ],
       coops: {
         serverUrl: COOPS_SERVER_URL,
         websocket: {
@@ -24,9 +25,23 @@
           cursorBlinks: true,
           cursorBlinkInterval: 1.2
         }
-      }
+      },
+      toolbar: [
+        { name: 'document', items : [ 'Templates' ] },
+        { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+        { name: 'editing', items : [ 'Find','Replace','-','SelectAll','-', 'Scayt' ] },
+        { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote', ,'-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+        { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+        { name: 'insert', items : [ 'Image', 'Table','HorizontalRule', 'SpecialChar','PageBreak' ] },
+        { name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
+        { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+        { name: 'colors', items : [ 'TextColor','BGColor' ] },
+        { name: 'tools', items : [ 'ShowBlocks', 'Maximize' ] }
+      ]
     });
-
+    
+    /* CoOps status messages */
+    
     editor.on("CoOPS:SessionStart", function (event) {
       $('.forge-ckdocument-editor-status>span').css('display', 'none');
       $('.forge-ckdocument-editor-status-loaded').css('display', 'block');
