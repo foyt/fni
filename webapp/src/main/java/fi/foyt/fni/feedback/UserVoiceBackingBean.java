58d7acf8b25ee6496b32f822bad1b43095b496fd
Antti Leppä <antti.leppa@foyt.fi>
2013-10-19 17:16:17 +0300

    ** Added user voice widget

---
+++ webapp/src/main/java/fi/foyt/fni/feedback/UserVoiceBackingBean.java
@@ -0,0 +1,36 @@
+package fi.foyt.fni.feedback;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateful;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.commons.lang3.StringUtils;
+
+import fi.foyt.fni.persistence.model.system.SystemSettingKey;
+import fi.foyt.fni.system.SystemSettingsController;
+
+@RequestScoped
+@Named
+@Stateful
+public class UserVoiceBackingBean {
+
+	@Inject
+	private SystemSettingsController systemSettingsController;
+	
+	@PostConstruct
+	public void init() {
+		clientKey = systemSettingsController.getSetting(SystemSettingKey.USERVOICE_CLIENT_KEY);
+	}
+	
+	public String getClientKey() {
+		return clientKey;
+	}
+
+	public boolean getHasUserVoice() {
+		return StringUtils.isNotBlank(getClientKey());
+	}
+
+	private String clientKey;
+}
