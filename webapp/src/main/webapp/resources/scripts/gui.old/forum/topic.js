7dc2e3867df88104ed768eb0775d3a0a385f19d0
Antti Leppä <antti.leppa@foyt.fi>
2013-10-20 15:13:01 +0300

    ** Removed deprecated forum xhtml and js files

--- webapp/src/main/webapp/resources/scripts/gui.old/forum/topic.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  $(document).ready(function() {
-    CKEDITOR.replaceClass = 'forum-post-editor';
-    CKEDITOR.replaceAll( function( textarea, config ) {
-      config.toolbar = 'Forum';
-    });
-  });
-}).call(this);
