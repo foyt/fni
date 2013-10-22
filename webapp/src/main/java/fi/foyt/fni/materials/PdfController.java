97067262ef744e26c4fe214fd9b31c4d78c30393
Antti Leppä <antti.leppa@foyt.fi>
2013-10-22 05:52:42 +0300

    ** Added PdfController

---
+++ webapp/src/main/java/fi/foyt/fni/materials/PdfController.java
@@ -0,0 +1,38 @@
+package fi.foyt.fni.materials;
+
+import java.util.Date;
+import java.util.logging.Logger;
+
+import javax.ejb.Stateful;
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
+
+import fi.foyt.fni.persistence.dao.materials.PdfDAO;
+import fi.foyt.fni.persistence.model.common.Language;
+import fi.foyt.fni.persistence.model.materials.Folder;
+import fi.foyt.fni.persistence.model.materials.MaterialPublicity;
+import fi.foyt.fni.persistence.model.materials.Pdf;
+import fi.foyt.fni.persistence.model.users.User;
+
+@Dependent
+@Stateful
+public class PdfController {
+	
+	@Inject
+	private Logger logger;
+
+	@Inject
+  private PdfDAO pdfDAO;
+  
+  /* Pdf */
+
+	public Pdf createPdf(User creator, Language language, Folder parentFolder, String urlName, String title, byte[] data) {
+		Date now = new Date();
+		return pdfDAO.create(creator, now, creator, now, language, parentFolder, urlName, title, data, MaterialPublicity.PRIVATE);
+	}
+
+	public Pdf findPdfById(Long pdfId) {
+		return pdfDAO.findById(pdfId);
+	}
+
+}
