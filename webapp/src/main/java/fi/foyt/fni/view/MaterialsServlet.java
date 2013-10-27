6d3fe82c50cc9455cfdb161acac21a76ee513960
Antti Leppä <antti.leppa@foyt.fi>
2013-10-27 09:35:27 +0200

    ** Reimplemented materials servlet without rest api

--- webapp/src/main/java/fi/foyt/fni/view/MaterialsServlet.java
+++ webapp/src/main/java/fi/foyt/fni/view/MaterialsServlet.java
@@ -1,24 +1,53 @@
 package fi.foyt.fni.view;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.security.GeneralSecurityException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.scribe.model.Response;
+
+import com.google.api.services.drive.Drive;
+
+import fi.foyt.fni.drive.DriveManager;
+import fi.foyt.fni.dropbox.DropboxManager;
 import fi.foyt.fni.materials.MaterialController;
 import fi.foyt.fni.materials.MaterialPermissionController;
+import fi.foyt.fni.persistence.model.materials.Binary;
+import fi.foyt.fni.persistence.model.materials.Document;
+import fi.foyt.fni.persistence.model.materials.DropboxFile;
+import fi.foyt.fni.persistence.model.materials.File;
+import fi.foyt.fni.persistence.model.materials.GoogleDocument;
+import fi.foyt.fni.persistence.model.materials.Image;
 import fi.foyt.fni.persistence.model.materials.Material;
+import fi.foyt.fni.persistence.model.materials.Pdf;
+import fi.foyt.fni.persistence.model.materials.UbuntuOneFile;
+import fi.foyt.fni.persistence.model.materials.VectorImage;
 import fi.foyt.fni.persistence.model.users.User;
 import fi.foyt.fni.session.SessionController;
+import fi.foyt.fni.ubuntuone.UbuntuOneManager;
+import fi.foyt.fni.utils.data.FileData;
+import fi.foyt.fni.utils.data.TypedData;
 import fi.foyt.fni.utils.servlet.RequestUtils;
 
 @WebServlet ( urlPatterns = "/materials/*")
-public class MaterialsServlet extends AbstractViewServlet {
+public class MaterialsServlet extends AbstractTransactionedServlet {
+	
+	private static final long serialVersionUID = -5739692573670665390L;
 
-	private static final long serialVersionUID = -7538319095786895858L;
+	@Inject
+	private Logger logger;
 	
   @Inject
   private SessionController sessionController;
@@ -28,58 +57,179 @@ public class MaterialsServlet extends AbstractViewServlet {
 
   @Inject
 	private MaterialController materialController;
+
+	@Inject
+	private DropboxManager dropboxManager;
+
+	@Inject
+	private UbuntuOneManager ubuntuOneManager;
+
+	@Inject
+	private DriveManager driveManager;
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		Material material = materialController.findMaterialByCompletePath(RequestUtils.stripCtxPath(request.getContextPath(), request.getRequestURI()));
 		if (material == null) {
-    	handleNotFound(request, response);
-      return;
+			response.sendError(HttpServletResponse.SC_NOT_FOUND);
+			return;
     }
 
     User loggedUser = sessionController.getLoggedUser();
     if (!(materialPermissionController.isPublic(loggedUser, material) || materialPermissionController.hasAccessPermission(loggedUser, material))) {
-    	handleForbidden(request, response, sessionController.isLoggedIn());
+			response.sendError(HttpServletResponse.SC_FORBIDDEN);
+			return;
     }
 
-    try {
-			switch (material.getType()) {
-				case IMAGE:
-					handleInclude(request, response, "/v1/materials/images/" + material.getId());
-				break;
-				case DOCUMENT:
-					handleInclude(request, response, "/v1/materials/documents/" + material.getId());
-				break;
-				case VECTOR_IMAGE:
-					handleInclude(request, response, "/v1/materials/vectorImages/" + material.getId());
-				break;
-				case PDF:
-					handleInclude(request, response, "/v1/materials/pdfs/" + material.getId());
-				break;
-				case FILE:
-					handleInclude(request, response, "/v1/materials/files/" + material.getId());
-				break;
-				case GOOGLE_DOCUMENT:
-					handleInclude(request, response, "/v1/materials/googleDocuments/" + material.getId());
-				break;
-				case DROPBOX_FILE:
-					handleInclude(request, response, "/v1/materials/dropbox/" + material.getId());
-				break;
-				case UBUNTU_ONE_FILE:
-					handleInclude(request, response, "/v1/materials/ubuntuOne/" + material.getId());
-				break;
-				default:
-					throw new ViewControllerException(Locales.getText(request.getLocale(), "generic.error.internalError"));
-			}
-    } catch (IOException e) {
-    	throw new ViewControllerException(Locales.getText(request.getLocale(), "generic.error.apiCommunicationError"));
-    } catch (ServletException e) {
-    	throw new ViewControllerException(Locales.getText(request.getLocale(), "generic.error.configurationError"));
+    FileData data = null;
+    
+		switch (material.getType()) {
+			case IMAGE:
+				data = getBinaryMaterialData((Image) material);
+			break;
+			case DOCUMENT:
+				data = getDocumentData((Document) material);
+			break;
+			case VECTOR_IMAGE:
+				data = getVectorImageData((VectorImage) material);
+			break;
+			case PDF:
+				data = getBinaryMaterialData((Pdf) material);
+			break;
+			case FILE:
+				data = getBinaryMaterialData((File) material);
+			break;
+			case GOOGLE_DOCUMENT:
+				try {
+					data = getGoogleDocument((GoogleDocument) material);
+				} catch (GeneralSecurityException e) {
+					logger.log(Level.SEVERE, "Error occured while retrieving GoogleDocument data", e);
+					response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		  		return;
+				}
+			break;
+			case DROPBOX_FILE:
+				data = getDropboxMaterialData((DropboxFile) material);
+			break;
+			case UBUNTU_ONE_FILE:
+				data = getUbuntuOneMaterialData((UbuntuOneFile) material);
+			break;
+			case BINARY:
+				data = getBinaryMaterialData((Binary) material);
+			case DROPBOX_FOLDER:
+			case DROPBOX_ROOT_FOLDER:
+			case FOLDER:
+			case UBUNTU_ONE_FOLDER:
+			case UBUNTU_ONE_ROOT_FOLDER:
+				data = null;
+  		break;
+		}
+		
+    if (data == null) {
+  		response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+  		return;
     }
+    
+    if (StringUtils.isNotBlank(data.getFileName())) {
+			response.setHeader("content-disposition", "attachment; filename=" + data.getFileName());
+		}
+		
+		response.setContentType(data.getContentType());
+
+		ServletOutputStream outputStream = response.getOutputStream();
+		try {
+			outputStream.write(data.getData());
+		} finally {
+			outputStream.flush();
+			outputStream.close();
+		}
 	}
 
-	private void handleInclude(HttpServletRequest request, HttpServletResponse response, String includeUrl) throws ServletException, IOException {
-		request.getRequestDispatcher(includeUrl).forward(request, response);
+	private FileData getDocumentData(Document document) throws UnsupportedEncodingException {
+		return new FileData(null, null, document.getData().getBytes("UTF-8"), "text/html", document.getModified());
+	}
+
+	private FileData getVectorImageData(VectorImage vectorImage) throws UnsupportedEncodingException {
+		return new FileData(null, null, vectorImage.getData().getBytes("UTF-8"), "image/svg+xml", vectorImage.getModified());
+	}
+
+	private FileData getGoogleDocument(GoogleDocument googleDocument) throws IOException, GeneralSecurityException {
+		Drive systemDrive = driveManager.getSystemDrive();
+		com.google.api.services.drive.model.File file = driveManager.getFile(systemDrive, googleDocument.getDocumentId());
+		TypedData typedData = null;
+		String fileName = null;
+		
+		switch (googleDocument.getDocumentType()) {
+			case DOCUMENT:
+				typedData = driveManager.exportFile(systemDrive, file, "text/html");
+			break;
+			case DRAWING:
+				typedData = driveManager.exportFile(systemDrive, file, "image/png");
+			break;
+			case PRESENTATION:
+				typedData = driveManager.exportFile(systemDrive, file, "application/pdf");
+				fileName = googleDocument.getUrlName();
+			break;
+			case SPREADSHEET:
+				typedData = driveManager.exportSpreadsheet(systemDrive, file);
+			break;
+			case FOLDER:
+			break;
+			case FILE:
+				typedData = driveManager.downloadFile(systemDrive, file);
+				fileName = googleDocument.getUrlName();
+			break;
+		}
+		
+		return new FileData(null, fileName, typedData.getData(), typedData.getContentType(), typedData.getModified());
+	}
+
+	private FileData getBinaryMaterialData(Binary binary) {
+		return new FileData(null, binary.getUrlName(), binary.getData(), binary.getContentType(), binary.getModified());
+	}
+	
+	private FileData getDropboxMaterialData(DropboxFile dropboxFile) throws IOException {
+		Response response = dropboxManager.getFileContent(sessionController.getLoggedUser(), dropboxFile);
+		if (response.getCode() == 200) {
+			byte[] data = null;
+			
+			InputStream inputStream = response.getStream();
+			try {
+				data = IOUtils.toByteArray(inputStream);
+				String fileName = null;
+				if (!dropboxFile.getMimeType().startsWith("image/")&&!dropboxFile.getMimeType().equals("text/html")) {
+					fileName = dropboxFile.getUrlName();
+				}
+				
+				return new FileData(null, fileName, data, dropboxFile.getMimeType(), dropboxFile.getModified());
+			} finally {
+			  inputStream.close();
+			}
+		}
+		
+		return null;
+	}
+	
+	private FileData getUbuntuOneMaterialData(UbuntuOneFile ubuntuOneFile) throws IOException {
+		Response response = ubuntuOneManager.getFileContent(sessionController.getLoggedUser(), ubuntuOneFile);
+		if (response.getCode() == 200) {
+			byte[] data = null;
+			
+			InputStream inputStream = response.getStream();
+			try {
+				data = IOUtils.toByteArray(inputStream);
+				String fileName = null;
+				if (!ubuntuOneFile.getMimeType().startsWith("image/")&&!ubuntuOneFile.getMimeType().equals("text/html")) {
+					fileName = ubuntuOneFile.getUrlName();
+				}
+				
+				return new FileData(null, fileName, data, ubuntuOneFile.getMimeType(), ubuntuOneFile.getModified());
+			} finally {
+			  inputStream.close();
+			}
+		}
+		
+		return null;
 	}
 	
 }
