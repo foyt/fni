6b4756dec73155b420bd273e9aa936bd89efcc12
Antti Leppä <antti.leppa@foyt.fi>
2013-10-30 17:59:07 +0200

    ** Converted error pages into new layout

--- webapp/src/main/java/fi/foyt/fni/jsf/ExceptionHandler.java
+++ webapp/src/main/java/fi/foyt/fni/jsf/ExceptionHandler.java
@@ -11,13 +11,14 @@ import javax.ejb.EJBException;
 import javax.el.ELException;
 import javax.enterprise.inject.CreationException;
 import javax.faces.FacesException;
+import javax.faces.application.ViewHandler;
+import javax.faces.component.UIViewRoot;
 import javax.faces.context.ExceptionHandlerWrapper;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ExceptionQueuedEvent;
 import javax.faces.event.ExceptionQueuedEventContext;
 import javax.inject.Inject;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
@@ -29,106 +30,84 @@ import fi.foyt.fni.security.ForbiddenException;
 import fi.foyt.fni.security.UnauthorizedException;
 
 public class ExceptionHandler extends ExceptionHandlerWrapper {
-	
-	@Inject
-	private Logger logger;
 
-	private final javax.faces.context.ExceptionHandler wrapped;
+  @Inject
+  private Logger logger;
 
-	public ExceptionHandler(final javax.faces.context.ExceptionHandler wrapped) {
-		this.wrapped = wrapped;
-	}
+  private final javax.faces.context.ExceptionHandler wrapped;
 
-	@Override
-	public javax.faces.context.ExceptionHandler getWrapped() {
-		return this.wrapped;
-	}
+  public ExceptionHandler(final javax.faces.context.ExceptionHandler wrapped) {
+    this.wrapped = wrapped;
+  }
 
-	@Override
-	public void handle() throws FacesException {
-		for (final Iterator<ExceptionQueuedEvent> queuedEventIterator = getUnhandledExceptionQueuedEvents().iterator(); queuedEventIterator.hasNext();) {
-			ExceptionQueuedEvent queuedEvent = queuedEventIterator.next();
-			ExceptionQueuedEventContext queuedEventContext = queuedEvent.getContext();
+  @Override
+  public javax.faces.context.ExceptionHandler getWrapped() {
+    return this.wrapped;
+  }
 
-			Throwable exception = queuedEventContext.getException();
-			while ((exception instanceof FacesException || 
-							exception instanceof EJBException || 
-							exception instanceof ELException ||  
-							exception instanceof PrettyException || 
-							exception instanceof CreationException || 
-							exception instanceof IllegalStateException) && 
-							exception.getCause() != null) {
-				exception = exception.getCause();
-			}
-			
-			FacesContext facesContext = FacesContext.getCurrentInstance();
-			ExternalContext externalContext = facesContext.getExternalContext();
-			
-			try {
-  			if (exception instanceof UnauthorizedException) {
-  				// User tried to perform an operation that required authenticated user but was not logged in.
-  				// Redirecting user into login form
-  				try {
-  					String redirectUrl = null;
-  					
-  					PrettyContext prettyContext = PrettyContext.getCurrentInstance();
-  					if (prettyContext != null) {
-  					  redirectUrl = new StringBuilder(externalContext.getRequestContextPath())
-  					    .append(prettyContext.getRequestURL().toString())
-  					    .toString();
-  					}
+  @Override
+  public void handle() throws FacesException {
+    for (final Iterator<ExceptionQueuedEvent> queuedEventIterator = getUnhandledExceptionQueuedEvents().iterator(); queuedEventIterator.hasNext();) {
+      ExceptionQueuedEvent queuedEvent = queuedEventIterator.next();
+      ExceptionQueuedEventContext queuedEventContext = queuedEvent.getContext();
 
-  					StringBuilder redirectBuilder = new StringBuilder()
-    				  .append(externalContext.getRequestContextPath())
-    				  .append("/login/");
-  					
-  					if (StringUtils.isNotBlank(redirectUrl)) {
-  						redirectBuilder.append("?redirectUrl=" + URLEncoder.encode(redirectUrl, "UTF-8"));
-  					}
-  					
-  					externalContext.redirect(redirectBuilder.toString());
-  				} catch (IOException e) {
-  					logger.log(Level.SEVERE, "Error occurred while redirecting to login page", e);
-  				}
-  			} else if (exception instanceof ForbiddenException) {
-  			  // TODO: JSP -> JSF
-  				externalContext.setResponseStatus(HttpServletResponse.SC_NOT_FOUND);
-  				externalContext.setResponseContentType("text/html; charset=utf-8");
-					try {
-						externalContext.dispatch("/jsp/generic/error-403.jsp");
-					} catch (IOException e) {
-  					logger.log(Level.SEVERE, "Error occurred while redirecting to forbidden error page", e);
-					}
-  			} else if (exception instanceof FileNotFoundException) {
-  				// TODO: JSP -> JSF
-  				externalContext.setResponseStatus(HttpServletResponse.SC_NOT_FOUND);
-  				externalContext.setResponseContentType("text/html; charset=utf-8");
-					try {
-						externalContext.dispatch("/jsp/generic/error-404.jsp");
-					} catch (IOException e) {
-  					logger.log(Level.SEVERE, "Error occurred while redirecting to not found error page", e);
-					}
-  			} else {
-  				// TODO: JSP -> JSF
-  				HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
-  				externalContext.setResponseStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-  				externalContext.setResponseContentType("text/html; charset=utf-8");
-  				request.setAttribute("cause", exception);
-  				try {
-						externalContext.dispatch("/jsp/generic/error-500.jsp");
-					} catch (IOException e) {
-  					logger.log(Level.SEVERE, "Error occurred while redirecting to internal error page", e);
-					}
-  				
-  				exception.printStackTrace();
-  			}
+      Throwable exception = queuedEventContext.getException();
+      while ((exception instanceof FacesException || exception instanceof EJBException || exception instanceof ELException
+          || exception instanceof PrettyException || exception instanceof CreationException || exception instanceof IllegalStateException)
+          && exception.getCause() != null) {
+        exception = exception.getCause();
+      }
 
-  			facesContext.responseComplete();
-			} finally {
-				queuedEventIterator.remove();
-	    }
-		}
-		
-		getWrapped().handle();
-	}
+      FacesContext facesContext = FacesContext.getCurrentInstance();
+      ExternalContext externalContext = facesContext.getExternalContext();
+
+      try {
+        if (exception instanceof UnauthorizedException) {
+          // User tried to perform an operation that required authenticated user
+          // but was not logged in.
+          // Redirecting user into login form
+          try {
+            String redirectUrl = null;
+
+            PrettyContext prettyContext = PrettyContext.getCurrentInstance();
+            if (prettyContext != null) {
+              redirectUrl = new StringBuilder(externalContext.getRequestContextPath()).append(prettyContext.getRequestURL().toString()).toString();
+            }
+
+            StringBuilder redirectBuilder = new StringBuilder().append(externalContext.getRequestContextPath()).append("/login/");
+
+            if (StringUtils.isNotBlank(redirectUrl)) {
+              redirectBuilder.append("?redirectUrl=" + URLEncoder.encode(redirectUrl, "UTF-8"));
+            }
+
+            externalContext.redirect(redirectBuilder.toString());
+          } catch (IOException e) {
+            logger.log(Level.SEVERE, "Error occurred while redirecting to login page", e);
+          }
+        } else if (exception instanceof ForbiddenException) {
+          externalContext.setResponseStatus(HttpServletResponse.SC_FORBIDDEN);
+          renderView("/error/access-denied.jsf");
+        } else if (exception instanceof FileNotFoundException) {
+          externalContext.setResponseStatus(HttpServletResponse.SC_NOT_FOUND);
+          renderView("/error/not-found.jsf");
+        } else {
+          externalContext.setResponseStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+          renderView("/error/internal-error.jsf");
+          exception.printStackTrace();
+        }
+      } finally {
+        queuedEventIterator.remove();
+      }
+    }
+
+    getWrapped().handle();
+  }
+
+  private void renderView(String viewId) {
+    FacesContext facesContext = FacesContext.getCurrentInstance();
+    ViewHandler viewHandler = facesContext.getApplication().getViewHandler();
+    UIViewRoot viewRoot = viewHandler.createView(facesContext, viewId);
+    facesContext.setViewRoot(viewRoot);
+    facesContext.renderResponse();
+  }
 }
