6a1bd99727d29c895f6d5c35c515d1e8f166ab8d
Antti Leppä <antti.leppa@foyt.fi>
2013-11-01 08:05:55 +0200

    ** Added permission checks and client side caching into ForgeGoogleDrieServlet

--- webapp/src/main/java/fi/foyt/fni/view/forge/ForgeGoogleDriveServlet.java
+++ webapp/src/main/java/fi/foyt/fni/view/forge/ForgeGoogleDriveServlet.java
@@ -3,6 +3,7 @@ package fi.foyt.fni.view.forge;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.security.GeneralSecurityException;
+import java.util.Date;
 
 import javax.inject.Inject;
 import javax.servlet.ServletException;
@@ -15,17 +16,21 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 
 import fi.foyt.fni.materials.GoogleDriveMaterialController;
+import fi.foyt.fni.materials.MaterialPermissionController;
 import fi.foyt.fni.persistence.model.materials.GoogleDocument;
+import fi.foyt.fni.persistence.model.users.User;
 import fi.foyt.fni.session.SessionController;
 import fi.foyt.fni.users.UserController;
 import fi.foyt.fni.utils.data.TypedData;
+import fi.foyt.fni.utils.servlet.RequestUtils;
 import fi.foyt.fni.view.AbstractTransactionedServlet;
 
 @WebServlet(urlPatterns = "/forge/gdrive/*")
 public class ForgeGoogleDriveServlet extends AbstractTransactionedServlet {
 
 	private static final long serialVersionUID = -1L;
-	
+	private static final long DEFAULT_EXPIRE_TIME = 1000 * 60 * 60;
+
 	@Inject
 	private GoogleDriveMaterialController googleDriveMaterialController;
 	
@@ -35,11 +40,11 @@ public class ForgeGoogleDriveServlet extends AbstractTransactionedServlet {
 	@Inject
 	private SessionController sessionController;
 
+	@Inject
+  private MaterialPermissionController materialPermissionController;
+
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO: Security
-		// TODO: Caching
-		
 		String pathInfo = request.getPathInfo();
 		String googleDocumentIdStr = StringUtils.removeStart(pathInfo, "/");
 		if (!StringUtils.isNumeric(googleDocumentIdStr)) {
@@ -53,6 +58,28 @@ public class ForgeGoogleDriveServlet extends AbstractTransactionedServlet {
 			response.sendError(HttpServletResponse.SC_NOT_FOUND, "Not Found");
 			return;
 		}
+		
+		User loggedUser = sessionController.getLoggedUser();
+		if (!materialPermissionController.isPublic(loggedUser, googleDocument)) {
+		  if (loggedUser == null) {
+	      response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+	      return;
+	    }
+		  
+		  if (!materialPermissionController.hasAccessPermission(loggedUser, googleDocument)) {
+        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        return;
+		  }
+		}
+		
+    String eTag = createETag(googleDocument.getModified());
+		long lastModified = googleDocument.getModified().getTime();
+
+    if (!RequestUtils.isModifiedSince(request, lastModified, eTag)) {
+      response.setHeader("ETag", eTag); // Required in 304.
+      response.sendError(HttpServletResponse.SC_NOT_MODIFIED);
+      return;
+    }
 		
 		TypedData data;
 		try {
@@ -68,6 +95,10 @@ public class ForgeGoogleDriveServlet extends AbstractTransactionedServlet {
 		}
 
 		response.setContentType(data.getContentType());
+		response.setHeader("ETag", eTag);
+    response.setDateHeader("Last-Modified", lastModified);
+    response.setDateHeader("Expires", System.currentTimeMillis() + DEFAULT_EXPIRE_TIME);
+    
 		ServletOutputStream outputStream = response.getOutputStream();
 		try {
 			outputStream.write(data.getData());
@@ -76,4 +107,10 @@ public class ForgeGoogleDriveServlet extends AbstractTransactionedServlet {
 			outputStream.close();
 		}
 	}
+
+  private String createETag(Date modified) {
+    StringBuilder eTagBuilder = new StringBuilder();
+    eTagBuilder.append("W/").append(modified.getTime());
+    return eTagBuilder.toString();
+  }
 }
