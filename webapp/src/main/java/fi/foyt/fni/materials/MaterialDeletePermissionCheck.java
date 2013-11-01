3650c6e47933e8a946edc7e6d12b73fd392ab71f
Antti Leppä <antti.leppa@foyt.fi>
2013-11-01 08:34:41 +0200

    ** Added permission checks for MATERIAL_ACCESS, MATERIAL_MODIFY and MATERIAL_DELETE

---
+++ webapp/src/main/java/fi/foyt/fni/materials/MaterialDeletePermissionCheck.java
@@ -0,0 +1,47 @@
+package fi.foyt.fni.materials;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import fi.foyt.fni.persistence.model.materials.Material;
+import fi.foyt.fni.persistence.model.users.Permission;
+import fi.foyt.fni.persistence.model.users.User;
+import fi.foyt.fni.security.PermissionCheck;
+import fi.foyt.fni.security.PermissionCheckImplementation;
+import fi.foyt.fni.security.SecurityException;
+import fi.foyt.fni.session.SessionController;
+
+@Stateless
+@PermissionCheck (Permission.MATERIAL_DELETE)
+public class MaterialDeletePermissionCheck implements PermissionCheckImplementation<Long> {
+  
+  @Inject
+  private MaterialController materialController;
+
+  @Inject
+  private MaterialPermissionController materialPermissionController;
+
+  @Inject
+  private SessionController sessionController;
+
+	@Override
+	public boolean checkPermission(Long materialId) {
+	  Material material = null;
+	  
+	  if (!sessionController.isLoggedIn()) {
+	    return false;
+	  }
+	  
+	  if (materialId != null) {
+	    material = materialController.findMaterialById(materialId);
+	  }
+	  
+		if (material == null) {
+			throw new SecurityException("Could not resolve material while checking permission for MATERIAL_DELETE");
+		}
+		
+		User loggedUser = sessionController.getLoggedUser();
+		return materialPermissionController.hasModifyPermission(loggedUser, material);
+	}
+
+}
