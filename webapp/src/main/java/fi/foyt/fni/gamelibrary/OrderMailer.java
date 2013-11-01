25132de60a6cc808034188d0b86868896ec727ad
Antti Leppä <antti.leppa@foyt.fi>
2013-11-01 12:39:19 +0200

    ** Changed OrderMailer to send a copy of confirm mail for shop owner also

--- webapp/src/main/java/fi/foyt/fni/gamelibrary/OrderMailer.java
+++ webapp/src/main/java/fi/foyt/fni/gamelibrary/OrderMailer.java
@@ -35,56 +35,56 @@ public class OrderMailer {
 		if (event.getOrderId() != null) {
   		Order order = orderController.findOrderById(event.getOrderId()); 
   		if (order != null) {
-  			Locale locale = event.getLocale();
-  			String customerName = order.getCustomerFirstName() + ' ' + order.getCustomerLastName();
-  			String customerEmail = order.getCustomerEmail();
-  			String fromName = systemSettingsController.getSetting(SystemSettingKey.GAMELIBRARY_ORDERMAILER_NAME);
-  			String fromMail = systemSettingsController.getSetting(SystemSettingKey.GAMELIBRARY_ORDERMAILER_MAIL);
-  			
-  			List<OrderItem> items = orderController.listOrderItems(order);
-  			
-    		StringBuilder contentBuilder = new StringBuilder();
-    		contentBuilder.append(getLocalizedValue(locale, "gamelibrary.mail.contentGreeting", customerName, order.getId()));
-    		contentBuilder.append(getLocalizedValue(locale, "gamelibrary.mail.contentWaitingForDelivery"));
-    		    		
-    		if (order.getDeliveryAddress() != null) {
-    			String streetAddress = order.getDeliveryAddress().getStreet1();
-    			if (StringUtils.isNotBlank(order.getDeliveryAddress().getStreet2())) {
-    				streetAddress += '\n' + order.getDeliveryAddress().getStreet2();
-    			}
-    			contentBuilder.append(getLocalizedValue(locale, "gamelibrary.mail.contentAddress", streetAddress, order.getDeliveryAddress().getPostalCode() + ' ' + order.getDeliveryAddress().getCity(), order.getDeliveryAddress().getCountry().getName()));
-    		}
-    		
-    		Double totalCosts = 0d;
-    		
-    		StringBuilder itemsList = new StringBuilder();
-    		for (int i = 0, l = items.size(); i < l; i++) {
-    			OrderItem item = items.get(i);
-    			itemsList.append(item.getCount());
-    			itemsList.append(" x ");
-    			itemsList.append(item.getName());
-    			if (i < (l - 1)) {
-    				itemsList.append('\n');
-    			}
-    			
-    			totalCosts += item.getUnitPrice() * item.getCount();
-    		}
-    		
-    		totalCosts += order.getShippingCosts();
-    		NumberFormat currencyInstance = NumberFormat.getCurrencyInstance(locale);
-    		currencyInstance.setCurrency(Currency.getInstance("EUR"));
-    		String totalCostsFormatted = currencyInstance.format(totalCosts);
-    		
-  			contentBuilder.append(getLocalizedValue(locale, "gamelibrary.mail.contentProducts", itemsList.toString(), totalCostsFormatted));
-  			contentBuilder.append(getLocalizedValue(locale, "gamelibrary.mail.contentFooter"));
-  			
-  			String subject = getLocalizedValue(locale, "gamelibrary.mail.subjectWaitingForDelivery");
-  			
-  			try {
-					MailUtils.sendMail(fromMail, fromName, customerEmail, customerName, subject, contentBuilder.toString(), "text/plain");
-				} catch (MessagingException e) {
-					logger.log(Level.SEVERE, "Failed to send 'waiting for delivery' mail", e);
-				}
+  		  Locale locale = event.getLocale();
+  		  String customerName = order.getCustomerFirstName() + ' ' + order.getCustomerLastName();
+  	    String customerEmail = order.getCustomerEmail();
+  	    
+  		  List<OrderItem> items = orderController.listOrderItems(order);
+  	    
+  	    StringBuilder contentBuilder = new StringBuilder();
+  	    contentBuilder.append(getLocalizedValue(locale, "gamelibrary.mail.contentGreeting", customerName, order.getId()));
+  	    contentBuilder.append(getLocalizedValue(locale, "gamelibrary.mail.contentWaitingForDelivery"));
+  	            
+  	    if (order.getDeliveryAddress() != null) {
+  	      String streetAddress = order.getDeliveryAddress().getStreet1();
+  	      if (StringUtils.isNotBlank(order.getDeliveryAddress().getStreet2())) {
+  	        streetAddress += '\n' + order.getDeliveryAddress().getStreet2();
+  	      }
+  	      contentBuilder.append(getLocalizedValue(locale, "gamelibrary.mail.contentAddress", streetAddress, order.getDeliveryAddress().getPostalCode() + ' ' + order.getDeliveryAddress().getCity(), order.getDeliveryAddress().getCountry().getName()));
+  	    }
+  	    
+  	    Double totalCosts = 0d;
+  	    
+  	    StringBuilder itemsList = new StringBuilder();
+  	    for (int i = 0, l = items.size(); i < l; i++) {
+  	      OrderItem item = items.get(i);
+  	      itemsList.append(item.getCount());
+  	      itemsList.append(" x ");
+  	      itemsList.append(item.getName());
+  	      if (i < (l - 1)) {
+  	        itemsList.append('\n');
+  	      }
+  	      
+  	      totalCosts += item.getUnitPrice() * item.getCount();
+  	    }
+  	    
+  	    totalCosts += order.getShippingCosts();
+  	    NumberFormat currencyInstance = NumberFormat.getCurrencyInstance(locale);
+  	    currencyInstance.setCurrency(Currency.getInstance("EUR"));
+  	    String totalCostsFormatted = currencyInstance.format(totalCosts);
+  	    
+  	    contentBuilder.append(getLocalizedValue(locale, "gamelibrary.mail.contentProducts", itemsList.toString(), totalCostsFormatted));
+  	    contentBuilder.append(getLocalizedValue(locale, "gamelibrary.mail.contentFooter"));
+  	    
+  	    String subject = getLocalizedValue(locale, "gamelibrary.mail.subjectWaitingForDelivery");
+  		  String content = contentBuilder.toString();
+  	    
+  		  try {
+          notifyShopOwner("Fwd: " + subject, content);
+        } catch (MessagingException e) {
+          logger.severe("Failed to notify shop owner of mail payment");
+        }
+  			notifyCustomer(customerName, customerEmail, subject, content);
   		} else {
   			logger.severe("Tried to mail 'waiting for delivery' mail for non-existing order");
   		}
@@ -92,6 +92,26 @@ public class OrderMailer {
 			logger.severe("Tried to mail 'waiting for delivery' mail for non-existing orderId");
 		}
 	}
+
+  private void notifyCustomer(String customerName, String customerEmail, String subject, String content) {
+    String fromName = systemSettingsController.getSetting(SystemSettingKey.GAMELIBRARY_ORDERMAILER_NAME);
+    String fromMail = systemSettingsController.getSetting(SystemSettingKey.GAMELIBRARY_ORDERMAILER_MAIL);
+    
+    try {
+    	MailUtils.sendMail(fromMail, fromName, customerEmail, customerName, subject, content, "text/plain");
+    } catch (MessagingException e) {
+    	logger.log(Level.SEVERE, "Failed to send 'waiting for delivery' mail", e);
+    }
+  }
+  
+  private void notifyShopOwner(String subject, String content) throws MessagingException {
+    String shopOwnerName = systemSettingsController.getSetting(SystemSettingKey.GAMELIBRARY_SHOP_OWNER_NAME);
+    String shopOwnerEmail = systemSettingsController.getSetting(SystemSettingKey.GAMELIBRARY_SHOP_OWNER_MAIL);
+    String fromName = systemSettingsController.getSetting(SystemSettingKey.GAMELIBRARY_ORDERMAILER_NAME);
+    String fromMail = systemSettingsController.getSetting(SystemSettingKey.GAMELIBRARY_ORDERMAILER_MAIL);
+    
+    MailUtils.sendMail(fromMail, fromName, shopOwnerEmail, shopOwnerName, subject, content, "text/plain");
+  }
 
 	private String getLocalizedValue(Locale locale, String key, Object... params) {
 		return ExternalLocales.getText(locale, key, params);
