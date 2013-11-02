213fc0a9f7bae3e9143f3aa66d407f9dafa466a0
Antti Leppä <antti.leppa@foyt.fi>
2013-11-02 07:07:58 +0200

    ** Added image popup support into front page

---
+++ webapp/src/main/webapp/resources/scripts/gui/index.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+  
+  $(document).ready(function () {
+    $('.index-publication-image-link').magnificPopup({ 
+      type: 'image'
+    });
+  });
+
+}).call(this);
