67a71945da67ce08b6d8aeb64854daabd3a47a66
Antti Leppä <antti.leppa@foyt.fi>
2013-11-08 20:04:33 +0200

    ** Added users search functionality into user's profile page

--- webapp/src/main/webapp/resources/scripts/gui/users-profile.js
+++ webapp/src/main/webapp/resources/scripts/gui/users-profile.js
@@ -1,6 +1,54 @@
 (function() {
   'use strict';
   
+  function search() {
+    var input = $('.users-profile-search');
+    var query = $.trim(input.val());
+    if (query) {
+      if (input.data('searching') != true) {
+        input.data('searching', true);
+        $('.users-profile-search-loading').show();
+        $.ajax({
+          url : CONTEXTPATH + "/search/",
+          data : {
+            q: query,
+            source: 'USERS'
+          },
+          success : function(data) {
+            input.data('searching', false);
+            $('.users-profile-search-loading').hide();
+            
+            dust.render("users-search", data, function(err, html) {
+              if (!err) {
+                $('.users-profile-search-result').show().html(html);
+              } else {
+                // TODO: Proper error handling...
+                alert(err);
+              }
+            }); 
+            
+            if (query != $.trim(input.val())) {
+              search();
+            }
+          }
+        });
+      }
+    } else {
+      $('.users-profile-search-result').hide();
+    }
+  }
+
+  $(document).on('keyup', '.users-profile-search', function (event) {
+    search();
+  });
+  
+  $(document).on('mouseup', function (event) {
+    var target = event.target;
+    if ($(target).closest('.users-profile-search-container').length == 0) {
+      $('.users-profile-search-result').hide(); 
+    }
+  });
+  
   $(document).ready(function () {
     $('.users-profile-gamelibrary-publication-image-link').magnificPopup({ 
       type: 'image'
