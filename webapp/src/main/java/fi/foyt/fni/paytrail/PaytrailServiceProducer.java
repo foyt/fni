2c21ddf72f0c3729f5dc9fd1559fd1f57dc00125
Antti Leppä <antti.leppa@foyt.fi>
2013-11-15 05:46:04 +0200

    ** Changed paytrail merchant id and merchant secret as system settings

--- webapp/src/main/java/fi/foyt/fni/paytrail/PaytrailServiceProducer.java
+++ webapp/src/main/java/fi/foyt/fni/paytrail/PaytrailServiceProducer.java
@@ -2,19 +2,27 @@ package fi.foyt.fni.paytrail;
 
 import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
 
+import fi.foyt.fni.persistence.model.system.SystemSettingKey;
+import fi.foyt.fni.system.SystemSettingsController;
 import fi.foyt.paytrail.PaytrailService;
 import fi.foyt.paytrail.io.IOHandler;
 import fi.foyt.paytrail.json.Marshaller;
 
 public class PaytrailServiceProducer {
+  
+  @Inject
+  private SystemSettingsController systemSettingsController;
 
 	@Dependent
 	@Produces
 	public PaytrailService producePaytrailService() {
 		IOHandler ioHandler = new HttpClientIOHandler();
 		Marshaller marshaller = new JacksonMarshaller();
-		PaytrailService paytrailService = new PaytrailService(ioHandler, marshaller, "13466", "6pKF4jkv97zmqBJ3ZL8gUw5DfT2NMQ");
+    String merchantId = systemSettingsController.getSetting(SystemSettingKey.PAYTRAIL_MERCHANT_ID);
+    String merchantSecret = systemSettingsController.getSetting(SystemSettingKey.PAYTRAIL_MERCHANT_SECRET);
+    PaytrailService paytrailService = new PaytrailService(ioHandler, marshaller, merchantId, merchantSecret);
 		return paytrailService;
 	}
 	
