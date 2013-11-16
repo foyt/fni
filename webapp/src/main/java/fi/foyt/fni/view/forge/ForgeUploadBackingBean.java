6bada8e03681ea1a58fb573b103e2f94ba51a27e
Antti Leppä <antti.leppa@foyt.fi>
2013-11-16 09:46:43 +0200

    ** Added parentFolderId parameter into forge upload link

--- webapp/src/main/java/fi/foyt/fni/view/forge/ForgeUploadBackingBean.java
+++ webapp/src/main/java/fi/foyt/fni/view/forge/ForgeUploadBackingBean.java
@@ -4,13 +4,20 @@ import java.io.FileNotFoundException;
 
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.ocpsoft.pretty.faces.annotation.URLAction;
 import com.ocpsoft.pretty.faces.annotation.URLMapping;
 import com.ocpsoft.pretty.faces.annotation.URLMappings;
+import com.ocpsoft.pretty.faces.annotation.URLQueryParameter;
 
+import fi.foyt.fni.materials.FolderController;
+import fi.foyt.fni.materials.MaterialPermissionController;
+import fi.foyt.fni.persistence.model.materials.Folder;
+import fi.foyt.fni.security.ForbiddenException;
 import fi.foyt.fni.security.LoggedIn;
+import fi.foyt.fni.session.SessionController;
 
 @RequestScoped
 @Named
@@ -24,10 +31,38 @@ import fi.foyt.fni.security.LoggedIn;
 })
 public class ForgeUploadBackingBean {
 
+  @Inject
+  private FolderController folderController;
+
+  @Inject
+  private SessionController sessionController;
+
+  @Inject
+  private MaterialPermissionController materialPermissionController;
+
 	@URLAction
 	@LoggedIn
 	public void load() throws FileNotFoundException {
-
+    if (parentFolderId != null) {
+      Folder parentFolder = parentFolderId != null ? folderController.findFolderById(parentFolderId) : null;
+      if (parentFolder != null) {
+        if (!materialPermissionController.hasModifyPermission(sessionController.getLoggedUser(), parentFolder)) {
+          throw new ForbiddenException();
+        }
+      } else {
+        throw new FileNotFoundException();
+      }
+    }
 	}
+	
+	public Long getParentFolderId() {
+    return parentFolderId;
+  }
+	
+	public void setParentFolderId(Long parentFolderId) {
+    this.parentFolderId = parentFolderId;
+  }
 
+  @URLQueryParameter ("parentFolderId")
+  private Long parentFolderId;
 }
