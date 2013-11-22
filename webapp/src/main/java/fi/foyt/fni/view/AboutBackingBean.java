f3d8ecc8a60db102538e8299b89b84a216bbde3e
Antti Leppä <antti.leppa@foyt.fi>
2013-11-22 20:23:16 +0200

    ** Added added text about open source, contacting and thanks into about page

---
+++ webapp/src/main/java/fi/foyt/fni/view/AboutBackingBean.java
@@ -0,0 +1,69 @@
+package fi.foyt.fni.view;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateful;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.ocpsoft.pretty.faces.annotation.URLMapping;
+import com.ocpsoft.pretty.faces.annotation.URLMappings;
+
+import fi.foyt.fni.users.UserController;
+
+@RequestScoped
+@Named
+@Stateful
+@URLMappings (
+  mappings = @URLMapping (
+    id = "about",
+    pattern = "/about",
+    viewId = "/about.jsf"
+  )    
+)
+public class AboutBackingBean {
+  
+  @PostConstruct
+  public void init() {
+    this.pattern = Pattern.compile("\\[(.*?)\\]");
+  }
+  
+  @Inject
+  private UserController userController;
+	
+  public String getParsedText(String text) {
+    String contextPath = FacesContext.getCurrentInstance().getExternalContext().getRequestContextPath();
+    
+    Matcher matcher = pattern.matcher(text);
+    while (matcher.find()) {
+      String[] parts = matcher.group(1).split("\\|");
+      String href = null;
+      String linkText = null;
+      
+      if (parts.length == 1) {
+        href = linkText = parts[0];
+      } else {
+        linkText = parts[0];
+        href = parts[1];
+      }
+      
+      if (!(StringUtils.startsWith(href, "http://") || StringUtils.startsWith(href, "https://"))) {
+        href = contextPath + href;
+      }
+      
+      String link = String.format("<a href=\"%s\">%s</a>", href, linkText);
+      text = matcher.replaceFirst(link);
+      matcher = pattern.matcher(text);
+    }
+    
+    return text;
+  }
+
+  private Pattern pattern;
+}
