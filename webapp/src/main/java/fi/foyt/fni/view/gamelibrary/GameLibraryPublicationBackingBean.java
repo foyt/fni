f1dacc44293a826cfd2bbcbb03e4936ff0f3096e
Antti Leppä <antti.leppa@foyt.fi>
2013-11-27 17:54:31 +0200

    ** Fixed issue #64

--- webapp/src/main/java/fi/foyt/fni/view/gamelibrary/GameLibraryPublicationBackingBean.java
+++ webapp/src/main/java/fi/foyt/fni/view/gamelibrary/GameLibraryPublicationBackingBean.java
@@ -1,15 +1,22 @@
 package fi.foyt.fni.view.gamelibrary;
 
+import java.io.FileNotFoundException;
+
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.ocpsoft.pretty.faces.annotation.URLAction;
 import com.ocpsoft.pretty.faces.annotation.URLMapping;
 import com.ocpsoft.pretty.faces.annotation.URLMappings;
 
 import fi.foyt.fni.gamelibrary.PublicationController;
 import fi.foyt.fni.persistence.model.gamelibrary.Publication;
+import fi.foyt.fni.persistence.model.users.Role;
+import fi.foyt.fni.security.ForbiddenException;
+import fi.foyt.fni.security.UnauthorizedException;
+import fi.foyt.fni.session.SessionController;
 
 @RequestScoped
 @Named
@@ -23,11 +30,34 @@ import fi.foyt.fni.persistence.model.gamelibrary.Publication;
 })
 public class GameLibraryPublicationBackingBean {
   
+  @Inject
+  private SessionController sessionController;
+  
+  @URLAction
+  public void init() throws FileNotFoundException {
+    Publication publication = publicationController.findPublicationByUrlName(getUrlName());
+    if (publication == null) {
+      throw new FileNotFoundException();
+    }
+    
+    if (!publication.getPublished()) {
+      if (!sessionController.isLoggedIn()) {
+        throw new UnauthorizedException();
+      }
+      
+      if (!sessionController.hasLoggedUserRole(Role.GAME_LIBRARY_MANAGER)) {
+        throw new ForbiddenException();
+      }
+    }
+    
+    publicationId = publication.getId();
+  }
+  
 	@Inject
 	private PublicationController publicationController;
 	
 	public Publication getPublication() {
-		return publicationController.findPublicationByUrlName(getUrlName());
+		return publicationController.findPublicationById(publicationId);
 	}
 	
 	public String getUrlName() {
@@ -39,4 +69,6 @@ public class GameLibraryPublicationBackingBean {
 	}
 	
 	private String urlName;
+	
+	private Long publicationId;
 }
