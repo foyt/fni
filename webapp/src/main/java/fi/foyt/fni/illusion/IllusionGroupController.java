c4847684bc6e011caf11fe4018d352c8c1fbec0b
Antti Leppä <antti.leppa@foyt.fi>
2014-02-25 17:39:34 +0200

    ** Added Illusion group settings handling into IllusionGroupController

--- webapp/src/main/java/fi/foyt/fni/illusion/IllusionGroupController.java
+++ webapp/src/main/java/fi/foyt/fni/illusion/IllusionGroupController.java
@@ -1,24 +1,39 @@
 package fi.foyt.fni.illusion;
 
+import java.io.IOException;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.ejb.Stateless;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 
+import org.apache.commons.lang3.StringUtils;
+import org.codehaus.jackson.map.ObjectMapper;
+
 import fi.foyt.fni.persistence.dao.illusion.IllusionGroupDAO;
+import fi.foyt.fni.persistence.dao.illusion.IllusionGroupSettingDAO;
 import fi.foyt.fni.persistence.dao.illusion.IllusionGroupUserDAO;
 import fi.foyt.fni.persistence.dao.illusion.IllusionGroupUserImageDAO;
+import fi.foyt.fni.persistence.dao.illusion.IllusionGroupUserSettingDAO;
 import fi.foyt.fni.persistence.model.illusion.IllusionGroup;
+import fi.foyt.fni.persistence.model.illusion.IllusionGroupSetting;
+import fi.foyt.fni.persistence.model.illusion.IllusionGroupSettingKey;
 import fi.foyt.fni.persistence.model.illusion.IllusionGroupUser;
 import fi.foyt.fni.persistence.model.illusion.IllusionGroupUserImage;
 import fi.foyt.fni.persistence.model.illusion.IllusionGroupUserRole;
+import fi.foyt.fni.persistence.model.illusion.IllusionGroupUserSetting;
 import fi.foyt.fni.persistence.model.users.User;
 
 @Dependent
 @Stateless
 public class IllusionGroupController {
+  
+  @Inject
+  private Logger logger;
 
   @Inject
   private IllusionGroupDAO illusionGroupDAO;
@@ -28,6 +43,12 @@ public class IllusionGroupController {
 
   @Inject
   private IllusionGroupUserImageDAO illusionGroupUserImageDAO;
+  
+  @Inject
+  private IllusionGroupSettingDAO illusionGroupSettingDAO;
+  
+  @Inject
+  private IllusionGroupUserSettingDAO illusionGroupUserSettingDAO;
   
   /* IllusionGroup */
 
@@ -81,6 +102,46 @@ public class IllusionGroupController {
   
   public IllusionGroupUserImage updateIllusionGroupUserImage(IllusionGroupUserImage image, String contentType, byte[] data, Date modified) {
     return illusionGroupUserImageDAO.updateModified(illusionGroupUserImageDAO.updateContentType(illusionGroupUserImageDAO.updateData(image, data), contentType), modified);
+  }
+  
+  /* Settings */
+  
+  public String getIllusionGroupSettingValue(IllusionGroupUser user, IllusionGroupSettingKey key) {
+    IllusionGroupUserSetting userSetting = illusionGroupUserSettingDAO.findByUserAndKey(user, key);
+    if ((userSetting != null) && StringUtils.isNotBlank(userSetting.getValue())) {
+      return userSetting.getValue();
+    }
+    
+    IllusionGroupSetting groupSetting = illusionGroupSettingDAO.findByUserAndKey(user.getGroup(), key);
+    if (groupSetting != null) {
+      return groupSetting.getValue();
+    }
+    
+    return null;
+  }
+  
+  public <T> T getIllusionGroupSetting(IllusionGroupUser user, IllusionGroupSettingKey key, Class<T> clazz) {
+    String value = getIllusionGroupSettingValue(user, key);
+    if (StringUtils.isNotBlank(value)) {
+      ObjectMapper objectMapper = new ObjectMapper();
+      try {
+        return objectMapper.readValue(value, clazz);
+      } catch (IOException e) {
+        logger.log(Level.SEVERE, "Could not parse IllusionGroupSetting " + key + " from user: " + user.getId());
+      }
+    }
+    
+    return null;
+  }
+  
+  public List<String> getIllusionGroupDiceSetting(IllusionGroupUser user) {
+    @SuppressWarnings("unchecked")
+    List<String> result = getIllusionGroupSetting(user, IllusionGroupSettingKey.DICE, List.class);
+    if (result == null) {
+      result = Collections.emptyList();
+    }
+    
+    return result;
   }
   
 }
