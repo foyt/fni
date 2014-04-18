4b1c370834a82080840b611c1c7eaba9b429690b
Antti Leppä <antti.leppa@foyt.fi>
2014-04-18 21:08:40 +0300

    ** Converted pretty-config into url rewrite format

---
+++ webapp/src/main/java/fi/foyt/fni/jsf/DefaultsRewriteConfigurationProvider.java
@@ -0,0 +1,29 @@
+package fi.foyt.fni.jsf;
+
+import javax.servlet.ServletContext;
+
+import org.ocpsoft.rewrite.config.Configuration;
+import org.ocpsoft.rewrite.config.ConfigurationBuilder;
+import org.ocpsoft.rewrite.servlet.config.HttpConfigurationProvider;
+import org.ocpsoft.rewrite.servlet.config.rule.Join;
+
+public class DefaultsRewriteConfigurationProvider extends HttpConfigurationProvider {
+  
+  @Override
+  public Configuration getConfiguration(ServletContext context) {
+    ConfigurationBuilder configuration = ConfigurationBuilder.begin();
+
+    configuration
+      .addRule(Join.path("/login").to("/users/login.jsf"))
+      .addRule(Join.path("/forge").to("/forge/index.jsf"))
+      .addRule(Join.path("/gamelibrary").to("/gamelibrary/index.jsf"));
+
+    return configuration;
+  }
+
+  @Override
+  public int priority() {
+    return 0;
+  }
+  
+}
