34e6c5c636892194ae9b25237026fccc892ef02b
Antti Leppä <antti.leppa@foyt.fi>
2014-04-20 09:34:29 +0300

    ** Added web.xml error-page based generic error handling system

---
+++ webapp/src/main/java/fi/foyt/fni/view/error/ErrorNotFoundBackingBean.java
@@ -0,0 +1,21 @@
+package fi.foyt.fni.view.error;
+
+import java.io.IOException;
+
+import javax.ejb.Stateless;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpServletResponse;
+
+@Named
+@Stateless
+public class ErrorNotFoundBackingBean {
+
+  public void preRenderListener() throws IOException {
+    FacesContext facesContext = FacesContext.getCurrentInstance();
+    ExternalContext externalContext = facesContext.getExternalContext();
+    externalContext.setResponseStatus(HttpServletResponse.SC_NOT_FOUND);
+  }
+  
+}
