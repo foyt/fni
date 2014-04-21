fb603f9b5daf435faef0b5ccead4742da671a018
Antti Leppä <antti.leppa@foyt.fi>
2014-04-21 23:30:08 +0300

    ** Moved login required test into AbstractUITest class from ForgeIndexTestIT

--- webapp/src/test/java/fi/foyt/fni/test/ui/AbstractUITest.java
+++ webapp/src/test/java/fi/foyt/fni/test/ui/AbstractUITest.java
@@ -1,7 +1,25 @@
 package fi.foyt.fni.test.ui;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 import fi.foyt.fni.test.AbstractTest;
 
 public abstract class AbstractUITest extends AbstractTest {
+  
+  protected void testLoginRequired(RemoteWebDriver driver, String path) throws UnsupportedEncodingException {
+    try {
+      String appUrl = getAppUrl();
+      String ctxPath = getCtxPath();
+      driver.get(appUrl + path);
+      assertEquals(appUrl + "/login?redirectUrl=" + URLEncoder.encode("/" + ctxPath + path, "UTF-8"), driver.getCurrentUrl());
+    } finally {
+      driver.close();
+    }
+  }
   
 }
--- webapp/src/test/java/fi/foyt/fni/test/ui/ForgeIndexTestsIT.java
+++ webapp/src/test/java/fi/foyt/fni/test/ui/ForgeIndexTestsIT.java
@@ -1,9 +1,5 @@
 package fi.foyt.fni.test.ui;
 
-import static org.junit.Assert.assertEquals;
-
-import java.net.URLEncoder;
-
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -16,14 +12,7 @@ public class ForgeIndexTestsIT extends AbstractUITest {
 
   @Test
   public void testLoginRedirect() throws Exception {
-    try {
-      String appUrl = getAppUrl();
-      String ctxPath = getCtxPath();
-      driver.get(appUrl + "/forge/");
-      assertEquals(appUrl + "/login?redirectUrl=" + URLEncoder.encode("/" + ctxPath + "/forge/", "UTF-8"), driver.getCurrentUrl());
-    } finally {
-      driver.close();
-    }
+    testLoginRequired(driver, "/forge/");
   }
   
   private RemoteWebDriver driver; 
