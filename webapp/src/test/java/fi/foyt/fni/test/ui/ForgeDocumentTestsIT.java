4448f2f9a175d70353ccb7bc786018e0c47d477c
Antti Leppä <antti.leppa@foyt.fi>
2014-04-22 17:54:04 +0300

    ** Added forbidden test into ForgeDocumentTestIT

--- webapp/src/test/java/fi/foyt/fni/test/ui/ForgeDocumentTestsIT.java
+++ webapp/src/test/java/fi/foyt/fni/test/ui/ForgeDocumentTestsIT.java
@@ -7,7 +7,7 @@ public class ForgeDocumentTestsIT extends AbstractUITest {
  
   private static final String DOCUMENT_IN_ROOT = "/forge/documents/2/document";
   private static final String DOCUMENT_IN_FOLDER = "/forge/documents/2/folder/document_in_folder";
-  private static final String DOCUMENT_IN_SUBFOLDER = "/forge/documents/2/subfolder/document_in_folder";
+  private static final String DOCUMENT_IN_SUBFOLDER = "/forge/documents/2/folder/subfolder/document_in_subfolder";
     
   @Test
   public void testLoginRedirect() throws Exception {
@@ -16,6 +16,19 @@ public class ForgeDocumentTestsIT extends AbstractUITest {
       testLoginRequired(driver, DOCUMENT_IN_ROOT);
       testLoginRequired(driver, DOCUMENT_IN_FOLDER);
       testLoginRequired(driver, DOCUMENT_IN_SUBFOLDER);
+    } finally {
+      driver.close();
+    }
+  }
+  
+  @Test
+  public void testForbidden() throws Exception {
+    ChromeDriver driver = new ChromeDriver();
+    try {
+      loginInternal(driver, "guest@foyt.fi", "pass");
+      testAccessDenied(driver, DOCUMENT_IN_ROOT);
+      testAccessDenied(driver, DOCUMENT_IN_FOLDER);
+      testAccessDenied(driver, DOCUMENT_IN_SUBFOLDER);
     } finally {
       driver.close();
     }
