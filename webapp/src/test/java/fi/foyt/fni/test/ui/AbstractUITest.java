20a85d7cfd16a4d591fd57f2ab5020cfba0ab4b8
Antti Leppä <antti.leppa@foyt.fi>
2014-04-22 14:11:32 +0300

    ** Added helper method into AbstractUITest that can be used to login user with internal credentials ** Removed driver.close from testLoginRequired method in AbstractUITest

--- webapp/src/test/java/fi/foyt/fni/test/ui/AbstractUITest.java
+++ webapp/src/test/java/fi/foyt/fni/test/ui/AbstractUITest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertEquals;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
+import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import fi.foyt.fni.test.AbstractTest;
@@ -12,14 +14,24 @@ import fi.foyt.fni.test.AbstractTest;
 public abstract class AbstractUITest extends AbstractTest {
   
   protected void testLoginRequired(RemoteWebDriver driver, String path) throws UnsupportedEncodingException {
-    try {
-      String appUrl = getAppUrl();
-      String ctxPath = getCtxPath();
-      driver.get(appUrl + path);
-      assertEquals(appUrl + "/login?redirectUrl=" + URLEncoder.encode("/" + ctxPath + path, "UTF-8"), driver.getCurrentUrl());
-    } finally {
-      driver.close();
+    String appUrl = getAppUrl();
+    String ctxPath = getCtxPath();
+    driver.get(appUrl + path);
+    assertEquals(appUrl + "/login?redirectUrl=" + URLEncoder.encode("/" + ctxPath + path, "UTF-8"), driver.getCurrentUrl());
+  }
+  
+  protected void loginInternal(RemoteWebDriver driver, String email, String password) {
+    String loginUrl = getAppUrl() + "/login";
+    if (!StringUtils.startsWith(driver.getCurrentUrl(), loginUrl)) {
+      driver.get(loginUrl);
     }
+    
+    driver.findElement(By.cssSelector(".user-login-login-panel input[type='text']"))
+      .sendKeys(email);
+    driver.findElement(By.cssSelector(".user-login-login-panel input[type='password']"))
+      .sendKeys(password);
+    driver.findElement(By.cssSelector(".user-login-login-panel input[type='submit']"))
+      .click();
   }
   
 }
