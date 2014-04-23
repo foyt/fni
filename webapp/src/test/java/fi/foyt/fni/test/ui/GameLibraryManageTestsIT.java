5531c34730565721e9581c5df08ddefcdd554682
Antti Leppä <antti.leppa@foyt.fi>
2014-04-23 22:53:36 +0300

    ** Added selenium tests for game library management view

---
+++ webapp/src/test/java/fi/foyt/fni/test/ui/GameLibraryManageTestsIT.java
@@ -0,0 +1,56 @@
+package fi.foyt.fni.test.ui;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class GameLibraryManageTestsIT extends AbstractUITest {
+  
+  @Test
+  public void testLoginRedirect() throws Exception {
+    ChromeDriver driver = new ChromeDriver();
+    try {
+      testLoginRequired(driver, "/gamelibrary/manage/", true);
+    } finally {
+      driver.close();
+    }
+  }
+  
+  @Test
+  public void testUnauthorized() throws Exception {
+    ChromeDriver driver = new ChromeDriver();
+    try {
+      loginInternal(driver, "user@foyt.fi", "pass");
+      testAccessDenied(driver, "/gamelibrary/manage/", true);
+    } finally {
+      driver.close();
+    }
+  }
+  
+  @Test
+  public void testLibrarian() throws Exception {
+    ChromeDriver driver = new ChromeDriver();
+    try {
+      loginInternal(driver, "librarian@foyt.fi", "pass");
+      driver.get(getAppUrl(true) + "/gamelibrary/manage/");
+      assertEquals("Game Library - Management", driver.getTitle());
+    } finally {
+      driver.close();
+    }
+  }
+  
+  @Test
+  public void testAdmin() throws Exception {
+    ChromeDriver driver = new ChromeDriver();
+    try {
+      loginInternal(driver, "admin@foyt.fi", "pass");
+      driver.get(getAppUrl(true) + "/gamelibrary/manage/");
+      assertEquals("Game Library - Management", driver.getTitle());
+    } finally {
+      driver.close();
+    }
+  }
+  
+}
