72909957de8d36d669263d2e43f0cf5bfb984f76
Antti Leppä <antti.leppa@foyt.fi>
2014-04-23 15:43:34 +0300

    ** Added support for executing sql scripts into AbstractTest class

--- webapp/src/test/java/fi/foyt/fni/test/AbstractTest.java
+++ webapp/src/test/java/fi/foyt/fni/test/AbstractTest.java
@@ -1,5 +1,9 @@
 package fi.foyt.fni.test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 public abstract class AbstractTest {
   
   protected String getAppUrl() {
@@ -16,6 +20,34 @@ public abstract class AbstractTest {
 
   protected String getCtxPath() {
     return System.getProperty("it.ctx");
+  }
+  
+  protected Connection getConnection() throws Exception {
+    String username = System.getProperty("it.jdbc.username");
+    String password = System.getProperty("it.jdbc.password");
+    String url = System.getProperty("it.jdbc.url");
+    Class.forName(System.getProperty("it.jdbc.driver")).newInstance();
+    
+    return DriverManager.getConnection(url, username, password);
+  }
+  
+  protected void executeSql(String sql, Object... params) throws Exception {
+    Connection connection = getConnection();
+    try {
+      connection.setAutoCommit(true);
+      PreparedStatement statement = connection.prepareStatement(sql);
+      try {
+        for (int i = 0, l = params.length; i < l; i++) {
+          statement.setObject(i + 1, params[i]);
+        }
+        
+        statement.execute();
+      } finally {
+        statement.close();
+      }
+    } finally {
+      connection.close();
+    }
   }
 
 }
