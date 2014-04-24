1b0573dd5bdbe3d2d02bb16a925f4359ed82400d
Antti Leppä <antti.leppa@foyt.fi>
2014-04-24 07:13:01 +0300

    ** Added a helper method for testing page titles into AbstractUITest

--- webapp/src/test/java/fi/foyt/fni/test/ui/AbstractUITest.java
+++ webapp/src/test/java/fi/foyt/fni/test/ui/AbstractUITest.java
@@ -7,6 +7,7 @@ import java.net.URLEncoder;
 
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import fi.foyt.fni.test.AbstractTest;
@@ -36,6 +37,15 @@ public abstract class AbstractUITest extends AbstractTest {
       .sendKeys(password);
     driver.findElement(By.cssSelector(".user-login-login-panel input[type='submit']"))
       .click();
+  }
+    
+  protected void testTitle(ChromeDriver driver, String view, String expectedTitle) {
+    testTitle(driver, view, expectedTitle, false);
+  }
+
+  protected void testTitle(ChromeDriver driver, String view, String expectedTitle, boolean secure) {
+    driver.get(getAppUrl(secure) + view);
+    assertEquals(expectedTitle, driver.getTitle());
   }
 
   protected void testAccessDenied(RemoteWebDriver driver, String view) {
