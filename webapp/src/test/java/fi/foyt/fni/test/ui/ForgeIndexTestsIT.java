25fe0f8df37ad982ddc86216ccc07798e1d52efa
Antti Leppä <antti.leppa@foyt.fi>
2014-04-24 07:13:30 +0300

    ** Added basic title tests for Forge index and upload views

--- webapp/src/test/java/fi/foyt/fni/test/ui/ForgeIndexTestsIT.java
+++ webapp/src/test/java/fi/foyt/fni/test/ui/ForgeIndexTestsIT.java
@@ -4,6 +4,17 @@ import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ForgeIndexTestsIT extends AbstractUITest {
+  
+  @Test
+  public void testTitle() {
+    ChromeDriver driver = new ChromeDriver();
+    try {
+      loginInternal(driver, "user@foyt.fi", "pass");
+      testTitle(driver, "/forge/", "Forge");
+    } finally {
+      driver.close();
+    }
+  }
  
   @Test
   public void testLoginRedirect() throws Exception {
--- webapp/src/test/java/fi/foyt/fni/test/ui/ForgeUploadTestsIT.java
+++ webapp/src/test/java/fi/foyt/fni/test/ui/ForgeUploadTestsIT.java
@@ -6,6 +6,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ForgeUploadTestsIT extends AbstractUITest {
   
   @Test
+  public void testTitle() {
+    ChromeDriver driver = new ChromeDriver();
+    try {
+      loginInternal(driver, "user@foyt.fi", "pass");
+      testTitle(driver, "/forge/upload", "Forge - Import From My Computer");
+    } finally {
+      driver.close();
+    }
+  }
+  
+  @Test
   public void testLoginRedirect() throws Exception {
     ChromeDriver driver = new ChromeDriver();
     try {
