6b87eb4d8df03a7d1a5c383c9fb106c15aae2555
Antti Leppä <antti.leppa@foyt.fi>
2014-04-24 20:28:42 +0300

    ** Added test data for testing forum functionality

--- webapp/src/test/resources/data.sql
+++ webapp/src/test/resources/data.sql
@@ -82,3 +82,72 @@ values
   ('MAY_EDIT', 6, 4),
   ('MAY_VIEW', 9, 3),
   ('MAY_EDIT', 9, 4);
+  
+insert into 
+  ForumCategory (id, name)
+values 
+  (1, 'Category');
+  
+insert into 
+  Forum (id, allowTopicCreation, name, description, urlName, category_id)
+values 
+  (1, true, 'Empty Forum', 'Topicless forum', 'empty_forum', 1),
+  (2, true, 'Single topic Forum', 'Single topic forum', '1_topic_forum', 1),
+  (3, true, 'Five topic Forum', 'Five topic forum', '5_topic_forum', 1),
+  (4, false, 'Immutable Forum', 'No topic creation allowed Forum', 'immutable', 1);
+ 
+insert into 
+  ForumMessage (id, created, modified, views, author_id)
+values 
+  (1, PARSEDATETIME('1 1 2010 12:30', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2010 12:30', 'd M yyyy HH:mm'), 0, 1),
+  (2, PARSEDATETIME('1 1 2010 13:30', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2010 13:30', 'd M yyyy HH:mm'), 0, 1),
+  (3, PARSEDATETIME('1 1 2010 14:30', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2010 14:30', 'd M yyyy HH:mm'), 0, 1),
+  (4, PARSEDATETIME('1 1 2010 15:30', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2010 15:30', 'd M yyyy HH:mm'), 0, 1),
+  (5, PARSEDATETIME('1 1 2010 16:30', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2010 16:30', 'd M yyyy HH:mm'), 0, 1),
+  (6, PARSEDATETIME('1 1 2010 17:30', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2010 17:30', 'd M yyyy HH:mm'), 0, 1),
+  (7, PARSEDATETIME('1 1 2010 18:30', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2010 17:30', 'd M yyyy HH:mm'), 0, 1);
+  
+insert into 
+  ForumTopic (id, forum_id, urlName, subject) 
+values 
+  (1, 2, 'single_topic', 'Topic of single topic forum'),
+  (2, 3, 'topic1of5', 'Topic 1 of 5 topic forum'),
+  (3, 3, 'topic2of5', 'Topic 2 of 5 topic forum'),
+  (4, 3, 'topic3of5', 'Topic 3 of 5 topic forum'),
+  (5, 3, 'topic4of5', 'Topic 4 of 5 topic forum'),
+  (6, 3, 'topic5of5', 'Topic 5 of 5 topic forum'),
+  (7, 4, 'immutable_topic', 'Topic of immutable Forum topic');
+ 
+insert into 
+  ForumMessage (id, created, modified, views, author_id)
+values 
+  ( 8, PARSEDATETIME('1 1 2010 13:00', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2010 13:00', 'd M yyyy HH:mm'), 0, 1),
+  ( 9, PARSEDATETIME('1 1 2010 14:00', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2010 14:00', 'd M yyyy HH:mm'), 0, 1),
+  (10, PARSEDATETIME('1 1 2010 15:00', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2010 15:00', 'd M yyyy HH:mm'), 0, 1),
+  (11, PARSEDATETIME('1 1 2010 16:00', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2010 16:00', 'd M yyyy HH:mm'), 0, 1),
+  (12, PARSEDATETIME('1 1 2010 17:00', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2010 17:00', 'd M yyyy HH:mm'), 0, 1),
+  (13, PARSEDATETIME('1 1 2010 18:00', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2010 18:00', 'd M yyyy HH:mm'), 0, 1),
+  (14, PARSEDATETIME('1 1 2010 19:00', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2010 19:00', 'd M yyyy HH:mm'), 0, 1),
+  (15, PARSEDATETIME('1 1 2010 20:00', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2010 20:00', 'd M yyyy HH:mm'), 0, 1),
+  (16, PARSEDATETIME('1 1 2010 21:00', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2010 21:00', 'd M yyyy HH:mm'), 0, 1),
+  (17, PARSEDATETIME('1 1 2010 22:00', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2010 22:00', 'd M yyyy HH:mm'), 0, 1),
+  (18, PARSEDATETIME('1 1 2010 23:00', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2010 23:00', 'd M yyyy HH:mm'), 0, 1),
+  (19, PARSEDATETIME('1 1 2011 01:00', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2011 01:00', 'd M yyyy HH:mm'), 0, 1),
+  (20, PARSEDATETIME('1 1 2011 02:00', 'd M yyyy HH:mm'), PARSEDATETIME('1 1 2011 02:00', 'd M yyyy HH:mm'), 0, 1);
+  
+insert into 
+  ForumPost (id, topic_id, content)
+values 
+  ( 8, 1, '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu iaculis mi, et fringilla nulla. Integer ut imperdiet lacus. In in massa eget lacus tincidunt aliquet auctor ut enim. Praesent rutrum leo eu sem feugiat, non dignissim lorem vulputate. Nam fermentum euismod sapien, et cursus mi pretium quis.</p>'),
+  ( 9, 3, '<p>Replyless</p>'),
+  (10, 4, '<p>With one reply</p>'),
+  (11, 4, '<p>Re: With one reply</p>'),
+  (12, 5, '<p>With two replies</p>'),
+  (13, 5, '<p>Re: With two replies</p>'),
+  (14, 5, '<p>Re: With two replies</p>'),
+  (15, 6, '<p>With three replies</p>'),
+  (16, 6, '<p>Re: With three replies</p>'),
+  (17, 6, '<p>Re: With three replies</p>'),
+  (18, 6, '<p>Re: With three replies</p>'),
+  (19, 7, '<p>Immutable</p>'),
+  (20, 7, '<p>Re: Immutable</p>');
