5499242267e719bf72193780b5f65c1c5d183fc5
Antti Leppä <antti.leppa@foyt.fi>
2014-04-25 19:51:04 +0300

    ** Fixed menu tests

--- webapp/src/test/java/fi/foyt/fni/test/ui/MenuTestsIT.java
+++ webapp/src/test/java/fi/foyt/fni/test/ui/MenuTestsIT.java
@@ -28,9 +28,9 @@ public class MenuTestsIT extends AbstractUITest {
       assertEquals("Forum", forumMenuLink.getText());
 
       assertEquals(getAppUrl() + "/", stripLinkJSessionId(logoLink.getAttribute("href")));
-      assertEquals(getAppUrl() + "/forge/", stripLinkJSessionId(forgeMenuLink.getAttribute("href")));
+      assertEquals(getAppUrl() + "/forge", stripLinkJSessionId(forgeMenuLink.getAttribute("href")));
       assertEquals(getAppUrl() + "/illusion/", stripLinkJSessionId(illusionMenuLink.getAttribute("href")));
-      assertEquals(getAppUrl() + "/gamelibrary/", stripLinkJSessionId(gameLibraryMenuLink.getAttribute("href")));
+      assertEquals(getAppUrl() + "/gamelibrary", stripLinkJSessionId(gameLibraryMenuLink.getAttribute("href")));
       assertEquals(getAppUrl() + "/forum/", stripLinkJSessionId(forumMenuLink.getAttribute("href")));
     } finally {
       driver.close();
