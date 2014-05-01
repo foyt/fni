a2dc90cff466c7a07fd1ebe8106b73d27d3f81ab
Antti Leppä <antti.leppa@foyt.fi>
2014-05-01 17:13:37 +0300

    ** Added not found handling into Game Library tags view

--- webapp/src/main/java/fi/foyt/fni/view/gamelibrary/GameLibraryTagsBackingBean.java
+++ webapp/src/main/java/fi/foyt/fni/view/gamelibrary/GameLibraryTagsBackingBean.java
@@ -7,11 +7,15 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.apache.commons.lang3.StringUtils;
 import org.ocpsoft.rewrite.annotation.Join;
 import org.ocpsoft.rewrite.annotation.Matches;
 import org.ocpsoft.rewrite.annotation.Parameter;
+import org.ocpsoft.rewrite.annotation.RequestAction;
 
+import fi.foyt.fni.gamelibrary.GameLibraryTagController;
 import fi.foyt.fni.gamelibrary.PublicationController;
+import fi.foyt.fni.persistence.model.gamelibrary.GameLibraryTag;
 import fi.foyt.fni.persistence.model.gamelibrary.Publication;
 
 @RequestScoped
@@ -26,6 +30,23 @@ public class GameLibraryTagsBackingBean {
   
 	@Inject
 	private PublicationController publicationController;
+
+  @Inject
+	private GameLibraryTagController gameLibraryTagController;
+
+  @RequestAction
+  public String load() {
+    if (StringUtils.isBlank(tag)) {
+      return "/error/not-found.jsf"; 
+    }
+    
+    GameLibraryTag libraryTag = gameLibraryTagController.findTagByText(tag);
+    if (libraryTag == null) {
+      return "/error/not-found.jsf"; 
+    }
+    
+    return null;
+  }
 	
 	public List<Publication> getPublications() {
 		return publicationController.listPublicationsByPublishedAndTags(false, tag);
