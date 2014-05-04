283fd4f088006bec7094a10aaed448c92d35e548
Antti Leppä <antti.leppa@foyt.fi>
2014-05-04 18:49:38 +0300

    ** Added test for unicode garbage into IndexTestIT ** Added test for publication tags into IndexTestIT

--- webapp/src/test/java/fi/foyt/fni/test/ui/IndexTestsIT.java
+++ webapp/src/test/java/fi/foyt/fni/test/ui/IndexTestsIT.java
@@ -2,8 +2,12 @@ package fi.foyt.fni.test.ui;
 
 import static org.junit.Assert.assertEquals;
 
+import java.net.URLEncoder;
+import java.util.List;
+
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class IndexTestsIT extends AbstractUITest {
@@ -14,6 +18,16 @@ public class IndexTestsIT extends AbstractUITest {
     try {
       driver.get(getAppUrl() + "/");
       assertEquals("Forge & Illusion", driver.getTitle());
+    } finally {
+      driver.close();
+    }
+  }
+  
+  @Test
+  public void testUnicodeGarbage() {
+    ChromeDriver driver = new ChromeDriver();
+    try {
+      testNotFound(driver, "/å®‰è£…è¯´æ˜Ž.txt");
     } finally {
       driver.close();
     }
@@ -37,5 +51,19 @@ public class IndexTestsIT extends AbstractUITest {
       driver.close();
     }
   }
-  
+ 
+  @Test
+  public void testPublicationTags() throws Exception {
+    ChromeDriver driver = new ChromeDriver();
+    try {
+      driver.get(getAppUrl());
+      List<WebElement> tagLinks = driver.findElements(By.cssSelector(".index-publication-tag a"));
+      for (WebElement tagLink : tagLinks) {
+        String tag = tagLink.getText().toLowerCase();
+        assertEquals(getAppUrl() + "/gamelibrary/tags/" + URLEncoder.encode(tag, "UTF-8").replaceAll("\\+", "%20"), tagLink.getAttribute("href"));
+      }
+    } finally {
+      driver.close();
+    }
+  }
 }
