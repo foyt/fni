e3a6551a43c4c5c1416e979131ce37f78319be37
Antti Leppä <antti.leppa@foyt.fi>
2014-05-12 07:57:15 +0300

    ** Added Selenium tests for testing issue #82

--- webapp/src/test/java/fi/foyt/fni/test/ui/base/ForgeUploadTestsBase.java
+++ webapp/src/test/java/fi/foyt/fni/test/ui/base/ForgeUploadTestsBase.java
@@ -1,6 +1,12 @@
 package fi.foyt.fni.test.ui.base;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class ForgeUploadTestsBase extends AbstractUITest {
 
@@ -19,5 +25,31 @@ public class ForgeUploadTestsBase extends AbstractUITest {
   public void testNotFound() throws Exception {
     loginInternal(getWebDriver(), "user@foyt.fi", "pass");
     testNotFound(getWebDriver(), "/forge/upload?parentFolderId=12345");
+  }
+
+  @Test
+  public void testBreadcrumbs() {
+    loginInternal(getWebDriver(), "user@foyt.fi", "pass");
+    
+    getWebDriver().get(getAppUrl() + "/forge/upload");
+    List<WebElement> breadcrumps = getWebDriver().findElements(By.cssSelector(".view-header-navigation .view-header-navigation-item a"));
+    assertEquals(2, breadcrumps.size());
+    assertEquals(getAppUrl() + "/", breadcrumps.get(0).getAttribute("href"));
+    assertEquals(getAppUrl() + "/forge", breadcrumps.get(1).getAttribute("href"));
+    
+    getWebDriver().get(getAppUrl() + "/forge/upload?parentFolderId=1");
+    breadcrumps = getWebDriver().findElements(By.cssSelector(".view-header-navigation .view-header-navigation-item a"));
+    assertEquals(3, breadcrumps.size());
+    assertEquals(getAppUrl() + "/", breadcrumps.get(0).getAttribute("href"));
+    assertEquals(getAppUrl() + "/forge", breadcrumps.get(1).getAttribute("href"));
+    assertEquals(getAppUrl() + "/forge/folders/2/folder", breadcrumps.get(2).getAttribute("href"));
+    
+    getWebDriver().get(getAppUrl() + "/forge/upload?parentFolderId=2");
+    breadcrumps = getWebDriver().findElements(By.cssSelector(".view-header-navigation .view-header-navigation-item a"));
+    assertEquals(4, breadcrumps.size());
+    assertEquals(getAppUrl() + "/", breadcrumps.get(0).getAttribute("href"));
+    assertEquals(getAppUrl() + "/forge", breadcrumps.get(1).getAttribute("href"));
+    assertEquals(getAppUrl() + "/forge/folders/2/folder", breadcrumps.get(2).getAttribute("href"));
+    assertEquals(getAppUrl() + "/forge/folders/2/folder/subfolder", breadcrumps.get(3).getAttribute("href"));
   }
 }
