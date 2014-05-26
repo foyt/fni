7a9e4b8b38bc4fba00df5e9ff181b1991cead475
Antti Leppä <antti.leppa@foyt.fi>
2014-05-26 06:10:28 +0300

    ** Added selenium tests for testing login view registering

--- webapp/src/test/java/fi/foyt/fni/test/ui/base/LoginTestsBase.java
+++ webapp/src/test/java/fi/foyt/fni/test/ui/base/LoginTestsBase.java
@@ -1,11 +1,23 @@
 package fi.foyt.fni.test.ui.base;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import javax.mail.MessagingException;
+
+import org.apache.commons.lang3.StringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.GreenMailUtil;
 
 public class LoginTestsBase extends AbstractUITest {
-
+  
   @Before
   public void baseSetUp() throws Exception {
     createOAuthSettings();
@@ -34,6 +46,59 @@ public class LoginTestsBase extends AbstractUITest {
   @Test
   public void testGoogle() {
     loginGoogle(getWebDriver());
+  }
+
+  @Test
+  public void testRegisterMandatories() {
+    getWebDriver().get(getAppUrl(true) + "/login/");
+    getWebDriver().findElement(By.cssSelector(".user-register-button")).submit();
+    new WebDriverWait(getWebDriver(), 60).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".jsf-messages-container li:nth-child(1).error span")));
+    assertEquals("First Name Is Required", getWebDriver().findElement(By.cssSelector(".jsf-messages-container li:nth-child(1).error span")).getText());
+    assertEquals("Last Name Is Required", getWebDriver().findElement(By.cssSelector(".jsf-messages-container li:nth-child(2).error span")).getText());
+    assertEquals("Email Is Required", getWebDriver().findElement(By.cssSelector(".jsf-messages-container li:nth-child(3).error span")).getText());
+  }
+
+  @Test
+  public void testRegisterPasswordMismatch() {
+    getWebDriver().get(getAppUrl(true) + "/login/");
+    
+    getWebDriver().findElement(By.cssSelector(".user-register-first-name")).sendKeys("Ärri");
+    getWebDriver().findElement(By.cssSelector(".user-register-last-name")).sendKeys("Pörri");
+    getWebDriver().findElement(By.cssSelector(".user-register-email")).sendKeys("register.tester@foyt.fi");
+    getWebDriver().findElement(By.cssSelector(".user-register-password1")).sendKeys("qwe");
+    getWebDriver().findElement(By.cssSelector(".user-register-password2")).sendKeys("asd");
+    getWebDriver().findElement(By.cssSelector(".user-register-button")).click();
+    
+    new WebDriverWait(getWebDriver(), 60).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".jsf-messages-container li span")));
+    assertEquals("Passwords Do Not Match", getWebDriver().findElement(By.cssSelector(".jsf-messages-container li span")).getText());
+  }
+
+  @Test
+  public void testRegister() throws MessagingException {
+    acceptCookieDirective(getWebDriver());
+    
+    GreenMail greenMail = startSmtpServer();
+    try {
+      getWebDriver().get(getAppUrl(true) + "/login/");
+      
+      getWebDriver().findElement(By.cssSelector(".user-register-first-name")).sendKeys("Ärri");
+      getWebDriver().findElement(By.cssSelector(".user-register-last-name")).sendKeys("Pörri");
+      getWebDriver().findElement(By.cssSelector(".user-register-email")).sendKeys("register.tester@foyt.fi");
+      getWebDriver().findElement(By.cssSelector(".user-register-password1")).sendKeys("qwe");
+      getWebDriver().findElement(By.cssSelector(".user-register-password2")).sendKeys("qwe");
+      getWebDriver().findElement(By.cssSelector(".user-register-button")).click();
+      
+      new WebDriverWait(getWebDriver(), 60).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".jsf-messages-container li:nth-child(1).info span")));
+      String notification = getWebDriver().findElement(By.cssSelector(".jsf-messages-container li:nth-child(1).info span")).getText();
+      assertTrue(notification, StringUtils.startsWithIgnoreCase(notification, "Verification Email Has Been Sent"));
+      assertEquals(1, greenMail.getReceivedMessages().length);
+
+      String mailBody = GreenMailUtil.getBody(greenMail.getReceivedMessages()[0]);
+      assertEquals("Welcome to Forge & Illusion", greenMail.getReceivedMessages()[0].getSubject());
+      assertTrue(mailBody, StringUtils.startsWithIgnoreCase(mailBody, "Welcome to the world of"));
+    } finally {
+      greenMail.stop();
+    } 
   }
 
 }
