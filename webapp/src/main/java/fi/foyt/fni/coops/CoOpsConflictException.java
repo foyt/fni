206b9a286d940859470c55bea055c412045018a3
Antti Leppä <antti.leppa@foyt.fi>
2014-06-02 06:09:49 +0300

    ** Removed old CoOps server implementation

--- webapp/src/main/java/fi/foyt/fni/coops/CoOpsConflictException.java
+++
@@ -1,7 +0,0 @@
-package fi.foyt.fni.coops;
-
-public class CoOpsConflictException extends CoOpsException {
-
-  private static final long serialVersionUID = 1L;
-
-}
--- webapp/src/main/java/fi/foyt/fni/coops/CoOpsNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package fi.foyt.fni.coops;
-
-public class CoOpsNotFoundException extends CoOpsException {
-
-  private static final long serialVersionUID = 1L;
-
-}
