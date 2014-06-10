fabffd9f6325941be6a7cd50a08ce8e36858585b
Antti Leppä <antti.leppa@foyt.fi>
2014-06-10 18:34:56 +0300

    ** Added servlet context listener that closes zombie coops sessions when application starts

---
+++ webapp/src/main/java/fi/foyt/fni/coops/CoOpsSessionCloseServletContextListener.java
@@ -0,0 +1,29 @@
+package fi.foyt.fni.coops;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.transaction.Transactional;
+
+import fi.foyt.fni.persistence.model.materials.CoOpsSession;
+
+@WebListener
+public class CoOpsSessionCloseServletContextListener implements ServletContextListener {
+	
+	@Inject
+	private CoOpsSessionController coOpsSessionController;
+	
+	@Transactional
+  public void contextInitialized(ServletContextEvent event) {
+    List<CoOpsSession> openSessions = coOpsSessionController.listSessionsByClosed(Boolean.FALSE);
+    for (CoOpsSession openSession : openSessions) {
+      coOpsSessionController.closeSession(openSession, true);
+    }
+  }
+
+  public void contextDestroyed(ServletContextEvent event) {
+  }
+}
