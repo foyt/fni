773090e8f6ca5b4ce91f55b6fef7b163d78ec97f
Antti Leppä <antti.leppa@foyt.fi>
2014-06-19 22:55:24 +0300

    ** Changed logout to invalidate session instead of calling directly the SessionContoller logout method

--- webapp/src/main/java/fi/foyt/fni/view/users/LogoutBackingBean.java
+++ webapp/src/main/java/fi/foyt/fni/view/users/LogoutBackingBean.java
@@ -4,16 +4,12 @@ import java.io.IOException;
 
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
-import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.ocpsoft.rewrite.annotation.Join;
 import org.ocpsoft.rewrite.annotation.RequestAction;
 import org.ocpsoft.rewrite.faces.annotation.Deferred;
-
-import fi.foyt.fni.session.SessionController;
 
 @RequestScoped
 @Named
@@ -21,19 +17,11 @@ import fi.foyt.fni.session.SessionController;
 @Join ( path = "/logout", to = "/users/logout.jsf")
 public class LogoutBackingBean {
 	
-	@Inject
-  private SessionController sessionController;
-	
 	@RequestAction
 	@Deferred
-	public void load() throws IOException {
-		sessionController.logout();
-    
-		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-		externalContext.redirect(new StringBuilder()
-		  .append(externalContext.getRequestContextPath())
-		  .append("/")
-		  .toString());
+	public String load() throws IOException {
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		return "/index.jsf?faces-redirect=true";
 	}
 	
 }
