461b10defeea52b77b6d5143c707be9593d6ceff
Antti Leppä <antti.leppa@foyt.fi>
2014-06-28 18:21:47 +0300

    ** Removed pretty much everything from Illusion group and replaced the view with index document content

--- webapp/src/main/java/fi/foyt/fni/view/illusion/IllusionGroupBackingBean.java
+++ webapp/src/main/java/fi/foyt/fni/view/illusion/IllusionGroupBackingBean.java
@@ -1,9 +1,5 @@
 package fi.foyt.fni.view.illusion;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -12,20 +8,10 @@ import javax.inject.Named;
 import org.ocpsoft.rewrite.annotation.Join;
 import org.ocpsoft.rewrite.annotation.Parameter;
 import org.ocpsoft.rewrite.annotation.RequestAction;
-import org.ocpsoft.rewrite.faces.annotation.Deferred;
-import org.ocpsoft.rewrite.faces.annotation.IgnorePostback;
 
-import fi.foyt.fni.chat.ChatCredentialsController;
 import fi.foyt.fni.illusion.IllusionGroupController;
 import fi.foyt.fni.persistence.model.illusion.IllusionGroup;
-import fi.foyt.fni.persistence.model.illusion.IllusionGroupUser;
-import fi.foyt.fni.persistence.model.illusion.IllusionGroupUserRole;
-import fi.foyt.fni.persistence.model.system.SystemSettingKey;
-import fi.foyt.fni.persistence.model.users.User;
-import fi.foyt.fni.security.ForbiddenException;
 import fi.foyt.fni.security.LoggedIn;
-import fi.foyt.fni.session.SessionController;
-import fi.foyt.fni.system.SystemSettingsController;
 
 @RequestScoped
 @Named
@@ -36,46 +22,26 @@ public class IllusionGroupBackingBean {
 
   @Parameter
   private String urlName;
-  
-  @Inject
-  private SessionController sessionController;
-
-  @Inject
-  private SystemSettingsController systemSettingsController;
 
   @Inject
   private IllusionGroupController illusionGroupController;
-
-  @Inject
-  private ChatCredentialsController chatCredentialsController;
-
+  
   @RequestAction
-  @IgnorePostback
-  @Deferred
-  public void init() throws GeneralSecurityException, IOException {
+  public String init() {
     IllusionGroup illusionGroup = illusionGroupController.findIllusionGroupByUrlName(getUrlName());
     if (illusionGroup == null) {
-      throw new FileNotFoundException();
-    }
-
-    User loggedUser = sessionController.getLoggedUser();
-    IllusionGroupUser illusionGroupUser = illusionGroupController.findIllusionGroupUserByUserAndGroup(illusionGroup, loggedUser);
-    if (illusionGroupUser == null) {
-      throw new ForbiddenException();
+      return "/error/not-found.jsf";
     }
     
     id = illusionGroup.getId();
     name = illusionGroup.getName();
     description = illusionGroup.getDescription();
-    urlName = illusionGroup.getUrlName();
-    xmppRoom = illusionGroup.getXmppRoom();
-    userRole = illusionGroupUser.getRole();
-
-    userNickname = illusionGroupUser.getNickname();
-    xmppUserJid = chatCredentialsController.getUserJidByUser(loggedUser);
-    xmppPassword = chatCredentialsController.getPasswordByUser(loggedUser);
-    xmppBoshService = systemSettingsController.getSetting(SystemSettingKey.CHAT_BOSH_SERVICE);
-    chatBotJid = systemSettingsController.getSetting(SystemSettingKey.CHAT_BOT_JID);
+  
+    if (illusionGroup.getIndexDocument() != null) {
+      welcomeContent = illusionGroup.getIndexDocument().getData();
+    } 
+    
+    return null;
   }
 
   public String getUrlName() {
@@ -105,79 +71,13 @@ public class IllusionGroupBackingBean {
   public void setDescription(String description) {
     this.description = description;
   }
-
-  public String getXmppBoshService() {
-    return xmppBoshService;
-  }
-
-  public void setXmppBoshService(String xmppBoshService) {
-    this.xmppBoshService = xmppBoshService;
-  }
-
-  public String getXmppUserJid() {
-    return xmppUserJid;
-  }
-
-  public void setXmppUserJid(String xmppUserJid) {
-    this.xmppUserJid = xmppUserJid;
-  }
-
-  public String getXmppPassword() {
-    return xmppPassword;
-  }
-
-  public void setXmppPassword(String xmppPassword) {
-    this.xmppPassword = xmppPassword;
-  }
-
-  public String getXmppRoom() {
-    return xmppRoom;
-  }
-
-  public void setXmppRoom(String xmppRoom) {
-    this.xmppRoom = xmppRoom;
-  }
-
-  public String getUserNickname() {
-    return userNickname;
-  }
-
-  public void setUserNickname(String userNickname) {
-    this.userNickname = userNickname;
-  }
   
-  public String getChatBotJid() {
-    return chatBotJid;
-  }
-  
-  public IllusionGroupUserRole getUserRole() {
-    return userRole;
-  }
-
-  public String updateUserNickname() throws FileNotFoundException {
-    IllusionGroup illusionGroup = illusionGroupController.findIllusionGroupByUrlName(getUrlName());
-    if (illusionGroup == null) {
-      throw new FileNotFoundException();
-    }
-
-    User loggedUser = sessionController.getLoggedUser();
-    IllusionGroupUser illusionGroupUser = illusionGroupController.findIllusionGroupUserByUserAndGroup(illusionGroup, loggedUser);
-    if (illusionGroupUser == null) {
-      throw new ForbiddenException();
-    }
-    
-    illusionGroupController.updateIllusionGroupUserNickname(illusionGroupUser, getUserNickname());
-    return "pretty:illusion-group";
+  public String getWelcomeContent() {
+    return welcomeContent;
   }
   
   private Long id;
   private String name;
   private String description;
-  private String xmppBoshService;
-  private String xmppUserJid;
-  private String xmppPassword;
-  private String xmppRoom;
-  private String userNickname;
-  private String chatBotJid;
-  private IllusionGroupUserRole userRole;
+  private String welcomeContent;
 }
