dea6c92ee54fd6b3a8c71b4e2d5bb06e84e1aa72
Antti Leppä <antti.leppa@foyt.fi>
2014-07-01 14:01:29 +0300

    ** Generalized menu from illusion/group.xhtml into facelets/illusionmenu.xhtml ** Generalized common Illusion styles into illusion-group-common.scss ** Changed IllusionGroupBackingBean to use IllusionGroupDocument instead of indexText property

--- webapp/src/main/java/fi/foyt/fni/view/illusion/IllusionGroupBackingBean.java
+++ webapp/src/main/java/fi/foyt/fni/view/illusion/IllusionGroupBackingBean.java
@@ -1,5 +1,10 @@
 package fi.foyt.fni.view.illusion;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
@@ -10,8 +15,15 @@ import org.ocpsoft.rewrite.annotation.Parameter;
 import org.ocpsoft.rewrite.annotation.RequestAction;
 
 import fi.foyt.fni.illusion.IllusionGroupController;
+import fi.foyt.fni.materials.IllusionGroupDocumentController;
+import fi.foyt.fni.materials.MaterialController;
 import fi.foyt.fni.persistence.model.illusion.IllusionGroup;
+import fi.foyt.fni.persistence.model.materials.IllusionGroupDocument;
+import fi.foyt.fni.persistence.model.materials.IllusionGroupDocumentType;
+import fi.foyt.fni.persistence.model.materials.IllusionGroupFolder;
 import fi.foyt.fni.security.LoggedIn;
+import fi.foyt.fni.session.SessionController;
+import fi.foyt.fni.utils.data.FileData;
 
 @RequestScoped
 @Named
@@ -24,7 +36,19 @@ public class IllusionGroupBackingBean {
   private String urlName;
 
   @Inject
+  private Logger logger;
+
+  @Inject
   private IllusionGroupController illusionGroupController;
+
+  @Inject
+  private IllusionGroupDocumentController illusionGroupDocumentController;
+
+  @Inject
+  private MaterialController materialController;
+
+  @Inject
+  private SessionController sessionController;
   
   @RequestAction
   public String init() {
@@ -33,10 +57,22 @@ public class IllusionGroupBackingBean {
       return "/error/not-found.jsf";
     }
     
+    IllusionGroupFolder folder = illusionGroup.getFolder();
+    IllusionGroupDocument indexDocument = illusionGroupDocumentController.findByFolderAndDocumentType(folder, IllusionGroupDocumentType.INDEX);
+    if (indexDocument != null) {
+      try {
+        FileData indexData = materialController.getMaterialData(null, sessionController.getLoggedUser(), indexDocument);
+        if (indexData != null) {
+          indexText = new String(indexData.getData(), "UTF-8");
+        }
+      } catch (IOException | GeneralSecurityException e) {
+        logger.log(Level.WARNING, "Could not retreive group index text", e);
+      }
+    }
+    
     id = illusionGroup.getId();
     name = illusionGroup.getName();
     description = illusionGroup.getDescription();
-    indexText = illusionGroup.getIndexText();
   
     return null;
   }
