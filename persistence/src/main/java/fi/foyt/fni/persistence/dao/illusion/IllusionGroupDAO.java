326f197866f14ea63117d44aa0977c22b87237ff
Antti Leppä <antti.leppa@foyt.fi>
2014-07-02 08:14:15 +0300

    ** Added folder variable into IllusionGroupDAO create method

--- persistence/src/main/java/fi/foyt/fni/persistence/dao/illusion/IllusionGroupDAO.java
+++ persistence/src/main/java/fi/foyt/fni/persistence/dao/illusion/IllusionGroupDAO.java
@@ -10,12 +10,13 @@ import javax.persistence.criteria.Root;
 import fi.foyt.fni.persistence.dao.GenericDAO;
 import fi.foyt.fni.persistence.model.illusion.IllusionGroup;
 import fi.foyt.fni.persistence.model.illusion.IllusionGroup_;
+import fi.foyt.fni.persistence.model.materials.IllusionGroupFolder;
 
 public class IllusionGroupDAO extends GenericDAO<IllusionGroup> {
 
 	private static final long serialVersionUID = 1L;
 
-	public IllusionGroup create(String urlName, String name, String description, String xmppRoom, Date created) {
+	public IllusionGroup create(String urlName, String name, String description, String xmppRoom, IllusionGroupFolder folder, Date created) {
 		IllusionGroup illusionGroup = new IllusionGroup();
 		
 		illusionGroup.setName(name);
@@ -23,6 +24,7 @@ public class IllusionGroupDAO extends GenericDAO<IllusionGroup> {
 		illusionGroup.setUrlName(urlName);
 		illusionGroup.setXmppRoom(xmppRoom);
 		illusionGroup.setCreated(created);
+		illusionGroup.setFolder(folder);
 
 		return persist(illusionGroup);
 	}
