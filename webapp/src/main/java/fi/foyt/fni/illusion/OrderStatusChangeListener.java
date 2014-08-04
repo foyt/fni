d8394ea6acf1e6eff0393d1c2c52bc0a410729f9
Antti Leppä <antti.leppa@foyt.fi>
2014-08-04 19:38:04 +0300

    ** Added mail notification about accepted payment into the Illusion group

--- webapp/src/main/java/fi/foyt/fni/illusion/OrderStatusChangeListener.java
+++ webapp/src/main/java/fi/foyt/fni/illusion/OrderStatusChangeListener.java
@@ -1,17 +1,29 @@
 package fi.foyt.fni.illusion;
 
+import java.util.Locale;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+import javax.mail.MessagingException;
+
+import org.apache.commons.lang3.LocaleUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import fi.foyt.fni.gamelibrary.OrderController;
 import fi.foyt.fni.gamelibrary.OrderEvent;
 import fi.foyt.fni.gamelibrary.OrderPaid;
+import fi.foyt.fni.i18n.ExternalLocales;
+import fi.foyt.fni.mail.Mailer;
 import fi.foyt.fni.persistence.model.gamelibrary.Order;
 import fi.foyt.fni.persistence.model.illusion.IllusionGroup;
 import fi.foyt.fni.persistence.model.illusion.IllusionGroupMember;
 import fi.foyt.fni.persistence.model.illusion.IllusionGroupMemberRole;
+import fi.foyt.fni.persistence.model.system.SystemSettingKey;
+import fi.foyt.fni.persistence.model.users.User;
+import fi.foyt.fni.system.SystemSettingsController;
+import fi.foyt.fni.users.UserController;
 
 public class OrderStatusChangeListener {
 	
@@ -21,8 +33,17 @@ public class OrderStatusChangeListener {
 	@Inject
 	private OrderController orderController;
 
+  @Inject
+	private UserController userController;
+
+  @Inject
+  private Mailer mailer;
+  
 	@Inject
 	private IllusionGroupController illusionGroupController;
+
+  @Inject
+	private SystemSettingsController systemSettingsController;
 	
 	public void onOrderPaid(@Observes @OrderPaid OrderEvent event) {
 		if (event.getOrderId() != null) {
@@ -34,6 +55,7 @@ public class OrderStatusChangeListener {
   		      if (group != null) {
   		        IllusionGroupMember member = illusionGroupController.findIllusionGroupMemberByUserAndGroup(group, order.getCustomer());
     		      illusionGroupController.updateIllusionGroupMemberRole(member, IllusionGroupMemberRole.PLAYER);
+    		      sendPaymentAcceptedMail(member);
   		      } else {
   		        logger.severe("Tried to lift illusion group member role to player for non-existing group");
   		      }
@@ -48,5 +70,30 @@ public class OrderStatusChangeListener {
       logger.severe("Tried to lift illusion group member role to player for non-existing order");
 		}
 	}
+	
+  private void sendPaymentAcceptedMail(IllusionGroupMember groupMember) {
+    User user = groupMember.getUser();
+    Locale userLocale = LocaleUtils.toLocale(user.getLocale());
+    String userMail = userController.getUserPrimaryEmail(user);
+    String userName = groupMember.getUser().getFullName();
+    String groupName = groupMember.getGroup().getName();
+    String groupUrlName = groupMember.getGroup().getUrlName();
+    
+    String groupUrl = systemSettingsController.getSiteUrl(false, true);
+    if (StringUtils.isNotBlank(groupUrl)) {
+      groupUrl += "/illusion/group/" + groupUrlName;
+    }
 
+    String subject = ExternalLocales.getText(userLocale, "illusion.mail.paymentAccepted.subject");
+    String content = ExternalLocales.getText(userLocale, "illusion.mail.paymentAccepted.content", userName, groupName, groupUrl);
+
+    String fromName = systemSettingsController.getSetting(SystemSettingKey.SYSTEM_MAILER_NAME);
+    String fromMail = systemSettingsController.getSetting(SystemSettingKey.SYSTEM_MAILER_MAIL);
+    
+    try {
+      mailer.sendMail(fromMail, fromName, userMail, userName, subject, content, "text/plain");
+    } catch (MessagingException e) {
+      logger.log(Level.SEVERE, "Could not send a group accept notification mail", e);
+    }
+  }
 }
