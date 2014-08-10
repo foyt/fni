a42c4170ffc76a81ec54df675b92a3c51ebb737a
Antti Leppä <antti.leppa@foyt.fi>
2014-08-10 19:04:33 +0300

    ** Removed system setting tests from ServletCtxListener because they made testing more difficult

--- webapp/src/main/java/fi/foyt/fni/ServletCtxListener.java
+++ webapp/src/main/java/fi/foyt/fni/ServletCtxListener.java
@@ -8,18 +8,11 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 
-import org.apache.commons.lang3.StringUtils;
-
-import fi.foyt.fni.system.SystemSettingsController;
-
 @WebListener
 public class ServletCtxListener implements ServletContextListener {
 	
 	@Inject
 	private Logger logger;
-	
-	@Inject
-	private SystemSettingsController systemSettingsController;
 	
   public void contextInitialized(ServletContextEvent event) {
     ServletContext servletContext = event.getServletContext();
@@ -28,25 +21,7 @@ public class ServletCtxListener implements ServletContextListener {
     logger.info("Setting webappPath to " + webappPath);
     System.setProperty("webappPath", webappPath);
     System.setProperty("fni-context-path", servletContext.getContextPath());
-    
-    checkMandatorySetting("site host", systemSettingsController.getSiteHost());
-    checkMandatorySetting("site http port", systemSettingsController.getSiteHttpPort());
-    checkMandatorySetting("site https port", systemSettingsController.getSiteHttpsPort());
-    checkMandatorySetting("site context path", systemSettingsController.getSiteContextPath());
-
     logger.info("Forge & Illusion cloud started");
-  }
-
-  private void checkMandatorySetting(String name, Integer setting) {
-    if (setting == null) {
-      throw new ExceptionInInitializerError("Mandatory setting '" + name + "' is missing");
-    }
-  }
-
-  private void checkMandatorySetting(String name, String setting) {
-    if (StringUtils.isBlank(setting)) {
-      throw new ExceptionInInitializerError("Mandatory setting '" + name + "' is missing");
-    }
   }
 
   public void contextDestroyed(ServletContextEvent event) {
