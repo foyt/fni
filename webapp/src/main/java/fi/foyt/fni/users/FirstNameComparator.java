a828a10dc02d391f0621e21dd21bc8e300c88e7d
Antti Leppä <antti.leppa@foyt.fi>
2014-08-29 19:40:40 +0300

    ** Fixed a bug that caused integration tests to fail

--- webapp/src/main/java/fi/foyt/fni/users/FirstNameComparator.java
+++ webapp/src/main/java/fi/foyt/fni/users/FirstNameComparator.java
@@ -2,13 +2,15 @@ package fi.foyt.fni.users;
 
 import java.util.Comparator;
 
+import org.apache.commons.lang3.ObjectUtils;
+
 import fi.foyt.fni.persistence.model.users.User;
 
 public class FirstNameComparator implements Comparator<User> {
 
 	@Override
   public int compare(User user, User user2) {
-		return user.getFirstName().compareTo(user2.getFirstName());
+	  return ObjectUtils.compare(user.getFirstName(), user2.getFirstName());
   }
 
 }
