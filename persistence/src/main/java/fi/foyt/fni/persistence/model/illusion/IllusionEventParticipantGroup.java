fc6bc15aa477cae067577a27808b2194cc3c9a8d
Antti Leppä <antti.leppa@foyt.fi>
2014-08-31 08:10:41 +0300

    ** Added entity for Illusion event participant groups

---
+++ persistence/src/main/java/fi/foyt/fni/persistence/model/illusion/IllusionEventParticipantGroup.java
@@ -0,0 +1,45 @@
+package fi.foyt.fni.persistence.model.illusion;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+public class IllusionEventParticipantGroup {
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public String getName() {
+    return name;
+  }
+	
+	public void setName(String name) {
+    this.name = name;
+  }
+	
+	public IllusionEvent getEvent() {
+    return event;
+  }
+	
+	public void setEvent(IllusionEvent event) {
+    this.event = event;
+  }
+ 
+  @Id
+  @GeneratedValue (strategy=GenerationType.IDENTITY)
+  private Long id;
+  
+  @NotNull
+  @NotEmpty
+  private String name;
+ 
+  @ManyToOne
+  private IllusionEvent event;
+}
