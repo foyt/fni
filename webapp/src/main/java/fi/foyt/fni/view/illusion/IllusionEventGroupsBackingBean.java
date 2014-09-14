315a1a7a871875418a9fd909bd3a71d71d6dfd2a
Antti Leppä <antti.leppa@foyt.fi>
2014-09-14 17:41:43 +0300

    ** Added view for managing Illusion event groups

---
+++ webapp/src/main/java/fi/foyt/fni/view/illusion/IllusionEventGroupsBackingBean.java
@@ -0,0 +1,172 @@
+package fi.foyt.fni.view.illusion;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.ejb.Stateful;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.apache.commons.lang3.StringUtils;
+import org.ocpsoft.rewrite.annotation.Join;
+import org.ocpsoft.rewrite.annotation.Parameter;
+
+import fi.foyt.fni.illusion.IllusionEventController;
+import fi.foyt.fni.illusion.IllusionEventGroupController;
+import fi.foyt.fni.persistence.model.illusion.IllusionEvent;
+import fi.foyt.fni.persistence.model.illusion.IllusionEventGroupMember;
+import fi.foyt.fni.persistence.model.illusion.IllusionEventParticipant;
+import fi.foyt.fni.persistence.model.illusion.IllusionEventGroup;
+import fi.foyt.fni.persistence.model.illusion.IllusionEventParticipantRole;
+import fi.foyt.fni.persistence.model.users.Permission;
+import fi.foyt.fni.persistence.model.users.User;
+import fi.foyt.fni.security.LoggedIn;
+import fi.foyt.fni.security.Secure;
+import fi.foyt.fni.security.SecurityContext;
+import fi.foyt.fni.security.SecurityParam;
+import fi.foyt.fni.security.SecurityParams;
+import fi.foyt.fni.users.UserController;
+
+@RequestScoped
+@Named
+@Stateful
+@Join (path = "/illusion/event/{urlName}/groups", to = "/illusion/event-groups.jsf")
+@LoggedIn
+@Secure (value = Permission.ILLUSION_EVENT_ACCESS, deferred = true)
+@SecurityContext (context = "@urlName")
+@SecurityParams ({
+  @SecurityParam (name = "roles", value = "ORGANIZER")
+})
+public class IllusionEventGroupsBackingBean extends AbstractIllusionEventBackingBean {
+
+  @Parameter
+  private String urlName;
+
+  @Inject
+  private UserController userController;
+
+  @Inject
+  private IllusionEventController illusionEventController;
+  
+  @Inject
+  private IllusionEventGroupController illusionEventGroupController;
+  
+  @Override
+  public String init(IllusionEvent illusionEvent, IllusionEventParticipant participant) {
+    groups = illusionEventGroupController.listGroups(illusionEvent);
+    participants = illusionEventController.listIllusionEventParticipantsByEventAndRole(illusionEvent, IllusionEventParticipantRole.PARTICIPANT);
+
+    return null;
+  }
+
+  @Override
+  public String getUrlName() {
+    return urlName;
+  }
+  
+  public void setUrlName(String urlName) {
+    this.urlName = urlName;
+  }
+  
+  public List<IllusionEventGroup> getGroups() {
+    return groups;
+  }
+  
+  public List<IllusionEventParticipant> getParticipants() {
+    return participants;
+  }
+  
+  public String getSelectedGroupName() {
+    return selectedGroupName;
+  }
+  
+  public void setSelectedGroupName(String selectedGroupName) {
+    this.selectedGroupName = selectedGroupName;
+  }
+  
+  public List<IllusionEventGroupMember> getMembers() {
+    return members;
+  }
+  
+  public Long getSelectedGroupId() {
+    return selectedGroupId;
+  }
+  
+  public void setSelectedGroupId(Long selectedGroupId) {
+    this.selectedGroupId = selectedGroupId;
+  }
+  
+  public List<Long> getSelectedGroupMemberParticipantIds() {
+    return selectedGroupMemberParticipantIds;
+  }
+  
+  public void setSelectedGroupMemberParticipantIds(List<Long> selectedGroupMemberParticipantIds) {
+    this.selectedGroupMemberParticipantIds = selectedGroupMemberParticipantIds;
+  }
+  
+  public String getParticipantDisplayName(IllusionEventParticipant participant) {
+    User user = participant.getUser();
+    String result = user.getFullName();
+    if (StringUtils.isNotBlank(result)) {
+      return result;
+    }
+    
+    return "<" + userController.getUserPrimaryEmail(user) + ">";
+  }
+  
+  public void selectGroup(IllusionEventGroup group) {
+    selectedGroupId = group.getId();
+    selectedGroupName = group.getName();
+    members = illusionEventGroupController.listMembers(group);
+    selectedGroupMemberParticipantIds = new ArrayList<>();
+    
+    for (IllusionEventGroupMember member : members) {
+      selectedGroupMemberParticipantIds.add(member.getParticipant().getId());
+    }
+  }
+  
+  public void saveGroup() {
+    IllusionEventGroup group = illusionEventGroupController.findGroupById(selectedGroupId);
+    List<IllusionEventGroupMember> currentMembers = illusionEventGroupController.listMembers(group);
+    Set<Long> addParticipantIds = new HashSet<>();
+    Set<Long> removeParticipantIds = new HashSet<>();
+    
+    for (IllusionEventGroupMember currentMember : currentMembers) {
+      removeParticipantIds.add(currentMember.getParticipant().getId());
+    }
+    
+    for (Long selectedGroupMemberParticipantId : selectedGroupMemberParticipantIds) {
+      if (!removeParticipantIds.contains(selectedGroupMemberParticipantId)) {
+        addParticipantIds.add(selectedGroupMemberParticipantId);
+      }
+      
+      removeParticipantIds.remove(selectedGroupMemberParticipantId);
+    }
+    
+    for (Long participantId : removeParticipantIds) {
+      IllusionEventParticipant participant = illusionEventController.findIllusionEventParticipantById(participantId);
+      IllusionEventGroupMember member = illusionEventGroupController.findMemberByGroupAndParticipant(group, participant);
+      if (member != null) {
+        illusionEventGroupController.deleteMember(member);
+      }
+    }
+    
+    for (Long participantId : addParticipantIds) {
+      IllusionEventParticipant participant = illusionEventController.findIllusionEventParticipantById(participantId);
+      illusionEventGroupController.createMember(group, participant);
+    }
+    
+    illusionEventGroupController.updateGroupName(group, getSelectedGroupName());
+    groups = illusionEventGroupController.listGroups(group.getEvent());
+  }
+  
+  private List<IllusionEventGroup> groups;
+  private List<IllusionEventParticipant> participants;
+  private Long selectedGroupId;
+  private List<IllusionEventGroupMember> members;
+  private String selectedGroupName;
+  private List<Long> selectedGroupMemberParticipantIds;
+}
---
+++ webapp/src/main/webapp/resources/scripts/gui/illusion-event-groups.js
@@ -0,0 +1,106 @@
+(function() {
+  'use strict';
+  
+  $.widget("custom.illusionGroupMemberList", {
+    options : {
+    },
+    _create : function() {
+      var selectedMembers = ($(this.element).find('.illusion-event-group-members-list-ids').val()||'').split('&');
+      for (var i = 0, l = selectedMembers.length; i < l; i++) {
+        var selectedMember = selectedMembers[i];
+        
+        $(this.element)
+          .find('.illusion-event-group-members-list-item[data-participant-id="' + selectedMember + '"]')
+          .appendTo($(this.element).find('.illusion-event-group-members-list-column-members .illusion-event-group-members-list-column-contents'));
+      }
+      
+      this._updateList();
+            
+      $(this.element).on('click', '.illusion-event-group-members-list-item', $.proxy(function (event) {
+        $(this.element).find('.illusion-event-group-members-list-item-selected').removeClass('illusion-event-group-members-list-item-selected');
+        $(event.target).addClass('illusion-event-group-members-list-item-selected');
+        this._updateList();
+      }, this));
+      
+      $(this.element).on('click', '.illusion-event-group-members-list-action-add', $.proxy(function (event) {
+        event.preventDefault();
+        $(this.element)
+          .find('.illusion-event-group-members-list-item-selected')
+          .appendTo($(this.element).find('.illusion-event-group-members-list-column-members .illusion-event-group-members-list-column-contents'));
+        this._updateList();
+      }, this));
+      
+      $(this.element).on('click', '.illusion-event-group-members-list-action-remove', $.proxy(function (event) {
+        event.preventDefault();
+        $(this.element).find('.illusion-event-group-members-list-column-participants').append($(this.element).find('.illusion-event-group-members-list-item-selected'));
+        this._updateList();
+      }, this));
+    },
+    _updateList: function() {
+      var participantSelected = $(this.element).find('.illusion-event-group-members-list-item-selected').closest('.illusion-event-group-members-list-column-participants').length > 0;
+      var memberSelected = $(this.element).find('.illusion-event-group-members-list-item-selected').closest('.illusion-event-group-members-list-column-members').length > 0;
+      
+      if (participantSelected) {
+        $(this.element).find('.illusion-event-group-members-list-action-add').removeAttr('disabled');
+      } else {
+        $(this.element).find('.illusion-event-group-members-list-action-add').attr('disabled', 'disabled');
+      }
+      
+      if (memberSelected) {
+        $(this.element).find('.illusion-event-group-members-list-action-remove').removeAttr('disabled');
+      } else {
+        $(this.element).find('.illusion-event-group-members-list-action-remove').attr('disabled', 'disabled');
+      } 
+      
+      var ids = $.map($(this.element).find('.illusion-event-group-members-list-column-members .illusion-event-group-members-list-item'), function (item) {
+        return $(item).data('participant-id');
+      });
+      
+      $(this.element).find('.illusion-event-group-members-list-ids').val(ids.join('&'));
+    }
+  });
+  
+  $(document).ready(function() { 
+    $('.illusion-event-group-members-list').illusionGroupMemberList();
+    
+    $('.illusion-new-group').click(function (event) {
+      dust.render("illusion-event-new-group", { }, function(err, html) {
+        if (!err) {
+          var dialog = $(html);
+          dialog.dialog({
+            modal: true,
+            width: 600,
+            buttons: [{
+              'text': dialog.data('create-button'),
+              'click': function(event) {
+                $.ajax(CONTEXTPATH + '/rest/illusion/events/' + $('input[name="eventUrlName"]').val() + '/groups/', {
+                  type: 'POST',
+                  contentType: "application/json",
+                  dataType : "json",
+                  data: JSON.stringify({
+                    'name': $(this).find('input[name="name"]').val()
+                  }),
+                  accepts: {
+                    'json' : 'application/json'
+                  },
+                  success : function(data) {
+                    window.location.reload(true);
+                  }
+                });
+                
+              }
+            }, {
+              'text': dialog.data('cancel-button'),
+              'click': function(event) {
+                $(this).dialog("close");
+              }
+            }]
+          });
+        } else {
+          $('.notifications').notifications('notification', 'error', err);
+        }
+      });
+    });
+  });
+
+}).call(this);
