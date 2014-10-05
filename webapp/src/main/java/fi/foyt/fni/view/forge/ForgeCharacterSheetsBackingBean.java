263ddb61fa9b21b32d174f4e4dfe8ed69503965e
Antti Leppä <antti.leppa@foyt.fi>
2014-10-05 20:44:32 +0300

    ** Added view for editing / viewing Character Sheets

---
+++ webapp/src/main/java/fi/foyt/fni/view/forge/ForgeCharacterSheetsBackingBean.java
@@ -0,0 +1,171 @@
+package fi.foyt.fni.view.forge;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import javax.ejb.Stateful;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.ocpsoft.rewrite.annotation.Join;
+import org.ocpsoft.rewrite.annotation.Matches;
+import org.ocpsoft.rewrite.annotation.Parameter;
+import org.ocpsoft.rewrite.annotation.RequestAction;
+import org.ocpsoft.rewrite.faces.annotation.Deferred;
+
+import fi.foyt.fni.materials.CharacterSheetMaterialController;
+import fi.foyt.fni.materials.MaterialController;
+import fi.foyt.fni.materials.MaterialPermissionController;
+import fi.foyt.fni.persistence.model.materials.CharacterSheet;
+import fi.foyt.fni.persistence.model.materials.Folder;
+import fi.foyt.fni.persistence.model.materials.Material;
+import fi.foyt.fni.persistence.model.users.User;
+import fi.foyt.fni.security.LoggedIn;
+import fi.foyt.fni.session.SessionController;
+
+@RequestScoped
+@Named
+@Stateful
+@Join (path = "/forge/character-sheets/{ownerId}/{urlPath}", to = "/forge/character-sheets.jsf")
+@LoggedIn
+public class ForgeCharacterSheetsBackingBean {
+  
+  @Parameter
+  @Matches ("[0-9]{1,}")
+  private Long ownerId;
+  
+  @Parameter
+  @Matches ("[a-zA-Z0-9_/.\\-:,]{1,}")
+  private String urlPath;
+  
+	@Inject
+	private SessionController sessionController;
+	
+	@Inject
+	private MaterialController materialController;
+
+  @Inject
+	private MaterialPermissionController materialPermissionController;
+
+  @Inject
+  private CharacterSheetMaterialController characterSheetMaterialController;
+	
+	@RequestAction
+	@Deferred
+	public String load() throws FileNotFoundException {
+		if ((getOwnerId() == null)||(getUrlPath() == null)) {
+			return "/error/not-found.jsf";
+		}
+		
+		String completePath = "/materials/" + getOwnerId() + "/" + getUrlPath();
+		Material material = materialController.findMaterialByCompletePath(completePath);
+		User loggedUser = sessionController.getLoggedUser();
+		
+		if (!(material instanceof CharacterSheet)) {
+      return "/error/not-found.jsf";
+		}
+
+    CharacterSheet characterSheet = (CharacterSheet) material;
+    
+		if (!materialPermissionController.hasAccessPermission(loggedUser, characterSheet)) {
+      return "/error/access-denied.jsf";
+		}
+		
+		readOnly = !materialPermissionController.hasModifyPermission(loggedUser, material);
+		materialId = characterSheet.getId();
+		characterSheetTitle = characterSheet.getTitle();
+		characterSheetContents = characterSheet.getContents();
+		characterSheetStyles = characterSheet.getStyles();
+		characterSheetScripts = characterSheet.getScripts();
+		
+		folders = ForgeViewUtils.getParentList(material);
+		
+		return null;
+	}
+	
+	public Long getOwnerId() {
+		return ownerId;
+	}
+	
+	public void setOwnerId(Long ownerId) {
+		this.ownerId = ownerId;
+	}
+
+	public String getUrlPath() {
+		return urlPath;
+	}
+	
+	public void setUrlPath(String urlPath) {
+		this.urlPath = urlPath;
+	}
+	
+	public Long getMaterialId() {
+		return materialId;
+	}
+	
+	public List<Folder> getFolders() {
+		return folders;
+	}
+	
+	public String getCharacterSheetTitle() {
+    return characterSheetTitle;
+  }
+	
+	public void setCharacterSheetTitle(String characterSheetTitle) {
+    this.characterSheetTitle = characterSheetTitle;
+  }
+	
+	public String getCharacterSheetContents() {
+    return characterSheetContents;
+  }
+	
+	public void setCharacterSheetContents(String characterSheetContents) {
+    this.characterSheetContents = characterSheetContents;
+  }
+	
+	public String getCharacterSheetScripts() {
+    return characterSheetScripts;
+  }
+	
+	public void setCharacterSheetScripts(String characterSheetScripts) {
+    this.characterSheetScripts = characterSheetScripts;
+  }
+	
+	public String getCharacterSheetStyles() {
+    return characterSheetStyles;
+  }
+	
+	public void setCharacterSheetStyles(String characterSheetStyles) {
+    this.characterSheetStyles = characterSheetStyles;
+  }
+	
+	public boolean isReadOnly() {
+    return readOnly;
+  }
+	
+	public String save() {
+    User loggedUser = sessionController.getLoggedUser();
+    CharacterSheet characterSheet = characterSheetMaterialController.findCharacterSheetById(materialId);
+    
+	  if (materialPermissionController.hasModifyPermission(loggedUser, characterSheet)) {
+	    characterSheetMaterialController.updateCharacterSheet(characterSheet, getCharacterSheetTitle(), 
+        getCharacterSheetContents(), getCharacterSheetStyles(), getCharacterSheetScripts(), loggedUser);
+
+      String ownerId = characterSheet.getCreator().getId().toString();
+	    String urlPath = characterSheet.getPath().substring(ownerId.length() + 1);
+	    
+	    return "/forge/character-sheets.jsf?faces-redirect=true&ownerId=" + ownerId + "&urlPath=" + urlPath;
+	  } else {
+      return "/error/access-denied.jsf";
+	  }
+	}
+	
+	private Long materialId;
+	private String characterSheetTitle;
+  private String characterSheetContents;
+  private String characterSheetScripts;
+	private String characterSheetStyles;
+	private List<Folder> folders;
+	private boolean readOnly;
+}
---
+++ webapp/src/main/webapp/resources/scripts/gui/forge-character-sheets.js
@@ -0,0 +1,105 @@
+(function() {
+  'use strict';
+  
+  var updateTimeout = 300;
+  
+  function utf8_to_b64( str ) {
+    return window.btoa(unescape(encodeURIComponent( str )));
+  }
+   
+  function b64_to_utf8( str ) {
+    return decodeURIComponent(escape(window.atob( str )));
+  }
+  
+  $.widget("custom.codeMirror", {
+    options: {
+      lineNumbers: true,
+      foldGutter: true,
+      gutters: ["CodeMirror-linenumbers", "CodeMirror-foldgutter"]
+    },
+    _create : function() {
+      this._editor = CodeMirror.fromTextArea(this.element[0], {
+        mode: this.options.mode,
+        extraKeys: {"Ctrl-Space": "autocomplete"},
+        lineNumbers: this.options.lineNumbers,
+        foldGutter: this.options.foldGutter,
+        gutters: this.options.gutters
+      });
+
+      this._editor.on("change", $.proxy(function() {
+        $(this.element).trigger("change");
+      }, this));
+    },
+    
+    value: function () {
+      return this._editor.getValue();
+    },
+    
+    _destroy : function() {
+    
+    }
+  });
+  
+  $(document).ready(function () {
+    var previewTimeout = null;
+    
+    function updatePreview(title, styles, contents, scripts) {
+      dust.render("character-sheet", { 
+        "title": title,
+        "styles": styles,
+        "contents": contents,
+        "scripts": scripts 
+      }, function(err, html) {
+        var previewFrame = $('.forge-chracter-sheet-editor-preview');
+        previewFrame.attr("src", 'data:text/html;base64,' + utf8_to_b64(html));
+      });
+    }
+    
+    function onChange() {
+      if (previewTimeout) {
+        clearTimeout(previewTimeout);  
+      }
+      
+      previewTimeout = setTimeout(function () {
+        updatePreview(
+          $('.forge-chracter-sheet-title').val(), 
+          $('.forge-chracter-sheet-editor-styles').codeMirror('value'), 
+          $('.forge-chracter-sheet-editor-contents').codeMirror('value'), 
+          $('.forge-chracter-sheet-editor-scripts').codeMirror('value')
+        ); 
+      }, updateTimeout);
+    }
+    
+    $('.forge-chracter-sheet-editor-preview').load(function () {
+      var contentDocument = this.contentDocument || this.contentWindow.document;
+      var scrollHeight = $(contentDocument).find('html').prop('scrollHeight');
+      $(this).css("height", scrollHeight);
+    });
+
+    updatePreview(
+      $('.forge-chracter-sheet-title').val(), 
+      $('.forge-chracter-sheet-editor-styles').val(), 
+      $('.forge-chracter-sheet-editor-contents').val(),
+      $('.forge-chracter-sheet-editor-scripts').val()
+    ); 
+    
+    $('textarea.forge-chracter-sheet-editor-contents')
+      .codeMirror({
+        mode: "text/html"
+      })
+      .on('change', onChange);
+    
+    $('textarea.forge-chracter-sheet-editor-styles')
+      .codeMirror({
+        mode: "text/css"
+      })
+      .on('change', onChange);
+    
+    $('textarea.forge-chracter-sheet-editor-scripts')
+      .codeMirror({
+        mode: "text/javascript"
+      })
+      .on('change', onChange);
+  });
+  
+}).call(this);
