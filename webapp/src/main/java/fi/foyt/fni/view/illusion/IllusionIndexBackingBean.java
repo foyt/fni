77dc9c568b62f0473420fee73a2709862b83a44a
Antti Leppä <antti.leppa@foyt.fi>
2014-10-26 19:43:55 +0200

    ** Changed Illusion index view to list upcoming and past events not regarding whether user is logged in or not

--- webapp/src/main/java/fi/foyt/fni/view/illusion/IllusionIndexBackingBean.java
+++ webapp/src/main/java/fi/foyt/fni/view/illusion/IllusionIndexBackingBean.java
@@ -1,5 +1,7 @@
 package fi.foyt.fni.view.illusion;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Stateful;
@@ -12,16 +14,16 @@ import org.ocpsoft.rewrite.annotation.RequestAction;
 
 import fi.foyt.fni.illusion.IllusionEventController;
 import fi.foyt.fni.persistence.model.illusion.IllusionEvent;
+import fi.foyt.fni.persistence.model.illusion.IllusionEventParticipant;
 import fi.foyt.fni.persistence.model.illusion.IllusionEventParticipantRole;
 import fi.foyt.fni.persistence.model.users.User;
-import fi.foyt.fni.security.LoggedIn;
 import fi.foyt.fni.session.SessionController;
+import fi.foyt.fni.users.UserController;
 
 @RequestScoped
 @Named
 @Stateful
 @Join (path = "/illusion/", to = "/illusion/index.jsf")
-@LoggedIn
 public class IllusionIndexBackingBean {
 
   @Inject
@@ -30,28 +32,141 @@ public class IllusionIndexBackingBean {
   @Inject
   private IllusionEventController illusionEventController;
 
+  @Inject
+  private UserController userController;
+  
   @RequestAction
   public String init() {
-    User loggedUser = sessionController.getLoggedUser();
+    upcomingEvents = new ArrayList<>();
+    pastEvents = new ArrayList<>();
     
-    organizingEvents = illusionEventController.listIllusionEventsByUserAndRole(loggedUser, IllusionEventParticipantRole.ORGANIZER);
-    events = illusionEventController.listIllusionEventsByUserAndRole(loggedUser, IllusionEventParticipantRole.PARTICIPANT);
+    for (IllusionEvent illusionEvent : illusionEventController.listNextIllusionEvents(1000)) {
+      upcomingEvents.add(createEventPojo(illusionEvent));
+    }
+
+    for (IllusionEvent illusionEvent : illusionEventController.listPastIllusionEvents(1000)) {
+      pastEvents.add(createEventPojo(illusionEvent));
+    }
     
     return null;
   }
   
-  public List<IllusionEvent> getOrganizingEvents() {
-    return organizingEvents;
+  private Event createEventPojo(IllusionEvent event) {
+    List<EventOrganizer> organizers = new ArrayList<>();
+    
+    for (IllusionEventParticipant organizer : illusionEventController.listIllusionEventParticipantsByEventAndRole(event, IllusionEventParticipantRole.ORGANIZER)) {
+      organizers.add(new EventOrganizer(organizer.getUser().getId(), organizer.getId(), userController.getUserDisplayName(organizer.getUser())));
+    }
+    
+    IllusionEventParticipantRole role = null;
+    
+    if (sessionController.isLoggedIn()) {
+      User loggedUser = sessionController.getLoggedUser();
+      IllusionEventParticipant eventParticipant = illusionEventController.findIllusionEventParticipantByEventAndUser(event, loggedUser);
+      if (eventParticipant != null) {
+        role = eventParticipant.getRole();
+      }
+    }
+
+    return new Event(event.getName(), event.getUrlName(), event.getDescription(), event.getStartDate(), event.getStartTime(), event.getEndDate(), event.getEndTime(), organizers, role);
+  }
+
+  public List<Event> getUpcomingEvents() {
+    return upcomingEvents;
   }
   
-  public List<IllusionEvent> getEvents() {
-    return events;
+  public List<Event> getPastEvents() {
+    return pastEvents;
   }
   
-  public Long getEventParticipantCount(IllusionEvent group) {
-    return illusionEventController.countIllusionEventParticipantsByEventAndRole(group, IllusionEventParticipantRole.PARTICIPANT);
+  private List<Event> upcomingEvents;
+  private List<Event> pastEvents;
+  
+  public class Event {
+    
+    public Event(String name, String urlName, String description, Date startDate, Date startTime, Date endDate, Date endTime, List<EventOrganizer> organizers,
+        IllusionEventParticipantRole role) {
+      super();
+      this.name = name;
+      this.urlName = urlName;
+      this.description = description;
+      this.startDate = startDate;
+      this.startTime = startTime;
+      this.endDate = endDate;
+      this.endTime = endTime;
+      this.organizers = organizers;
+      this.role = role;
+    }
+
+    public String getName() {
+      return name;
+    }
+    
+    public String getUrlName() {
+      return urlName;
+    }
+    
+    public String getDescription() {
+      return description;
+    }
+    
+    public Date getStartDate() {
+      return startDate;
+    }
+    
+    public Date getStartTime() {
+      return startTime;
+    }
+    
+    public Date getEndDate() {
+      return endDate;
+    }
+    
+    public Date getEndTime() {
+      return endTime;
+    }
+    
+    public List<EventOrganizer> getOrganizers() {
+      return organizers;
+    }
+    
+    public IllusionEventParticipantRole getRole() {
+      return role;
+    }
+
+    private String name;
+    private String urlName;
+    private String description;
+    private Date startDate;
+    private Date startTime;
+    private Date endDate;
+    private Date endTime;
+    private List<EventOrganizer> organizers;
+    private IllusionEventParticipantRole role;
   }
   
-  private List<IllusionEvent> organizingEvents;
-  private List<IllusionEvent> events;
+  public class EventOrganizer {
+ 
+    public EventOrganizer(Long userId, Long participantId, String name) {
+      this.userId = userId;
+      this.participantId = participantId;
+      this.name = name;
+    }
+
+    public String getName() {
+      return name;
+    }
+    
+    public Long getParticipantId() {
+      return participantId;
+    }
+    
+    public Long getUserId() {
+      return userId;
+    }
+    
+    private Long userId;
+    private Long participantId;
+    private String name;
+  }
 }
