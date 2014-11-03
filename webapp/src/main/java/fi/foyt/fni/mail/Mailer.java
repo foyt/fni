d0f96521e080091057e8da3dda0f8e72af3e4f95
Antti Leppä <antti.leppa@foyt.fi>
2014-11-03 18:43:06 +0200

    ** Added MailBuilder into mailer that can be used to build emails more easily

--- webapp/src/main/java/fi/foyt/fni/mail/Mailer.java
+++ webapp/src/main/java/fi/foyt/fni/mail/Mailer.java
@@ -1,6 +1,8 @@
 package fi.foyt.fni.mail;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.Resource;
 import javax.enterprise.context.Dependent;
@@ -8,6 +10,7 @@ import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
@@ -36,5 +39,119 @@ public class Mailer {
     } catch (UnsupportedEncodingException e) {
       throw new MessagingException("Error occured while trying to encode internet address", e);
     }
+  }
+  
+  public MailBuilder getBuilder() {
+    return new MailBuilder();
+  }
+  
+  public class MailBuilder {
+    
+    public MailBuilder() {
+      this.to = new ArrayList<>();
+      this.cc = new ArrayList<>();
+      this.bcc = new ArrayList<>();
+      this.contentType = "text/plain;charset=\"utf-8\"";
+    }
+    
+    public MailBuilder setContentType(String contentType) {
+      this.contentType = contentType;
+      return this;
+    }
+    
+    public MailBuilder setFrom(String address) throws AddressException {
+      fromAddress = new InternetAddress(address);
+      return this;
+    }
+    
+    public MailBuilder setFrom(String address, String personal) throws AddressException, UnsupportedEncodingException {
+      fromAddress = new InternetAddress(address, personal,  "UTF-8");
+      return this;
+    }
+    
+    public MailBuilder setReplyTo(String address) throws AddressException {
+      replyToAddress = new InternetAddress(address);
+      return this;
+    }
+    
+    public MailBuilder setReplyTo(String address, String personal) throws AddressException, UnsupportedEncodingException {
+      replyToAddress = new InternetAddress(address, personal,  "UTF-8");
+      return this;
+    }
+    
+    public MailBuilder addTo(String address) throws AddressException {
+      to.add(new InternetAddress(address));
+      return this;
+    }
+    
+    public MailBuilder addCc(String address) throws AddressException {
+      cc.add(new InternetAddress(address));
+      return this;
+    }
+    
+    public MailBuilder addBcc(String address) throws AddressException {
+      bcc.add(new InternetAddress(address));
+      return this;
+    }
+    
+    public MailBuilder addTo(String address, String personal) throws AddressException, UnsupportedEncodingException {
+      to.add(new InternetAddress(address, personal,  "UTF-8"));
+      return this;
+    }
+    
+    public MailBuilder addCc(String address, String personal) throws AddressException, UnsupportedEncodingException {
+      cc.add(new InternetAddress(address, personal,  "UTF-8"));
+      return this;
+    }
+    
+    public MailBuilder addBcc(String address, String personal) throws AddressException, UnsupportedEncodingException {
+      bcc.add(new InternetAddress(address, personal,  "UTF-8"));
+      return this;
+    }
+    
+    public MailBuilder setContent(String content) {
+      this.content = content;
+      return this;
+    }
+    
+    public MailBuilder setSubject(String subject) {
+      this.subject = subject;
+      return this;
+    }
+    
+    public void send() throws MessagingException {
+      MimeMessage message = new MimeMessage(mailSession);
+
+      message.setSubject(subject, "UTF-8");
+      message.setContent(content, contentType);
+      message.setFrom(fromAddress);
+      
+      if (replyToAddress != null) {
+        message.setReplyTo(new InternetAddress[] { replyToAddress });
+      }
+      
+      if (!to.isEmpty()) {
+        message.setRecipients(Message.RecipientType.TO, to.toArray(new InternetAddress[0]));
+      }
+      
+      if (!cc.isEmpty()) {
+        message.setRecipients(Message.RecipientType.CC, cc.toArray(new InternetAddress[0]));
+      }
+      
+      if (!bcc.isEmpty()) {
+        message.setRecipients(Message.RecipientType.BCC, bcc.toArray(new InternetAddress[0]));
+      }
+      
+      Transport.send(message);
+    }
+    
+    private InternetAddress fromAddress;
+    private InternetAddress replyToAddress;
+    private List<InternetAddress> to;
+    private List<InternetAddress> cc;
+    private List<InternetAddress> bcc;
+    private String subject;
+    private String content;
+    private String contentType;
   }
 }
