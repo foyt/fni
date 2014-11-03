9a9ca24f4eb51cca8000b6ba39ae3cf75776e84b
Antti Leppä <antti.leppa@foyt.fi>
2014-11-03 20:16:23 +0200

    ** Added support for mailing event participants

---
+++ webapp/src/main/java/fi/foyt/fni/view/illusion/IllusionEventMailServlet.java
@@ -0,0 +1,181 @@
+package fi.foyt.fni.view.illusion;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.inject.Inject;
+import javax.mail.MessagingException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.transaction.Transactional;
+
+import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.lang3.StringUtils;
+
+import fi.foyt.fni.illusion.IllusionEventController;
+import fi.foyt.fni.illusion.IllusionEventGroupController;
+import fi.foyt.fni.mail.Mailer;
+import fi.foyt.fni.mail.Mailer.MailBuilder;
+import fi.foyt.fni.persistence.model.illusion.IllusionEvent;
+import fi.foyt.fni.persistence.model.illusion.IllusionEventGroup;
+import fi.foyt.fni.persistence.model.illusion.IllusionEventGroupMember;
+import fi.foyt.fni.persistence.model.illusion.IllusionEventParticipant;
+import fi.foyt.fni.persistence.model.illusion.IllusionEventParticipantRole;
+import fi.foyt.fni.persistence.model.system.SystemSettingKey;
+import fi.foyt.fni.persistence.model.users.User;
+import fi.foyt.fni.session.SessionController;
+import fi.foyt.fni.system.SystemSettingsController;
+import fi.foyt.fni.users.UserController;
+import fi.foyt.fni.view.AbstractFileServlet;
+
+@WebServlet(urlPatterns = "/illusion/eventMail/*", name = "illusion-eventmail")
+@Transactional
+public class IllusionEventMailServlet extends AbstractFileServlet {
+
+  private static final long serialVersionUID = 8840385463120576014L;
+
+  @Inject
+	private UserController userController;
+
+  @Inject
+	private SessionController sessionController;
+
+  @Inject
+  private IllusionEventController illusionEventController;
+
+  @Inject
+  private IllusionEventGroupController illusionEventGroupController;
+
+  @Inject
+  private SystemSettingsController systemSettingsController;
+  
+  @Inject
+  private Logger logger;
+
+  @Inject
+  private Mailer mailer;
+  
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	  String pathInfo = request.getPathInfo();
+    if (StringUtils.isBlank(pathInfo)) {
+      response.sendError(HttpServletResponse.SC_NOT_FOUND);
+      return;
+    }
+    
+    String[] pathItems = StringUtils.removeStart(pathInfo, "/").split("/");
+    if (pathItems.length != 1) {
+      response.sendError(HttpServletResponse.SC_NOT_FOUND);
+      return;
+    }
+    
+    String eventUrlName = pathItems[0];
+    if (StringUtils.isBlank(eventUrlName)) {
+      response.sendError(HttpServletResponse.SC_NOT_FOUND);
+      return;
+    }
+    
+    if (!sessionController.isLoggedIn()) {
+      response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+      return;
+    }
+
+    User loggedUser = sessionController.getLoggedUser();
+    IllusionEvent event = illusionEventController.findIllusionEventByUrlName(eventUrlName);
+    if (event == null) {
+      response.sendError(HttpServletResponse.SC_NOT_FOUND);
+      return;
+    }
+    
+    IllusionEventParticipant loggedParticipant = illusionEventController.findIllusionEventParticipantByEventAndUser(event, loggedUser);
+    if ((loggedParticipant == null)||(loggedParticipant.getRole() != IllusionEventParticipantRole.ORGANIZER)) {
+      response.sendError(HttpServletResponse.SC_FORBIDDEN);
+      return;
+    }
+
+    String mailSubject = request.getParameter("mailSubject");
+    String mailContent = request.getParameter("mailContent");
+    
+    List<String> emails = getRecipients(event, request.getParameter("recipients"));
+    if (!emails.isEmpty()) {
+      sendMails(loggedUser, event, emails, mailSubject, mailContent);
+    }
+    
+	  response.setStatus(HttpServletResponse.SC_NO_CONTENT);
+	}
+
+  private void sendMails(User loggedUser, IllusionEvent event, List<String> emails, String emailSubject, String emailContent) {  
+    String fromPersonal = systemSettingsController.getSetting(SystemSettingKey.SYSTEM_MAILER_NAME);
+    String fromAddress = systemSettingsController.getSetting(SystemSettingKey.SYSTEM_MAILER_MAIL);
+    String replyToPersonal = loggedUser.getFullName();
+    String replyToMail = userController.getUserPrimaryEmail(loggedUser);
+    
+    try {
+      MailBuilder mailBuilder = mailer.getBuilder()
+        .setFrom(fromAddress, fromPersonal)
+        .setReplyTo(replyToMail, replyToPersonal)
+        .setContent(emailContent)
+        .setSubject(emailSubject);
+
+      for (String email : emails) {
+        mailBuilder.addBcc(email);
+      }
+
+      mailBuilder.send();
+    } catch (MessagingException e) {
+      logger.log(Level.SEVERE, "Could not send a mail", e);
+    } catch (UnsupportedEncodingException e) {
+      logger.log(Level.SEVERE, "Could not send a mail", e);
+    }  
+  }
+  
+  private List<String> getRecipients(IllusionEvent event, String recipients) {
+    List<IllusionEventParticipant> participants = new ArrayList<>();
+    
+    switch (recipients) {
+      case "PARTICIPANTS":
+        participants.addAll(illusionEventController.listIllusionEventParticipantsByEventAndRole(event, IllusionEventParticipantRole.PARTICIPANT));
+      break;
+      case "ORGANIZERS":
+        participants.addAll(illusionEventController.listIllusionEventParticipantsByEventAndRole(event, IllusionEventParticipantRole.ORGANIZER));
+      break;
+      case "INVITED":
+        participants.addAll(illusionEventController.listIllusionEventParticipantsByEventAndRole(event, IllusionEventParticipantRole.INVITED));
+      break;
+      case "PARTICIPANTS_ORGANIZERS_INVITED":
+        participants.addAll(illusionEventController.listIllusionEventParticipantsByEventAndRole(event, IllusionEventParticipantRole.PARTICIPANT));
+        participants.addAll(illusionEventController.listIllusionEventParticipantsByEventAndRole(event, IllusionEventParticipantRole.ORGANIZER));
+        participants.addAll(illusionEventController.listIllusionEventParticipantsByEventAndRole(event, IllusionEventParticipantRole.INVITED));
+      break;
+    }
+    
+    if (StringUtils.startsWith(recipients, "GROUP_")) {
+      Long groupId = NumberUtils.createLong(recipients.substring(6));
+      IllusionEventGroup eventGroup = illusionEventGroupController.findGroupById(groupId);
+      if (eventGroup != null) {
+        List<IllusionEventGroupMember> members = illusionEventGroupController.listMembers(eventGroup);
+        for (IllusionEventGroupMember member : members) {
+          participants.add(member.getParticipant());
+        }
+      }
+    }
+    
+    List<String> emails = new ArrayList<>();
+    
+    for (IllusionEventParticipant participant : participants) {
+      String email = userController.getUserPrimaryEmail(participant.getUser());
+      if (!emails.contains(email)) {
+        emails.add(email);
+      }
+    }
+    
+    return emails;
+  }
+
+}
--- webapp/src/main/webapp/resources/scripts/gui/illusion-event-participants.js
+++ webapp/src/main/webapp/resources/scripts/gui/illusion-event-participants.js
@@ -2,6 +2,82 @@
   'use strict';
   
   $(document).ready(function() { 
+    $.ajax(CONTEXTPATH + '/rest/illusion/events/' + $('input[name="eventId"]').val() + '/groups/', {
+      dataType : "json",
+      error: function ( jqXHR, textStatus, errorThrown ) {
+        $('.notifications').notifications('notification', 'error', textStatus);
+      },
+      success : function(groups) {
+        $('.illusion-participants-send-mail').click(function (event) {
+          dust.render("illusion-event-participants-send-mail", {
+            groups: groups
+          }, function(err, html) {
+            if (!err) {
+              var dialog = $(html);
+              dialog.dialog({
+                modal: true,
+                width: 600,
+                buttons: [{
+                  'text': dialog.data('send-button'),
+                  'click': function(event) {
+                    var recipients = $('input[name="recipients"]:checked').val();
+                    var mailSubject = $(this).find('input[name="mail-subject"]').val();
+                    var mailContent = $(this).find('textarea[name="mail-content"]').val();
+                    var eventUrlName = $('input[name="eventUrlName"]').val();
+                    
+                    if (!mailSubject) {
+                      $('.notifications').notifications('notification', 'warning', $(this).find('input[name="mail-subject"]').data('required-message'));
+                      $(this).find('input[name="mail-subject"]').focus();
+                    } else if (!mailContent) {
+                      $('.notifications').notifications('notification', 'warning', $(this).find('textarea[name="mail-content"]').data('required-message'));
+                      $(this).find('textarea[name="mail-content"]').focus();
+                    } 
+    
+                    if (mailSubject && mailContent) {
+                      var waitDialog = $('<div>')
+                        .attr('title', dialog.data('wait-title'))
+                        .addClass('illusion-event-mailing-dialog')
+                        .append($('<p>').html(dialog.data('wait-text')))
+                        .dialog({
+                          modal: true,
+                          buttons: { }
+                        });
+
+                      $.ajax(CONTEXTPATH + '/illusion/eventMail/' + eventUrlName, {
+                        type: 'POST',
+                        traditional: true,
+                        data: {
+                          'recipients': recipients,
+                          'mailSubject': mailSubject,
+                          'mailContent': mailContent
+                        },
+                        complete: function (jqXHR, textStatus) {
+                          $(dialog).dialog('close');
+                          $(waitDialog).dialog("close");
+                        },
+                        error: function ( jqXHR, textStatus, errorThrown ) {
+                          $('.notifications').notifications('notification', 'error', textStatus);
+                        },
+                        success: function () {
+                          window.location.reload(true);
+                        }
+                      });
+                    }
+                  }
+                }, {
+                  'text': dialog.data('cancel-button'),
+                  'click': function(event) {
+                    $(this).dialog("close");
+                  }
+                }]
+              });
+            } else {
+              $('.notifications').notifications('notification', 'error', err);
+            }
+          });
+        });
+      }
+    });
     
     $('.illusion-invite-participants').click(function (event) {
       dust.render("illusion-event-invite-participants", { 
