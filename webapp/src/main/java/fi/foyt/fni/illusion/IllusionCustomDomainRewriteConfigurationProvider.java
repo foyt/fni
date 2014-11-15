b283f04ab61df16559490342071ab05794a562c5
Antti Leppä <antti.leppa@foyt.fi>
2014-11-15 20:18:48 +0200

    ** Refactored menu urls to point back to main site when using custom domain

--- webapp/src/main/java/fi/foyt/fni/illusion/IllusionCustomDomainRewriteConfigurationProvider.java
+++ webapp/src/main/java/fi/foyt/fni/illusion/IllusionCustomDomainRewriteConfigurationProvider.java
@@ -16,9 +16,13 @@ import org.ocpsoft.rewrite.servlet.config.PathAndQuery;
 import org.ocpsoft.rewrite.servlet.config.Substitute;
 
 import fi.foyt.fni.persistence.model.illusion.IllusionEvent;
+import fi.foyt.fni.system.SystemSettingsController;
 
 @RewriteConfiguration
 public class IllusionCustomDomainRewriteConfigurationProvider extends HttpConfigurationProvider {
+
+  @Inject
+  private SystemSettingsController systemSettingsController;
   
   @Inject
   private IllusionEventController illusionEventController;
@@ -30,6 +34,16 @@ public class IllusionCustomDomainRewriteConfigurationProvider extends HttpConfig
     for (IllusionEvent event : illusionEventController.listIllusionEventsWithDomain()) {
       addCustomDomainForwards(configuration, event.getDomain(), event.getUrlName());
     }
+    
+    String siteHost = systemSettingsController.getSiteHost();
+    String siteUrl = systemSettingsController.getSiteUrl(false, true);
+
+    addOutboundSiteRule(configuration, siteHost, siteUrl, "/index.jsf", "/");
+    addOutboundSiteRule(configuration, siteHost, siteUrl, "/forge/index.jsf", "/forge");
+    addOutboundSiteRule(configuration, siteHost, siteUrl, "/illusion/index.jsf", "/illusion");
+    addOutboundSiteRule(configuration, siteHost, siteUrl, "/gamelibrary/index.jsf", "/gamelibrary");
+    addOutboundSiteRule(configuration, siteHost, siteUrl, "/forum/index.jsf", "/forum");
+    addOutboundSiteRule(configuration, siteHost, siteUrl, "/about.jsf", "/about");
     
     return configuration;
   }
@@ -71,12 +85,12 @@ public class IllusionCustomDomainRewriteConfigurationProvider extends HttpConfig
     String forwardTo = forwardToBuilder.toString();
     
     configuration.addRule()
-    .when(
-        Direction.isInbound()
-          .and(Domain.matches(domain))
-          .and(Path.matches(path))
-    )
-    .perform(Substitute.with(forwardTo).and(Log.message(Level.DEBUG, String.format("Custom domain inbound substitute %s -> %s", path, forwardTo))));
+      .when(
+          Direction.isInbound()
+            .and(Domain.matches(domain))
+            .and(Path.matches(path))
+      )
+      .perform(Substitute.with(forwardTo).and(Log.message(Level.DEBUG, String.format("Custom domain inbound substitute %s -> %s", path, forwardTo))));
   }
 
   private void addOutboundDomainRule(ConfigurationBuilder configuration, String domain, String jsfRule, String path) {
@@ -86,6 +100,16 @@ public class IllusionCustomDomainRewriteConfigurationProvider extends HttpConfig
         .and(jsfRule.contains("?") ? PathAndQuery.matches(jsfRule) : Path.matches(jsfRule))
       )
       .perform(Substitute.with(path).and(Log.message(Level.DEBUG, String.format("Custom domain outbound substitute %s -> %s", jsfRule, path))))
+      .withPriority(0);
+  }
+
+  private void addOutboundSiteRule(ConfigurationBuilder configuration, String siteHost, String siteUrl, String jsfRule, String path) {
+    configuration.addRule()
+      .when(Direction.isOutbound()
+        .andNot(Domain.matches(siteHost))
+        .and(jsfRule.contains("?") ? PathAndQuery.matches(jsfRule) : Path.matches(jsfRule))
+      )
+      .perform(Substitute.with(siteUrl + path).and(Log.message(Level.DEBUG, String.format("Site outbound substitute %s -> %s", jsfRule, siteUrl + path))))
       .withPriority(0);
   }
  
