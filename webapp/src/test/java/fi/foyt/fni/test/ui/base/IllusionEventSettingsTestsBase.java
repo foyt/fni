5bab3fa38c37c5d1578502e0e8f74821d87f18ef
Antti Leppä <antti.leppa@foyt.fi>
2014-11-19 17:13:50 +0200

    ** Added custom url tests for event settings view

--- webapp/src/test/java/fi/foyt/fni/test/ui/base/IllusionEventSettingsTestsBase.java
+++ webapp/src/test/java/fi/foyt/fni/test/ui/base/IllusionEventSettingsTestsBase.java
@@ -1,11 +1,24 @@
 package fi.foyt.fni.test.ui.base;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import fi.foyt.fni.test.DefineSqlSet;
+import fi.foyt.fni.test.DefineSqlSets;
 import fi.foyt.fni.test.SqlAfter;
 import fi.foyt.fni.test.SqlBefore;
+import fi.foyt.fni.test.SqlSets;
 
-public class IllusionEventSettingsTestsBase extends AbstractUITest {
+@DefineSqlSets ({
+  @DefineSqlSet (id = "illusion-event-custom", 
+    before = { "illusion-basic-setup.sql", "illusion-event-open-setup.sql", "illusion-event-open-custom-setup.sql", "illusion-event-open-organizer-setup.sql" },
+    after = { "illusion-event-open-organizer-teardown.sql", "illusion-event-open-custom-teardown.sql", "illusion-event-open-teardown.sql", "illusion-basic-teardown.sql" }
+  )
+})
+public class IllusionEventSettingsTestsBase extends AbstractIllusionUITest {
   
   @Test
   @SqlBefore ({"illusion-basic-setup.sql", "illusion-event-open-setup.sql"})
@@ -108,6 +121,63 @@ public class IllusionEventSettingsTestsBase extends AbstractUITest {
     typeSelectorInputValue(".illusion-event-settings-location", location);
     clickSelector(".illusion-event-settings-save");
     assertSelectorValue(".illusion-event-settings-location", location);
+  }
+  
+  @Test
+  @SqlSets ("illusion-event-custom")
+  public void testCustomDomainNotLoggedIn() {
+    getWebDriver().get(getCustomEventUrl());
+    loginCustomEvent("admin@foyt.fi", "pass");
+    getWebDriver().get(getCustomEventUrl() + "/settings");
+    testTitle("Event Settings");
+  }
+  
+  @Test
+  @SqlSets ("illusion-event-custom")
+  public void testCustomDomainLoginRedirect() {
+    getWebDriver().get(getCustomEventUrl() + "/settings");
+    waitForUrlMatches(".*/login.*");
+    loginCustomEvent("admin@foyt.fi", "pass");
+    testTitle("Event Settings");
+  }
+  
+  @Test
+  @SqlSets ("illusion-event-custom")
+  public void testCustomDomainMenuItems() {
+    getWebDriver().get(getCustomEventUrl());
+    loginCustomEvent("admin@foyt.fi", "pass");
+    getWebDriver().get(getCustomEventUrl() + "/settings");
+    testTitle("Event Settings");
+
+    WebElement logoLink = getWebDriver().findElement(By.cssSelector(".index-menu>a:first-child"));
+    WebElement forgeMenuLink = getWebDriver().findElement(By.cssSelector(".index-menu .menu-navigation-container>a:nth-child(1)"));
+    WebElement illusionMenuLink = getWebDriver().findElement(By.cssSelector(".index-menu .menu-navigation-container>a:nth-child(2)"));
+    WebElement gameLibraryMenuLink = getWebDriver().findElement(By.cssSelector(".index-menu .menu-navigation-container>a:nth-child(3)"));
+    WebElement forumMenuLink = getWebDriver().findElement(By.cssSelector(".index-menu .menu-navigation-container>a:nth-child(4)"));
+
+    assertEquals("Forge", forgeMenuLink.getText());
+    assertEquals("Illusion", illusionMenuLink.getText());
+    assertEquals("Game Library", gameLibraryMenuLink.getText());
+    assertEquals("Forum", forumMenuLink.getText());
+
+    assertEquals(getAppUrl() + "/", stripLinkJSessionId(logoLink.getAttribute("href")));
+    assertEquals(getAppUrl() + "/forge", stripLinkJSessionId(forgeMenuLink.getAttribute("href")));
+    assertEquals(getAppUrl() + "/illusion", stripLinkJSessionId(illusionMenuLink.getAttribute("href")));
+    assertEquals(getAppUrl() + "/gamelibrary", stripLinkJSessionId(gameLibraryMenuLink.getAttribute("href")));
+    assertEquals(getAppUrl() + "/forum", stripLinkJSessionId(forumMenuLink.getAttribute("href")));
+  }
+  
+  @Test
+  @SqlSets ("illusion-event-custom")
+  public void testCustomDomainNavigationLinks() {
+    getWebDriver().get(getCustomEventUrl());
+    loginCustomEvent("admin@foyt.fi", "pass");
+    getWebDriver().get(getCustomEventUrl() + "/settings");
+    testTitle("Event Settings");
+
+    assertEquals(getAppUrl() + "/", findElementBySelector(".view-header-navigation .view-header-navigation-item:nth-child(1)").getAttribute("href"));
+    assertEquals(getAppUrl() + "/illusion", findElementBySelector(".view-header-navigation .view-header-navigation-item:nth-child(3)").getAttribute("href"));
+    assertEquals(getCustomEventUrl() + "/settings", findElementBySelector(".view-header-navigation .view-header-navigation-item:nth-child(5)").getAttribute("href"));
   }
   
 }
