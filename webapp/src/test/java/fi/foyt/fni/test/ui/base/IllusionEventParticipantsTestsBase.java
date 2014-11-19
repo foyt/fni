41d403dd2c460cad02ca8d7effbcf693fd1cc9db
Antti Leppä <antti.leppa@foyt.fi>
2014-11-19 17:13:35 +0200

    ** Added custom url tests for event participants view

--- webapp/src/test/java/fi/foyt/fni/test/ui/base/IllusionEventParticipantsTestsBase.java
+++ webapp/src/test/java/fi/foyt/fni/test/ui/base/IllusionEventParticipantsTestsBase.java
@@ -1,11 +1,24 @@
 package fi.foyt.fni.test.ui.base;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import fi.foyt.fni.test.DefineSqlSet;
+import fi.foyt.fni.test.DefineSqlSets;
 import fi.foyt.fni.test.SqlAfter;
 import fi.foyt.fni.test.SqlBefore;
+import fi.foyt.fni.test.SqlSets;
 
-public class IllusionEventParticipantsTestsBase extends AbstractUITest {
+@DefineSqlSets ({
+  @DefineSqlSet (id = "illusion-event-custom", 
+    before = { "illusion-basic-setup.sql", "illusion-event-open-setup.sql", "illusion-event-open-custom-setup.sql", "illusion-event-open-organizer-setup.sql" },
+    after = { "illusion-event-open-organizer-teardown.sql", "illusion-event-open-custom-teardown.sql", "illusion-event-open-teardown.sql", "illusion-basic-teardown.sql" }
+  )
+})
+public class IllusionEventParticipantsTestsBase extends AbstractIllusionUITest {
   
   @Test
   @SqlBefore ({"illusion-basic-setup.sql", "illusion-event-open-setup.sql"})
@@ -56,6 +69,64 @@ public class IllusionEventParticipantsTestsBase extends AbstractUITest {
     assertSelectorCount(".illusion-event-navigation-admin-menu .illusion-event-navigation-item.illusion-event-navigation-item-active", 1);
     assertSelectorPresent(".illusion-event-navigation-admin-menu");
     assertSelectorTextIgnoreCase(".illusion-event-navigation-admin-menu .illusion-event-navigation-item.illusion-event-navigation-item-active", "participants");
+  }
+  
+  @Test
+  @SqlSets ("illusion-event-custom")
+  public void testCustomDomainNotLoggedIn() {
+    getWebDriver().get(getCustomEventUrl());
+    loginCustomEvent("admin@foyt.fi", "pass");
+    getWebDriver().get(getCustomEventUrl() + "/participants");
+    testTitle("Illusion - Open Event");
+  }
+  
+  @Test
+  @SqlSets ("illusion-event-custom")
+  public void testCustomDomainLoginRedirect() {
+    getWebDriver().get(getCustomEventUrl() + "/participants");
+    waitForUrlMatches(".*/login.*");
+    loginCustomEvent("admin@foyt.fi", "pass");
+    testTitle("Illusion - Open Event");
+  }
+  
+  @Test
+  @SqlSets ("illusion-event-custom")
+  public void testCustomDomainMenuItems() {
+    getWebDriver().get(getCustomEventUrl());
+    loginCustomEvent("admin@foyt.fi", "pass");
+    getWebDriver().get(getCustomEventUrl() + "/participants");
+    testTitle("Illusion - Open Event");
+
+    WebElement logoLink = getWebDriver().findElement(By.cssSelector(".index-menu>a:first-child"));
+    WebElement forgeMenuLink = getWebDriver().findElement(By.cssSelector(".index-menu .menu-navigation-container>a:nth-child(1)"));
+    WebElement illusionMenuLink = getWebDriver().findElement(By.cssSelector(".index-menu .menu-navigation-container>a:nth-child(2)"));
+    WebElement gameLibraryMenuLink = getWebDriver().findElement(By.cssSelector(".index-menu .menu-navigation-container>a:nth-child(3)"));
+    WebElement forumMenuLink = getWebDriver().findElement(By.cssSelector(".index-menu .menu-navigation-container>a:nth-child(4)"));
+
+    assertEquals("Forge", forgeMenuLink.getText());
+    assertEquals("Illusion", illusionMenuLink.getText());
+    assertEquals("Game Library", gameLibraryMenuLink.getText());
+    assertEquals("Forum", forumMenuLink.getText());
+
+    assertEquals(getAppUrl() + "/", stripLinkJSessionId(logoLink.getAttribute("href")));
+    assertEquals(getAppUrl() + "/forge", stripLinkJSessionId(forgeMenuLink.getAttribute("href")));
+    assertEquals(getAppUrl() + "/illusion", stripLinkJSessionId(illusionMenuLink.getAttribute("href")));
+    assertEquals(getAppUrl() + "/gamelibrary", stripLinkJSessionId(gameLibraryMenuLink.getAttribute("href")));
+    assertEquals(getAppUrl() + "/forum", stripLinkJSessionId(forumMenuLink.getAttribute("href")));
+  }
+  
+  @Test
+  @SqlSets ("illusion-event-custom")
+  public void testCustomDomainNavigationLinks() {
+    getWebDriver().get(getCustomEventUrl());
+    loginCustomEvent("admin@foyt.fi", "pass");
+    getWebDriver().get(getCustomEventUrl() + "/participants");
+    testTitle("Illusion - Open Event");
+
+    assertEquals(getAppUrl() + "/", findElementBySelector(".view-header-navigation .view-header-navigation-item:nth-child(1) a").getAttribute("href"));
+    assertEquals(getAppUrl() + "/illusion", findElementBySelector(".view-header-navigation .view-header-navigation-item:nth-child(3) a").getAttribute("href"));
+    assertEquals(getCustomEventUrl() + "/", findElementBySelector(".view-header-navigation .view-header-navigation-item:nth-child(5) a").getAttribute("href"));
+    assertEquals(getCustomEventUrl() + "/participants", findElementBySelector(".view-header-navigation .view-header-navigation-item:nth-child(7) a").getAttribute("href"));
   }
   
 }
