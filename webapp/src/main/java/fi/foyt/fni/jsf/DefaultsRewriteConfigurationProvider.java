74db662527e07031e0cc7e0f1ea7691fd6e665a8
Antti Leppä <antti.leppa@foyt.fi>
2014-12-02 14:47:07 +0200

    ** Added jsf rule for refering theme files outside of jsf more easily

--- webapp/src/main/java/fi/foyt/fni/jsf/DefaultsRewriteConfigurationProvider.java
+++ webapp/src/main/java/fi/foyt/fni/jsf/DefaultsRewriteConfigurationProvider.java
@@ -5,6 +5,8 @@ import javax.servlet.ServletContext;
 import org.ocpsoft.rewrite.config.Configuration;
 import org.ocpsoft.rewrite.config.ConfigurationBuilder;
 import org.ocpsoft.rewrite.servlet.config.HttpConfigurationProvider;
+import org.ocpsoft.rewrite.servlet.config.Path;
+import org.ocpsoft.rewrite.servlet.config.Substitute;
 import org.ocpsoft.rewrite.servlet.config.rule.Join;
 
 public class DefaultsRewriteConfigurationProvider extends HttpConfigurationProvider {
@@ -17,7 +19,12 @@ public class DefaultsRewriteConfigurationProvider extends HttpConfigurationProvi
       .addRule(Join.path("/login").to("/users/login.jsf"))
       .addRule(Join.path("/forge").to("/forge/index.jsf"))
       .addRule(Join.path("/gamelibrary").to("/gamelibrary/index.jsf"));
-
+    
+    configuration.addRule()
+      .when(Path.matches("/theme/{file}"))
+      .perform(Substitute.with("/javax.faces.resource/{file}.jsf?ln=novus"))
+      .where("file").matches("[a-zA-Z0-9/_.\\-]*");
+    
     return configuration;
   }
 
