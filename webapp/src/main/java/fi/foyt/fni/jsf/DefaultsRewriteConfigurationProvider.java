743f5ea5437cc454f6fca3936e3fa57ffb77debb
Antti Leppä <antti.leppa@foyt.fi>
2014-12-06 19:12:46 +0200

    ** Fixed bug in logout that prevented logging out of custom domain events ** Fixed bug that prevented using unslashed version of login url in custom events

--- webapp/src/main/java/fi/foyt/fni/jsf/DefaultsRewriteConfigurationProvider.java
+++ webapp/src/main/java/fi/foyt/fni/jsf/DefaultsRewriteConfigurationProvider.java
@@ -4,10 +4,16 @@ import javax.servlet.ServletContext;
 
 import org.ocpsoft.rewrite.config.Configuration;
 import org.ocpsoft.rewrite.config.ConfigurationBuilder;
+import org.ocpsoft.rewrite.config.Direction;
+import org.ocpsoft.rewrite.context.EvaluationContext;
 import org.ocpsoft.rewrite.servlet.config.HttpConfigurationProvider;
+import org.ocpsoft.rewrite.servlet.config.HttpOperation;
 import org.ocpsoft.rewrite.servlet.config.Path;
+import org.ocpsoft.rewrite.servlet.config.Query;
+import org.ocpsoft.rewrite.servlet.config.Redirect;
 import org.ocpsoft.rewrite.servlet.config.Substitute;
 import org.ocpsoft.rewrite.servlet.config.rule.Join;
+import org.ocpsoft.rewrite.servlet.http.event.HttpServletRewrite;
 
 public class DefaultsRewriteConfigurationProvider extends HttpConfigurationProvider {
   
@@ -16,9 +22,21 @@ public class DefaultsRewriteConfigurationProvider extends HttpConfigurationProvi
     ConfigurationBuilder configuration = ConfigurationBuilder.begin();
 
     configuration
-      .addRule(Join.path("/login").to("/users/login.jsf"))
       .addRule(Join.path("/forge").to("/forge/index.jsf"))
       .addRule(Join.path("/gamelibrary").to("/gamelibrary/index.jsf"));
+    
+    configuration.addRule()
+      .when(Direction.isInbound().and(Path.matches("/login")).and(Query.matches("{query}")))
+      .perform(Redirect.temporary(context.getContextPath() + "/login/?{query}"));
+    
+    configuration.addRule()
+      .when(Direction.isInbound().and(Path.matches("/logout")))
+      .perform(new HttpOperation() {
+         @Override
+         public void performHttp(HttpServletRewrite event, EvaluationContext context) {
+           event.getRequest().getSession().invalidate();
+         }
+      }.and(Redirect.temporary(context.getContextPath() + "/")));
     
     configuration.addRule()
       .when(Path.matches("/theme/{file}"))
