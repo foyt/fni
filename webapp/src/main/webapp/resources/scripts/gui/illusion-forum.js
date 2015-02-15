412e62eef8a4b58d0fcbfcbb7d0321fb9a86f65e
Antti Leppä <antti.leppa@foyt.fi>
2015-02-15 20:12:08 +0200

    ** Added support for editing Illusion forum posts

--- webapp/src/main/webapp/resources/scripts/gui/illusion-forum.js
+++ webapp/src/main/webapp/resources/scripts/gui/illusion-forum.js
@@ -1,31 +1,127 @@
 (function() {
   'use strict';
   
-  $(document).ready(function() {
-    var editor = CKEDITOR.replace($('.illusion-forum-post-editor')[0], {
-      toolbar: [
-         { name: 'clipboard',   items : [ 'Cut','Copy','Paste','-','Undo','Redo' ] },
-         { name: 'editing',     items : [ 'Find','Replace','-','SelectAll','-','Scayt' ] },
-         { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
-         { name: 'paragraph',   items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
-         { name: 'links',       items : [ 'Link','Unlink' ] },
-         { name: 'insert',      items : [ 'Image','SpecialChar' ] },
-         { name: 'tools',       items : [ 'Maximize' ] }
-       ],
-       contentsCss: ['//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.2/contents.css', CONTEXTPATH + '/forum/ckcontents.css' ]
-    });
+  $.widget("custom.forumPostEditor", {
+    _create : function() {
+      this._editor = CKEDITOR.replace(this.element[0], {
+        toolbar: [
+           { name: 'clipboard',   items : [ 'Cut','Copy','Paste','-','Undo','Redo' ] },
+           { name: 'editing',     items : [ 'Find','Replace' ] },
+           { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+           { name: 'paragraph',   items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
+           { name: 'links',       items : [ 'Link','Unlink' ] },
+           { name: 'insert',      items : [ 'Image','SpecialChar' ] },
+           { name: 'tools',       items : [ 'Maximize' ] }
+         ],
+         contentsCss: ['//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.2/contents.css', CONTEXTPATH + '/forum/ckcontents.css' ]
+      });
+    },
     
+    data: function () {
+      return this._editor.getData();
+    },
+    
+    _destroy : function() {
+      this._editor.destroy();
+    }
+  });
+  
+  $.widget("custom.forumPost", {
+    options: {
+      eventId: null
+    },
+    
+    _create : function() {
+      this.element.find('.illusion-forum-post-content-save').click($.proxy(this._onSaveClick, this));
+      this.element.find('.illusion-forum-post-edit').click($.proxy(this._onEditClick, this));
+    },
+    
+    id: function () {
+      return this.element.attr('data-post-id');  
+    },
+    
+    reload: function (callback) {
+      $.ajax(CONTEXTPATH + '/rest/illusion/events/' + this.options.eventId + '/forumPosts/' + this.id(), {
+        type: 'GET',
+        contentType: "application/json",
+        dataType : "json",
+        accepts: {
+          'json' : 'application/json'
+        },
+        success : $.proxy(function(data) {
+          var modified = this.element.find('.illusion-forum-post-modified');
+          var pattern = $('.illusion-forum-posts').attr('data-date-format-long');
+
+          modified.html(formatJavaLocale(modified.attr('data-locale'), new Date(Date.parse(data.modified))));
+          this.element.find('.illusion-forum-post-content').html(data.content);
+          
+          if ($.isFunction(callback)) {
+            callback();
+          }
+        }, this)
+      });
+    },
+    
+    save: function (callback) {
+      $.ajax(CONTEXTPATH + '/rest/illusion/events/' + this.options.eventId + '/forumPosts/' + this.id(), {
+        type: 'PUT',
+        contentType: "application/json",
+        dataType : "json",
+        data: JSON.stringify({
+          'content': this.element.find('.illusion-forum-post-content-editor').forumPostEditor('data')
+        }),
+        accepts: {
+          'json' : 'application/json'
+        },
+        success : function(data) {
+          if ($.isFunction(callback)) {
+            callback();
+          }
+        }
+      });
+    },
+    
+    edit: function () {
+      this.element.find('.illusion-forum-post-content, .illusion-forum-post-footer').hide();
+      this.element.find('.illusion-forum-post-content-save').show();
+      this.element.find('.illusion-forum-post-content-editor').show().forumPostEditor();
+    },
+    
+    closeEditor: function () {
+      this.element.find('.illusion-forum-post-content-save').hide();
+      this.element.find('.illusion-forum-post-content-editor').forumPostEditor('destroy').hide();
+      this.element.find('.illusion-forum-post-content, .illusion-forum-post-footer').show();
+    },
+    
+    _onEditClick: function () {
+      this.edit();
+    },
+    
+    _onSaveClick: function (event) {
+      this.save($.proxy(function () {
+        this.closeEditor();
+        this.reload();
+      }, this));
+    },
+    
+    _destroy : function() {
+      
+    }
+  });
+  
+  $(document).ready(function() {
+    var eventId = $('#event-id').val()
+    
+    $('.illusion-forum-post-editor').forumPostEditor();
     $('.illusion-forum-post-reply').click(function (event) {
       event.preventDefault();
-      
-      var eventId = $('#event-id').val()
       
       $.ajax(CONTEXTPATH + '/rest/illusion/events/' + eventId + '/forumPosts/', {
         type: 'POST',
         contentType: "application/json",
         dataType : "json",
         data: JSON.stringify({
-          'content': editor.getData()
+          'content': $('.illusion-forum-post-editor').forumPostEditor('data')
         }),
         accepts: {
           'json' : 'application/json'
@@ -35,6 +131,10 @@
         }
       });
     });
+    
+    $('.illusion-forum-post').forumPost({
+      eventId: eventId
+    });
   });
-  
+
 }).call(this);
