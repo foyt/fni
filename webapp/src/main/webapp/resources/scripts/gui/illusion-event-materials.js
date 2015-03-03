e8b597f4e6e76bd5fdc0760a96ae4283c046970f
Antti Leppä <antti.leppa@foyt.fi>
2015-03-03 06:35:28 +0200

    ** Added required format parameter into character sheet link of Illusion event materials

--- webapp/src/main/webapp/resources/scripts/gui/illusion-event-materials.js
+++ webapp/src/main/webapp/resources/scripts/gui/illusion-event-materials.js
@@ -6,7 +6,7 @@
       event.preventDefault();
       var id = $(event.target).closest('.illusion-event-material').data('id');
       var eventId = $('#event-id').val();
-      window.location.href = CONTEXTPATH + '/rest/illusion/events/' + eventId + '/characterSheets/' + id + '/data';
+      window.location.href = CONTEXTPATH + '/rest/illusion/events/' + eventId + '/characterSheets/' + id + '/data?format=XLS';
     });
   });
 
