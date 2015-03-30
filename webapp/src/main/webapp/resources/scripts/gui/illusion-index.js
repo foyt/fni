8c93351435995c12678a5f4429d0cbf0f8534384
Antti Leppä <antti.leppa@foyt.fi>
2015-03-30 10:41:50 +0300

    ** Added calendar view into Illusion index page

--- webapp/src/main/webapp/resources/scripts/gui/illusion-index.js
+++ webapp/src/main/webapp/resources/scripts/gui/illusion-index.js
@@ -1,8 +1,39 @@
 (function() {
   'use strict';
   
+  var illusionClient = new $.RestClient(CONTEXTPATH + '/rest/illusion/');
+  illusionClient.add('events');
+  
   $(document).ready(function() { 
     $( "#tabs" ).tabs();
+    
+    illusionClient.events.read().done(function (events, textStatus, xhrObject){
+      switch (xhrObject.status) {
+        case 200:
+          var source = $.map(events, function (event) {
+            return {
+              title: event.name,
+              start: new Date(Date.parse(event.start)),
+              end: new Date(Date.parse(event.end)),
+              url: CONTEXTPATH + '/illusion/event/' + event.urlName
+            };
+          });
+          
+          $('.calendar-view').fullCalendar({
+            eventSources: [ {
+              events: source,
+              color: '#000',
+              textColor: '#fff'
+            }]
+          });
+        break;
+        case 204:
+        break;
+        default:
+          $('.notifications').notifications('notification', 'error', textStatus);
+        break;
+      }
+    });
   });
 
 }).call(this);
