0932ab3385878908b9f4f97c1e969fabc3e9f359
Antti Leppä <antti.leppa@foyt.fi>
2015-04-01 19:28:02 +0300

    ** Reduced Sauce test to chrome / Linux

--- itests/src/test/java/fi/foyt/fni/test/ui/sauce/SauceLabsUtils.java
+++ itests/src/test/java/fi/foyt/fni/test/ui/sauce/SauceLabsUtils.java
@@ -6,11 +6,11 @@ import java.util.List;
 public class SauceLabsUtils {
 
   public static List<String[]> getDefaultSauceBrowsers() {
-    return Arrays.asList(
-      ((String[]) new String[] { "firefox", "36.0", "Windows 8.1" }),
-      ((String[]) new String[] { "safari", "8.0", "OS X 10.10" }),
+    return Arrays.asList(new String[][]{
+//      ((String[]) new String[] { "firefox", "36.0", "Windows 8.1" }),
+//      ((String[]) new String[] { "safari", "8.0", "OS X 10.10" }),
       ((String[]) new String[] { "chrome", "41.0", "Linux" })
-    );
+    });
   }
   
 }
