bb60b3e023d77f2712d4fab793054e3d4f05ea7d
Antti Leppä <antti.leppa@foyt.fi>
2015-07-04 12:56:37 +0300

    ** Added support for loading language neutral dust templates ** Added iterate dust helper ** Added support for lowerCase and upperCase dust filters

--- webapp/src/main/webapp/resources/scripts/gui/dust.js
+++ webapp/src/main/webapp/resources/scripts/gui/dust.js
@@ -5,6 +5,20 @@
     $.ajax(CONTEXTPATH + '/resources/dust/' + name + '-' + LOCALE + '.dust', {
       success : function(data, textStatus, jqXHR) {
         callback(false, data);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        if (jqXHR.status == 404) {
+          $.ajax(CONTEXTPATH + '/resources/dust/' + name + '.dust', {
+            success : function(data, textStatus, jqXHR) {
+              callback(false, data);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+              $('.notifications').notifications('notification', 'error', textStatus);
+            }
+          });          
+        } else {
+          $('.notifications').notifications('notification', 'error', textStatus);
+        }
       }
     });
   };
@@ -25,5 +39,30 @@
 
     return chunk.write(baseUrl + CONTEXTPATH);
   };
+  
+  dust.helpers.iterate = function(chunk, context, bodies, params) {
+    var obj = (params||{})['obj'];
+    for (var k in obj) {
+      chunk = chunk.render(bodies.block, context.push({key: k, value: obj[k]}));
+    }
+    
+    return chunk;
+  }
+  
+  dust.filters.lowerCase = function(value) {
+    if (typeof value === 'string') {
+      return value.toLowerCase();
+    }
+    
+    return value;
+  }
+  
+  dust.filters.upperCase = function(value) {
+    if (typeof value === 'string') {
+      return value.toUpperCase();
+    }
+    
+    return value;
+  }
   
 }).call(this);
