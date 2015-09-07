2de2483020255db529e3fd8c255a98fa137b240c
Antti Leppä <antti.leppa@foyt.fi>
2015-09-07 18:46:13 +0300

    ** Added tests for forum index urls and fixed a bug discovered by the tests

--- itests/src/test/java/fi/foyt/fni/test/ui/base/ForumIndexTestsBase.java
+++ itests/src/test/java/fi/foyt/fni/test/ui/base/ForumIndexTestsBase.java
@@ -1,10 +1,11 @@
 package fi.foyt.fni.test.ui.base;
 
+import static org.junit.Assert.assertEquals;
+
 import java.sql.SQLException;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import fi.foyt.fni.test.DefineSqlSet;
 import fi.foyt.fni.test.DefineSqlSets;
 import fi.foyt.fni.test.SqlSets;
@@ -30,6 +31,38 @@ public class ForumIndexTestsBase extends AbstractUITest {
     navigate("/forum/");
     assertSelectorCount(".forum", 4);
     assertEquals(5, countForums().intValue());
+  }
+  
+  @Test
+  @SqlSets ({"basic-users", "forum-basic"})
+  public void testForumLink() {
+    navigate("/forum/");
+    assertSelectorTextIgnoreCase("*[data-forum-index=\"2\"] h3 a", "Five topic Forum");
+    assertEquals(String.format("%s/forum/5_topic_forum", getAppUrl()), findElementBySelector("*[data-forum-index=\"2\"] h3 a").getAttribute("href"));
+  }
+
+  @Test
+  @SqlSets ({"basic-users", "forum-basic"})
+  public void testForumMoreLink() {
+    navigate("/forum/");
+    assertSelectorTextIgnoreCase("*[data-forum-index=\"2\"] .more-link", "More >>");
+    assertEquals(String.format("%s/forum/5_topic_forum", getAppUrl()), findElementBySelector("*[data-forum-index=\"2\"] .more-link").getAttribute("href"));
+  }
+  
+  @Test
+  @SqlSets ({"basic-users", "forum-basic"})
+  public void testTopicLink() {
+    navigate("/forum/");
+    assertSelectorTextIgnoreCase("*[data-forum-index=\"2\"] *[data-topic-index=\"0\"] h5 a", "Topic 5 of 5 topic forum");
+    assertEquals(String.format("%s/forum/5_topic_forum/topic5of5", getAppUrl()), findElementBySelector("*[data-forum-index=\"2\"] *[data-topic-index=\"0\"] h5 a").getAttribute("href"));
+  }
+  
+  @Test
+  @SqlSets ({"basic-users", "forum-basic"})
+  public void testAuthorLink() {
+    navigate("/forum/");
+    assertSelectorTextIgnoreCase("*[data-forum-index=\"2\"] *[data-topic-index=\"0\"] .topic-start-info a", "Test Guest");
+    assertEquals(String.format("%s/profile/1", getAppUrl()), findElementBySelector("*[data-forum-index=\"2\"] *[data-topic-index=\"0\"] .topic-start-info a").getAttribute("href"));
   }
 
 }
