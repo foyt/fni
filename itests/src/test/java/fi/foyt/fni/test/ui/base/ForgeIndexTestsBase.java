0d3cf9f685dc12bce937fb26163fd1a01ea043f2
Antti Leppä <antti.leppa@foyt.fi>
2015-10-11 21:10:28 +0300

    ** Fixed ForgeIndexTestsIT#testRemoveDialogLongText to work with ie 11

--- itests/src/test/java/fi/foyt/fni/test/ui/base/ForgeIndexTestsBase.java
+++ itests/src/test/java/fi/foyt/fni/test/ui/base/ForgeIndexTestsBase.java
@@ -1,13 +1,6 @@
 package fi.foyt.fni.test.ui.base;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import fi.foyt.fni.test.DefineSqlSet;
 import fi.foyt.fni.test.DefineSqlSets;
@@ -33,21 +26,15 @@ public class ForgeIndexTestsBase extends AbstractUITest {
   @Test
   @SqlSets ({"basic-materials-users"})
   public void testRemoveDialogLongText() {
-    loginInternal(getWebDriver(), "user@foyt.fi", "pass");
-    getWebDriver().get(getAppUrl() + "/forge/");
-
-    new Actions(getWebDriver()).moveToElement(getWebDriver().findElement(By.cssSelector(".forge-material[data-material-id=\"15\"] .forge-material-info"))).build().perform();
-    WebElement deleteLink = getWebDriver().findElement(By.cssSelector(".forge-material-action-delete a[data-material-id=\"15\"]"));
-    new WebDriverWait(getWebDriver(), 60).until(ExpectedConditions.visibilityOf(deleteLink));
-    deleteLink.click();
-
-    new WebDriverWait(getWebDriver(), 60).until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(".forge-remove-material-dialog")));
-
-    assertEquals("Remove 'Beowulf pohti zuluja ja ång...'?", getWebDriver().findElement(By.cssSelector(".ui-dialog-title")).getText());
-    assertEquals("Do you really wish to remove 'Beowulf pohti zuluja ja ångström-yksikköä katsellessaan Q-stone- ja CMX-yhtyeitä videolta.'",
-        getWebDriver().findElement(By.cssSelector(".forge-remove-material-dialog p")).getText());
-
+    loginInternal("user@foyt.fi", "pass");
+    navigate("/forge/");
+    waitAndClick(".forge-material[data-material-id=\"15\"] .forge-material-info");
+    waitAndClick(".forge-material-action-delete a[data-material-id=\"15\"]");
+    waitForSelectorPresent(".forge-remove-material-dialog");
+    assertSelectorTextIgnoreCase(".ui-dialog-title", "Remove 'Beowulf pohti zuluja ja ång...'?");
+    assertSelectorTextIgnoreCase(".forge-remove-material-dialog p", "Do you really wish to remove 'Beowulf pohti zuluja ja ångström-yksikköä katsellessaan Q-stone- ja CMX-yhtyeitä videolta.'");
   }
+  
 
   @Test
   @SqlSets ({"basic-materials-users"})
