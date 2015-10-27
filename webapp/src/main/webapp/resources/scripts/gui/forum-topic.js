bbbb6020ef97c67143c9f566e1589d09ceebc079
Antti Leppä <antti.leppa@foyt.fi>
2015-10-27 21:39:00 +0200

    ** Fixed issue #316

--- webapp/src/main/webapp/resources/scripts/gui/forum-topic.js
+++ webapp/src/main/webapp/resources/scripts/gui/forum-topic.js
@@ -2,18 +2,20 @@
   'use strict';
   
   $(document).ready(function() {
-    CKEDITOR.replace($('.forum-topic-post-editor')[0], {
-      toolbar: [
-         { name: 'clipboard',   items : [ 'Cut','Copy','Paste','-','Undo','Redo' ] },
-         { name: 'editing',     items : [ 'Find','Replace','-','SelectAll','-','Scayt' ] },
-         { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
-         { name: 'paragraph',   items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
-         { name: 'links',       items : [ 'Link','Unlink' ] },
-         { name: 'insert',      items : [ 'Image','SpecialChar' ] },
-         { name: 'tools',       items : [ 'Maximize' ] }
-       ],
-       contentsCss: ['//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.2/contents.css', CONTEXTPATH + '/forum/ckcontents.css' ]
-    });
+    if ($('.forum-topic-post-editor').length) {
+      CKEDITOR.replace($('.forum-topic-post-editor')[0], {
+        toolbar: [
+           { name: 'clipboard',   items : [ 'Cut','Copy','Paste','-','Undo','Redo' ] },
+           { name: 'editing',     items : [ 'Find','Replace','-','SelectAll','-','Scayt' ] },
+           { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+           { name: 'paragraph',   items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote' ] },
+           { name: 'links',       items : [ 'Link','Unlink' ] },
+           { name: 'insert',      items : [ 'Image','SpecialChar' ] },
+           { name: 'tools',       items : [ 'Maximize' ] }
+         ],
+         contentsCss: ['//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.2/contents.css', CONTEXTPATH + '/forum/ckcontents.css' ]
+      });
+    }
   });
   
 }).call(this);
