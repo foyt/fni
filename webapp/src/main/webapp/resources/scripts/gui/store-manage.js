783cffe74ec52addc7ac106792ac49f8b85c05ea
Antti Leppä <antti.leppa@foyt.fi>
2015-12-20 23:05:48 +0200

    ** Added support for uploading images for store products

--- webapp/src/main/webapp/resources/scripts/gui/store-manage.js
+++ webapp/src/main/webapp/resources/scripts/gui/store-manage.js
@@ -1,15 +1,15 @@
 (function() {
   'use strict';
-/**
+
   $(document).ready(function () {
-    $('.gamelibrary-manage-publication-image-link').magnificPopup({ 
+    $('.store-manage-product-image-link').magnificPopup({ 
       type: 'image'
     });
   });
   
-  $(document).on('click', '.gamelibrary-manage-upload-image-link', function (event) {
-    dust.render("gamelibrary-image-upload", {
-      publicationId: $(this).data('publication-id')
+  $(document).on('click', '.store-manage-upload-image-link', function (event) {
+    dust.render("store-image-upload", {
+      productId: $(this).attr('data-product-id')
     }, function(err, html) {
       if (!err) {
         var dialog = $(html);
@@ -37,69 +37,7 @@
       }
     }); 
   });
-  
-  $(document).on('click', '.gamelibrary-manage-upload-downloadable-link', function (event) {
-    dust.render("gamelibrary-downloadable-upload", {
-      publicationId: $(this).data('publication-id')
-    }, function(err, html) {
-      if (!err) {
-        var dialog = $(html);
-        dialog.dialog({
-          modal: true,
-          width: 600,
-          buttons: [{
-            'text': dialog.data('upload-button'),
-            'click': function(event) { 
-              $(this).find('iframe').load($.proxy(function () {
-                window.location.reload();
-              }, this));
-              
-              $(this).find('form').submit();
-            }
-          }, {
-            'text': dialog.data('cancel-button'),
-            'click': function(event) { 
-              $(this).dialog("close");
-            }
-          }]
-        });
-      } else {
-        $('.notifications').notifications('notification', 'error', err);
-      }
-    }); 
-  });
-  
-  $(document).on('click', '.gamelibrary-manage-upload-printable-link', function (event) {
-    dust.render("gamelibrary-printable-upload", {
-      publicationId: $(this).data('publication-id')
-    }, function(err, html) {
-      if (!err) {
-        var dialog = $(html);
-        dialog.dialog({
-          modal: true,
-          width: 600,
-          buttons: [{
-            'text': dialog.data('upload-button'),
-            'click': function(event) { 
-              $(this).find('iframe').load($.proxy(function () {
-                window.location.reload();
-              }, this));
-              
-              $(this).find('form').submit();
-            }
-          }, {
-            'text': dialog.data('cancel-button'),
-            'click': function(event) { 
-              $(this).dialog("close");
-            }
-          }]
-        });
-      } else {
-        $('.notifications').notifications('notification', 'error', err);
-      }
-    }); 
-  });
-**/  
+
   $(document).on('click', '.store-manage-publish-link', function (event) {
     var form = $(this).closest('form');
     
