3456ae6b4761a161c5c920b23b71f485224ab2ee
Antti Leppä <antti.leppa@foyt.fi>
2015-12-28 09:39:04 +0200

    ** Moved Travis build config into travis-setenv.sh

---
+++ travis-setenv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export run_tests=false
+export start_sc_tunnel=false
+export deploy=false
+
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  export run_tests=true
+fi;
+
+if [[ ("$use_sc" = "true") && ("$run_tests" = "true") ]]; then
+  export start_sc_tunnel=true
+fi;
+
+if [[ ("$TRAVIS_PULL_REQUEST" = "false") && ("$TRAVIS_BRANCH" = "devel") ]]; then
+  export deploy=true
+fi;
+
+echo "Test setup: run tests: $run_tests, start sauce tunnel: $start_sc_tunnel; deploy: $deploy"
