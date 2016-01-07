829ec350cd4e1aae8a6c9fa97af532d15d6439e0
Antti Leppä <antti.leppa@foyt.fi>
2016-01-07 17:17:16 +0200

    ** Refactored event's dojoin view to use same email template as the registration form

--- webapp/src/main/java/fi/foyt/fni/view/illusion/IllusionEventDoJoinBackingBean.java
+++ webapp/src/main/java/fi/foyt/fni/view/illusion/IllusionEventDoJoinBackingBean.java
@@ -1,23 +1,27 @@
 package fi.foyt.fni.view.illusion;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.ejb.Stateful;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.inject.Inject;
 import javax.inject.Named;
+import javax.mail.MessagingException;
 
 import org.ocpsoft.rewrite.annotation.Join;
 import org.ocpsoft.rewrite.annotation.Parameter;
-import org.ocpsoft.rewrite.annotation.RequestAction;
-import org.ocpsoft.rewrite.faces.annotation.Deferred;
 
+import de.neuland.jade4j.exceptions.JadeException;
 import fi.foyt.fni.illusion.IllusionEventController;
 import fi.foyt.fni.jsf.NavigationController;
 import fi.foyt.fni.persistence.model.illusion.IllusionEvent;
 import fi.foyt.fni.persistence.model.illusion.IllusionEventParticipant;
 import fi.foyt.fni.persistence.model.illusion.IllusionEventParticipantRole;
+import fi.foyt.fni.persistence.model.illusion.IllusionEventRegistrationForm;
 import fi.foyt.fni.persistence.model.users.User;
-import fi.foyt.fni.security.LoggedIn;
 import fi.foyt.fni.security.SecurityContext;
 import fi.foyt.fni.session.SessionController;
 import fi.foyt.fni.utils.faces.FacesUtils;
@@ -26,10 +30,13 @@ import fi.foyt.fni.utils.faces.FacesUtils;
 @Named
 @Stateful
 @Join (path = "/illusion/event/{urlName}/dojoin", to = "/illusion/dojoin.jsf")
-public class IllusionEventDoJoinBackingBean {
+public class IllusionEventDoJoinBackingBean extends AbstractIllusionEventBackingBean {
 
   @Parameter
   private String urlName;
+  
+  @Inject
+  private Logger logger;
 
   @Inject
   private IllusionEventController illusionEventController;
@@ -40,17 +47,16 @@ public class IllusionEventDoJoinBackingBean {
   @Inject
   private NavigationController navigationController;
   
-  @RequestAction
-  @Deferred
-  @LoggedIn
-  public String init() {
-    IllusionEvent illusionEvent = illusionEventController.findIllusionEventByUrlName(getUrlName());
-    if (illusionEvent == null) {
-      return navigationController.notFound();
+  @Override
+  public String init(IllusionEvent illusionEvent, IllusionEventParticipant participant) {
+    IllusionEventRegistrationForm registrationForm = illusionEventController.findEventRegistrationForm(illusionEvent);
+    if (registrationForm != null) {
+      return String.format("/illusion/event-registration.jsf?faces-redirect=true&urlName=%s", getUrlName());
     }
     
+    boolean newParticipant = false;
+    
     User loggedUser = sessionController.getLoggedUser();
-    IllusionEventParticipant participant = illusionEventController.findIllusionEventParticipantByEventAndUser(illusionEvent, loggedUser);
     if (participant == null) {
       if (!illusionEvent.getPublished()) {
         return navigationController.accessDenied();
@@ -60,9 +66,11 @@ public class IllusionEventDoJoinBackingBean {
         case APPROVE:
           illusionEventController.createIllusionEventParticipant(loggedUser, illusionEvent, null, IllusionEventParticipantRole.PENDING_APPROVAL);
           FacesUtils.addPostRedirectMessage(FacesMessage.SEVERITY_INFO, FacesUtils.getLocalizedValue("illusion.event.approvalPendingMessage"));
+          newParticipant = true;
           return "/illusion/event.jsf?faces-redirect=true&ignoreMessages=true&urlName=" + getUrlName();
         case OPEN:
           illusionEventController.createIllusionEventParticipant(loggedUser, illusionEvent, null, IllusionEventParticipantRole.PARTICIPANT);
+          newParticipant = true;
           return "/illusion/event.jsf?faces-redirect=true&urlName=" + getUrlName();
         default:
           return navigationController.accessDenied();
@@ -88,6 +96,18 @@ public class IllusionEventDoJoinBackingBean {
         case ORGANIZER:
         case PARTICIPANT:
           return "/illusion/event.jsf?faces-redirect=true&urlName=" + getUrlName();
+      }
+    }
+    
+    if (newParticipant) {
+      try {
+        sendConfirmRegistrationMails(illusionEvent, participant, false, null, null);
+      } catch (JadeException | IOException e) {
+        logger.log(Level.SEVERE, "Failed to render registration mail template", e);
+        return navigationController.internalError();
+      } catch (MessagingException e) {
+        logger.log(Level.SEVERE, "Failed to send registration mail", e);
+        return navigationController.internalError();
       }
     }
     
