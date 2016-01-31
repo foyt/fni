49df2b2b25eb1cd2f65b8ba03e480835444aeff5
Antti Leppä <antti.leppa@foyt.fi>
2016-01-31 20:15:11 +0200

    ** Converted event join request and payment accepted mails to use Jade templates

--- webapp/src/main/java/fi/foyt/fni/illusion/OrderStatusChangeListener.java
+++ webapp/src/main/java/fi/foyt/fni/illusion/OrderStatusChangeListener.java
@@ -1,29 +1,25 @@
 package fi.foyt.fni.illusion;
 
+import java.util.HashMap;
 import java.util.Locale;
-import java.util.logging.Level;
+import java.util.Map;
 import java.util.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
-import javax.mail.MessagingException;
 
 import org.apache.commons.lang3.LocaleUtils;
-import org.apache.commons.lang3.StringUtils;
 
 import fi.foyt.fni.gamelibrary.OrderController;
 import fi.foyt.fni.gamelibrary.OrderPaidEvent;
 import fi.foyt.fni.i18n.ExternalLocales;
-import fi.foyt.fni.mail.Mailer;
+import fi.foyt.fni.jade.JadeLocaleHelper;
 import fi.foyt.fni.persistence.model.gamelibrary.Order;
 import fi.foyt.fni.persistence.model.illusion.IllusionEvent;
 import fi.foyt.fni.persistence.model.illusion.IllusionEventParticipant;
 import fi.foyt.fni.persistence.model.illusion.IllusionEventParticipantRole;
-import fi.foyt.fni.persistence.model.system.SystemSettingKey;
 import fi.foyt.fni.persistence.model.users.User;
-import fi.foyt.fni.system.SystemSettingsController;
-import fi.foyt.fni.users.UserController;
 
 @ApplicationScoped
 public class OrderStatusChangeListener {
@@ -35,17 +31,12 @@ public class OrderStatusChangeListener {
 	private OrderController orderController;
 
   @Inject
-	private UserController userController;
-
-  @Inject
-  private Mailer mailer;
+  private IllusionMailer illusionMailer;
   
 	@Inject
 	private IllusionEventController illusionEventController;
 
-  @Inject
-	private SystemSettingsController systemSettingsController;
-	
+  
 	public void onOrderPaid(@Observes OrderPaidEvent event) {
 		if (event.getOrderId() != null) {
   		Order order = orderController.findOrderById(event.getOrderId()); 
@@ -54,9 +45,9 @@ public class OrderStatusChangeListener {
   		    case ILLUSION_EVENT:
   		      IllusionEvent illusionEvent = orderController.findOrderIllusionEvent(order);
   		      if (illusionEvent != null) {
-  		        IllusionEventParticipant member = illusionEventController.findIllusionEventParticipantByEventAndUser(illusionEvent, order.getCustomer());
-    		      illusionEventController.updateIllusionEventParticipantRole(member, IllusionEventParticipantRole.PARTICIPANT);
-    		      sendPaymentAcceptedMail(member);
+  		        IllusionEventParticipant participant = illusionEventController.findIllusionEventParticipantByEventAndUser(illusionEvent, order.getCustomer());
+    		      illusionEventController.updateIllusionEventParticipantRole(participant, IllusionEventParticipantRole.PARTICIPANT);
+    		      sendPaymentAcceptedMail(participant);
   		      } else {
   		        logger.severe("Tried to lift illusion group member role to participant for non-existing group");
   		      }
@@ -74,27 +65,25 @@ public class OrderStatusChangeListener {
 	
   private void sendPaymentAcceptedMail(IllusionEventParticipant participant) {
     User user = participant.getUser();
-    Locale userLocale = LocaleUtils.toLocale(user.getLocale());
-    String userMail = userController.getUserPrimaryEmail(user);
-    String userName = participant.getUser().getFullName();
-    String groupName = participant.getEvent().getName();
-    String groupUrlName = participant.getEvent().getUrlName();
+    IllusionEvent illusionEvent = participant.getEvent();
+    Locale locale = LocaleUtils.toLocale(user.getLocale());
     
-    String groupUrl = systemSettingsController.getSiteUrl(false, true);
-    if (StringUtils.isNotBlank(groupUrl)) {
-      groupUrl += "/illusion/event/" + groupUrlName;
-    }
-
-    String subject = ExternalLocales.getText(userLocale, "illusion.mail.paymentAccepted.subject");
-    String content = ExternalLocales.getText(userLocale, "illusion.mail.paymentAccepted.content", userName, groupName, groupUrl);
-
-    String fromName = systemSettingsController.getSetting(SystemSettingKey.SYSTEM_MAILER_NAME);
-    String fromMail = systemSettingsController.getSetting(SystemSettingKey.SYSTEM_MAILER_MAIL);
+    String subject = ExternalLocales.getText(locale, "illusion.payment.eventPaidMail.subject", illusionEvent.getName());
+    Map<String, Object> templateModel = createMailTemplateModel(participant, illusionEvent);
+    illusionMailer.sendMail(participant, subject, "mail-event-paid", templateModel);
+  }
+  
+  private Map<String, Object> createMailTemplateModel(IllusionEventParticipant participant, IllusionEvent illusionEvent) {
+    Map<String, Object> templateModel = new HashMap<>();
+    User user = participant.getUser();
+    Locale locale = LocaleUtils.toLocale(user.getLocale());
+    String eventUrl = illusionEventController.getEventUrl(illusionEvent);
     
-    try {
-      mailer.sendMail(fromMail, fromName, userMail, userName, subject, content, "text/plain");
-    } catch (MessagingException e) {
-      logger.log(Level.SEVERE, "Could not send a group accept notification mail", e);
-    }
+    templateModel.put("firstName", user.getFirstName());
+    templateModel.put("eventName", illusionEvent.getName());
+    templateModel.put("eventLink", eventUrl);
+    templateModel.put("locale", new JadeLocaleHelper(locale));
+    
+    return templateModel;
   }
 }
