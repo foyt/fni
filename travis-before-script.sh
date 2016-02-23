4dc6b5d024507dc0b79630fb66f674eda7d79eb6
Antti Leppä <antti.leppa@foyt.fi>
2016-02-23 19:59:16 +0200

    ** Fixed issue with downloading phantomjs

--- travis-before-script.sh
+++ travis-before-script.sh
@@ -22,7 +22,8 @@ fi;
 if [[ $run_tests = "true" ]]; then
 
   if [[ $it_browser = "phantomjs" ]]; then
-    if [ ! -f itests/.phantomjs ]; then
+    if [ ! -f itests/.phantomjs/bin/phantomjs ]; then
+      rm -fR itests/.phantomjs
       curl -sSL "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2"|tar -xvjC itests/
       mv itests/phantomjs-2.1.1-linux-x86_64 itests/.phantomjs
     fi;
