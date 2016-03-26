04385726ec348fc91cde286680833a04e7c28154
Antti Leppä <antti.leppa@foyt.fi>
2016-03-26 07:10:59 +0200

    ** Changed phantomjs download url

--- travis-before-script.sh
+++ travis-before-script.sh
@@ -24,7 +24,7 @@ if [[ $run_tests = "true" ]]; then
   if [[ $it_browser = "phantomjs" ]]; then
     if [ ! -f itests/.phantomjs/bin/phantomjs ]; then
       rm -fR itests/.phantomjs
-      curl -sSL "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2"|tar -xvjC itests/
+      curl -sSL "https://dl.dropboxusercontent.com/s/wz7o1jqclt8f4sy/phantomjs-2.1.1-linux-x86_64.tar.bz2"|tar -xvjC itests/
       mv itests/phantomjs-2.1.1-linux-x86_64 itests/.phantomjs
     fi;
   fi;
