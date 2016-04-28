b2d13582d2465899ca792ba36ba10be30b8ea8bc
Antti Leppä <antti.leppa@foyt.fi>
2016-04-28 07:39:04 +0300

    Added tag tests to Forge public index view

--- itests/src/test/java/fi/foyt/fni/test/ui/base/forgepublic/ForgePublicIndexTestsBase.java
+++ itests/src/test/java/fi/foyt/fni/test/ui/base/forgepublic/ForgePublicIndexTestsBase.java
@@ -10,6 +10,7 @@ import fi.foyt.fni.test.ui.base.AbstractUITest;
 @DefineSqlSets ({
   @DefineSqlSet(id = "users", before = "basic-users-setup.sql", after = "basic-users-teardown.sql"),
   @DefineSqlSet(id = "public-materials", before = { "basic-materials-setup.sql", "basic-materials-public-setup.sql", "basic-materials-views-setup.sql" }, after = { "basic-materials-views-teardown.sql", "basic-materials-teardown.sql" } ),
+  @DefineSqlSet(id = "public-material-tags", before = { "basic-tags-setup.sql", "basic-materials-tags-setup.sql", "basic-materials-views-setup.sql" }, after = { "basic-materials-tags-teardown.sql", "basic-tags-teardown.sql" } ),
 })
 public class ForgePublicIndexTestsBase extends AbstractUITest {
 
@@ -30,7 +31,7 @@ public class ForgePublicIndexTestsBase extends AbstractUITest {
   }
 
   @Test
-  @SqlSets ({"users", "public-materials"})
+  @SqlSets ({"users", "public-materials", "public-material-tags"})
   public void testMostPopularAnonymous() {
     navigate("/forge/public");
     waitAndAssertSelectorText(".most-popular h3", "Most Popular", true, true);
@@ -38,17 +39,29 @@ public class ForgePublicIndexTestsBase extends AbstractUITest {
     assertSelectorText(".most-popular .material:nth-child(1) h4 a", "Document", true, true);
     assertSelectorText(".most-popular .material:nth-child(1) .modified", "MODIFIED: WEDNESDAY, JANUARY 6, 2010", true, true);
     assertSelectorText(".most-popular .material:nth-child(1) .creator-tag", "Test User", true, true);
+    assertSelectorText(".latest .material:nth-child(1) .material-tags a.creator-tag", "Test User", true, true);
+    assertSelectorText(".latest .material:nth-child(1) .material-tags a:nth-child(2)", "Test", true, true);
+    assertSelectorText(".latest .material:nth-child(1) .material-tags a:nth-child(3)", "Tag with space", true, true);
   }
   
   @Test
-  @SqlSets ({"users", "public-materials"})
+  @SqlSets ({"users", "public-materials", "public-material-tags"})
   public void testMostRecentAnonymous() {
     navigate("/forge/public");
     waitAndAssertSelectorText(".latest h3", "Latest", true, true);
     assertSelectorCount(".latest .material", 5);
     assertSelectorText(".latest .material:nth-child(1) h4 a", "Google Doc", true, true);
     assertSelectorText(".latest .material:nth-child(1) .modified", "MODIFIED: FRIDAY, JANUARY 22, 2010", true, true);
-    assertSelectorText(".latest .material:nth-child(1) .creator-tag", "Test User", true, true);
   }
-  
+
+  @Test
+  @SqlSets ({"users", "public-materials", "public-material-tags"})
+  public void testCategories() {
+    navigate("/forge/public");
+    waitAndAssertSelectorText(".categories h3", "Categories", true, true);
+    assertSelectorCount(".categories .tags li", 3);
+    assertSelectorText(".categories .tags li:nth-child(1) a", "test (3)", true, true);
+    assertSelectorText(".categories .tags li:nth-child(2) a", "tag (2)", true, true);
+    assertSelectorText(".categories .tags li:nth-child(3) a", "with space (1)", true, true);
+  }
 }
