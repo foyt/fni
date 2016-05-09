17715cfc64a31360f0215900aa4413a027631360
Antti Leppä <antti.leppa@foyt.fi>
2016-05-09 06:12:50 +0300

    Changed ForumTestsIT#testForumUnread to scroll before click

--- itests/src/test/java/fi/foyt/fni/test/ui/base/forum/ForumTestsBase.java
+++ itests/src/test/java/fi/foyt/fni/test/ui/base/forum/ForumTestsBase.java
@@ -103,7 +103,7 @@ public class ForumTestsBase extends AbstractUITest {
     navigate("/forum/1_topic_forum/single_topic");
     waitForSelectorVisible(".cke_wysiwyg_frame");
     setCKEditorContents(0, "<p>Post</p>");
-    waitAndClick(".forum-topic-post-send-container input");
+    scrollWaitAndClick(".forum-topic-post-send-container input");
     waitForSelectorCount(".post", 2);
 
     logout();
