02453fe07fb4877a440fdeb3eebf98b1ff50e80b
Antti Leppä <antti.leppa@foyt.fi>
2016-08-13 12:18:38 +0300

    Switched to use latest stable sc

--- travis-before-script.sh
+++ travis-before-script.sh
@@ -3,8 +3,8 @@
 echo "Running before script with test setup: run tests: $run_tests, start sauce tunnel: $start_sc_tunnel, deploy: $deploy"
 
 if [[ $start_sc_tunnel = true ]]; then
-  curl -sS https://saucelabs.com/downloads/sc-4.4.0-rc2-linux.tar.gz|tar -xzC /tmp/
-  /tmp/sc-4.4.0-rc2-linux/bin/sc -r 10 -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY -i $TRAVIS_JOB_NUMBER --vm-version dev-varnish -B *.facebook.com --daemonize --readyfile /tmp/sc-ready
+  curl -sS https://saucelabs.com/downloads/sc-4.3.6-linux.tar.gz|tar -xzC /tmp/
+  /tmp/sc-4.3.6-linux/bin/sc -r 10 -u $SAUCE_USERNAME -k $SAUCE_ACCESS_KEY -i $TRAVIS_JOB_NUMBER --vm-version dev-varnish -B *.facebook.com --daemonize --readyfile /tmp/sc-ready
   t=0;
   while [ ! -f /tmp/sc-ready ]; do 
     sleep 1; 
