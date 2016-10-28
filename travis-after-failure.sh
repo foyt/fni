ff66c12e1d3aa143e1a276d28a1a396bfef24d94
Antti Leppä <antti.leppa@metatavu.fi>
2016-10-28 17:39:31 +0300

    Fixed wildfly log file name in Travis conf

--- travis-after-failure.sh
+++ travis-after-failure.sh
@@ -5,6 +5,8 @@ if [[ $start_sc_tunnel = true ]]; then
   killall sc
 fi;
 
-for f in itests/target/*.png; do curl --form "fileupload=@$f" http://uploads.im/api?upload; done
+cat itests/target/cargo/configurations/wildfly10x/log/server.log
 
-cat itests/target/cargo/configurations/wildfly8x/log/server.log
+if [[ $it_browser = "phantomjs" ]]; then
+  for f in itests/target/*.png; do curl --form "fileupload=@$f" http://uploads.im/api?upload; done
+fi;
